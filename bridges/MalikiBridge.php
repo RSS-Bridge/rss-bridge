65b044b84da5345dae4e0f535d6d75477136b3d5
mitsukarenai <mitsu@suumitsu.eu>
2013-09-11 15:11:31 +0200

    Update MalikiBridge.php

--- bridges/MalikiBridge.php
+++ bridges/MalikiBridge.php
@@ -1,18 +1,18 @@
 <?php
 /**
 * RssBridgeMaliki 
-* Returns Maliki's strips from previous weeks
+* Returns Maliki's strips from previous weeks (20 items)
 *
 * @name Maliki
-* @description Returns Maliki's strips from previous weeks
+* @description Returns Maliki's strips from previous weeks (20 items)
 */
 class MalikiBridge extends BridgeAbstract{
 
     public function collectData(array $param){
         $html = file_get_html('http://www.maliki.com/') or $this->returnError('Could not request Maliki.', 404);
-
+	$count=0;
         foreach($html->find('div.boite_strip') as $element) {
-	  if(!empty($element->find('a',0)->href)) {
+	  if(!empty($element->find('a',0)->href) and $count < 20) {
             $item = new \Item();
             $item->uri = 'http://www.maliki.com/'.$element->find('a',0)->href;
             $item->thumbnailUri = 'http://www.maliki.com/'.$element->find('img',0)->src;
@@ -20,6 +20,7 @@ class MalikiBridge extends BridgeAbstract{
             $item->timestamp = strtotime(str_replace('/', '-', $element->find('span.stylepetit', 0)->innertext));
             $item->content = '<a href="' . $item->uri . '"><img src="' . $item->thumbnailUri . '" /></a>';
             $this->items[] = $item;
+	    $count++;
           }
         }
     }
