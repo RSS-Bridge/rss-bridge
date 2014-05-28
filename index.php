c4929d51e1df98bb455f9342812ac071e31b349d
Mitsukarenai <mitsu@suumitsu.eu>
2014-05-28 15:49:11 +0200

    unset curl ext until required

--- index.php
+++ index.php
@@ -19,8 +19,6 @@ error_reporting(0);
 // extensions check
 if (!extension_loaded('openssl'))
 	die('"openssl" extension not loaded. Please check "php.ini"');
-if (!extension_loaded('curl'))
-	die('"curl" extension not loaded. Please check "php.ini"');
 
 // FIXME : beta test UA spoofing, please report any blacklisting by PHP-fopen-unfriendly websites
 ini_set('user_agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:30.0) Gecko/20121202 Firefox/30.0 (rss-bridge/0.1; +https://github.com/sebsauvage/rss-bridge)');
