237886feb6092e6bd4c6b5adebc84b690a37340e
Mitsukarenai <mitsu@suumitsu.eu>
2014-05-30 17:53:48 +0200

    Add CoinDesk, update Maliki

--- bridges/MalikiBridge.php
+++ bridges/MalikiBridge.php
@@ -1,27 +1,45 @@
 <?php
 /**
 * RssBridgeMaliki 
-* Returns Maliki's strips from previous weeks
-* 2014-05-25
+* Returns Maliki's newest strips
 *
 * @name Maliki
 * @homepage http://www.maliki.com/
-* @description Returns Maliki's strips from previous weeks
+* @description Returns Maliki's newest strips
 * @maintainer mitsukarenai
+* @update 2014-05-30
 */
 class MalikiBridge extends BridgeAbstract{
 
     public function collectData(array $param){
         $html = file_get_html('http://www.maliki.com/') or $this->returnError('Could not request Maliki.', 404);
 	$count=0;
+	$latest=1; $latest_title="";
+	$latest = $html->find('div.conteneur_page a', 1)->href;
+	$latest_title = $html->find('div.conteneur_page img', 0)->title;
+
+	function MalikiExtractContent($url) {
+		$html2 = file_get_html($url);
+		$text = 'http://www.maliki.com/'.$html2->find('img', 0)->src;
+		$text = '<img alt="" src="'.$text.'"/><br>'.$html2->find('div.imageetnews', 0)->plaintext;
+		return $text;
+    	}
+
+            $item = new \Item();
+            $item->uri = 'http://www.maliki.com/'.$latest;
+            $item->title = $latest_title;
+            $item->timestamp = time();
+            $item->content = MalikiExtractContent($item->uri);
+            $this->items[] = $item;
+	
+
         foreach($html->find('div.boite_strip') as $element) {
-	  if(!empty($element->find('a',0)->href) and $count < 20) {
+	  if(!empty($element->find('a',0)->href) and $count < 3) {
             $item = new \Item();
             $item->uri = 'http://www.maliki.com/'.$element->find('a',0)->href;
-            $item->thumbnailUri = 'http://www.maliki.com/'.$element->find('img',0)->src;
             $item->title = $element->find('img',0)->title;
             $item->timestamp = strtotime(str_replace('/', '-', $element->find('span.stylepetit', 0)->innertext));
-            $item->content = '<a href="' . $item->uri . '"><img src="' . $item->thumbnailUri . '" /></a>';
+            $item->content = MalikiExtractContent($item->uri);
             $this->items[] = $item;
 	    $count++;
           }
@@ -37,6 +55,6 @@ class MalikiBridge extends BridgeAbstract{
     }
 
     public function getCacheDuration(){
-        return 86400; // 24 hours
+        return 86400*6; // 6 days
     }
 }
