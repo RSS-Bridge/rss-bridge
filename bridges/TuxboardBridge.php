d4de199501363f397b56cd48f76a74a9e7a74819
Mitsukarenai <mitsu@suumitsu.eu>
2014-07-08 17:06:49 +0200

    fix issue #64

--- bridges/TuxboardBridge.php
+++ bridges/TuxboardBridge.php
@@ -1,79 +1,46 @@
 <?php
 /**
  *
- * @name Tuxboard
+* @name Tuxboard
 * @homepage http://www.tuxboard.com/
- * @description Tuxboard
- * @update 15/01/2014
+* @description Tuxboard
+* @update 2014-07-08
 * initial maintainer: superbaillot.net
  */
 class TuxboardBridge extends BridgeAbstract{
 
     public function collectData(array $param){
-        $html = file_get_html('http://www.tuxboard.com') or $this->returnError('Could not request Tuxboard.', 404);
 
-        foreach($html->find('div.posts') as $element) {
-            $a = $element->find("h2 a", 0);
-            $category = $element->find("div#category", 0);
-            $catTxt = $category->innertext;
-            $posFinDate = strpos(" -", $catTxt);
-            $list = explode(" ", trim(substr($catTxt, $posFinDate)));
-            $jour = $list[0];
-            $mois = 1;
-            $annee = $list[2];
+    function StripCDATA($string) {
+    	$string = str_replace('<![CDATA[', '', $string);
+    	$string = str_replace(']]>', '', $string);
+    	return $string;
+    }
 
-            switch (strtolower($list[1]))
-            {
-                case "janvier" :
-                    $mois = 1;
-                    break;
-                case "février" :
-                case "fevrier" :
-                    $mois = 2;
-                    break;
-                case "mars" :
-                    $mois = 3;
-                    break;
-                case "avril" :
-                    $mois = 4;
-                    break;
-                case "mai" :
-                    $mois = 5;
-                    break;
-                case "juin" :
-                    $mois = 6;
-                    break;
-                case "juillet" :
-                    $mois = 7;
-                    break;
-                case "aout" :
-                case "août" :
-                    $mois = 8;
-                    break;
-                case "septembre" :
-                    $mois = 9;
-                    break;
-                case "octobre" :
-                    $mois = 10;
-                    break;
-                case "novembre" :
-                    $mois = 11;
-                    break;
-                case "decembre" :
-                case "décembre" :
-                    $mois = 12;
-                    break;
-            }
+    function ExtractContent($url) {
+	$html2 = file_get_html($url);
+	$text = $html2->find('article#page', 0)->innertext;
+	$text = preg_replace('@<script[^>]*?>.*?</script>@si', '', $text);
+	return $text;
+    }
 
-            $item = new Item();
+        $html = file_get_html('http://www.tuxboard.com/feed/atom/') or $this->returnError('Could not request Tuxboard.', 404);
+	$limit = 0;
 
-            $item->uri = $a->href;
-            $item->title = $a->innertext;
-            $item->content = trim($element->find("div.clear", 0)->innertext);
-            $item->timestamp = mktime(0, 0, 0, $mois, $jour, $annee);
-             
-            $this->items[] = $item;
-        }
+	foreach($html->find('entry') as $element) {
+	 if($limit < 10) {
+	 $item = new \Item();
+	 $item->title = StripCDATA($element->find('title', 0)->innertext);
+	 $item->uri = $element->find('link', 0)->href;
+	 $item->timestamp = strtotime($element->find('published', 0)->plaintext);
+	 $item->content = ExtractContent($item->uri);
+	 $this->items[] = $item;
+	 $limit++;
+	 }
+	}
+
+       
+        
     }
 
     public function getName(){
@@ -89,7 +56,7 @@ class TuxboardBridge extends BridgeAbstract{
     }
 
     public function getCacheDuration(){
-        return 14600; // 4 hours
+        return 3600; // 1 hour
     }
 }
 ?>
