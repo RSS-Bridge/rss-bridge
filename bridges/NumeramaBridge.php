136be273cec37e2b685ee919b55f069f103f8422
Mitsukarenai <mitsu@suumitsu.eu>
2014-07-16 18:06:15 +0200

    Numerama: add lists to output

--- bridges/NumeramaBridge.php
+++ bridges/NumeramaBridge.php
@@ -22,7 +22,7 @@ class NumeramaBridge extends BridgeAbstract{
 	$html2 = file_get_html($url);
 	$text = $html2->find('h2.intro', 0)->innertext;
 	$text = $text.$html2->find('div.content', 0)->innertext;
-	$text = strip_tags($text, '<p><b><a><blockquote><img><em>');
+	$text = strip_tags($text, '<p><b><a><blockquote><img><em><ul><ol>');
 	return $text;
     }
         $html = file_get_html('http://www.numerama.com/rss/news.rss') or $this->returnError('Could not request Numerama.', 404);
