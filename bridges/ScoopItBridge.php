a7ed30e2cc519ba386373a4ebdff738819de66b0
pitchoule <pitchoule@users.noreply.github.com>
2014-07-23 10:59:12 +0200

    Update ScoopItBridge
    Le "title" était en innertext et non en plaintext.

--- bridges/ScoopItBridge.php
+++ bridges/ScoopItBridge.php
@@ -23,7 +23,7 @@ class ScoopItBridge extends BridgeAbstract{
         foreach($html->find('div.post-view') as $element) {
                 $item = new Item();
                 $item->uri = $element->find('a', 0)->href;
-                $item->title = $element->find('div.tCustomization_post_title',0)->innertext;
+                $item->title = $element->find('div.tCustomization_post_title',0)->plaintext;
                 $item->content = $element->find('div.tCustomization_post_description', 0)->plaintext;
                 $this->items[] = $item;
         }
