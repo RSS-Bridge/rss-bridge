94ffb22fb057e0b07146c366503d0d396bcc8efe
Nicolas Delsaux <nicolas.delsaux@gmail.com>
2015-07-05 15:24:06 +0200

    un bridge The Oatmeal qui marche ... principalement pour les comics locaux (et pas pour explodingkittens)

--- bridges/TheOatMealBridge.php
+++ bridges/TheOatMealBridge.php
@@ -8,17 +8,33 @@
 require_once 'bridges/RssExpander.php';
 define("THE_OATMEAL", "http://theoatmeal.com/");
 define("RSS", "http://feeds.feedburner.com/oatmealfeed");
-class TheOatmeal extends RssExpander{
+class TheOatmealBridge extends RssExpander{
 
     public function collectData(array $param){
         $param['url'] = RSS;
         parent::collectData($param);
     }
-    
+
+
+    /**
+     * Since the oatmeal produces a weird RSS feed, I have to fix it by loading the items separatly from the feed infos
+     */
+    protected function collect_RSS_2_0_data($rssContent) {
+        $rssContent->registerXPathNamespace("dc", "http://purl.org/dc/elements/1.1/");
+        $rssHeaderContent = $rssContent->channel[0];
+//        $this->message("RSS content is ===========\n".var_export($rssHeaderContent, true)."===========");
+        $this->load_RSS_2_0_feed_data($rssHeaderContent);
+        foreach($rssContent->item as $item) {
+            $this->message("parsing item ".var_export($item, true));
+            $this->items[] = $this->parseRSSItem($item);
+        }
+    }
+
+
     protected function parseRSSItem($newsItem) {
         $item = new Item();
-      $item->title = trim($newsItem->title);
-//        $this->message("browsing item ".var_export($newsItem, true));
+        $item->title = trim($newsItem->title);
+        $this->message("browsing Oatmeal item ".var_export($newsItem, true));
         if(empty($newsItem->guid)) {
             $item->uri = $newsItem->link;
         } else {
@@ -32,12 +48,19 @@ class TheOatmeal extends RssExpander{
 		if($content==null) {
 			$content = $articlePage->find('#blog');
 		}
-        $item->content = $newsItem->description;
-        $item->name = $newsItem->author;
-        $item->timestamp = $this->RSS_2_0_time_to_timestamp($newsItem);
+        $item->content = $content->innertext;
+        
+        $namespaces = $newsItem->getNameSpaces(true);
+
+        $dc = $newsItem->children($namespaces['dc']);
+        $this->message("dc content is ".var_export($dc, true));
+        $item->name = $dc->creator;
+        $item->timestamp = DateTime::createFromFormat(DateTime::ISO8601, $dc->date)->getTimestamp();
+        $this->message("writtem by ".$item->name." on ".$item->timestamp);
         return $item;
     }
+    
     public function getCacheDuration(){
-        return 7200; // 2h hours
+        return 1; // 2h hours
     }
 }
