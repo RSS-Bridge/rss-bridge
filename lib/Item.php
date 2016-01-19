4deefdfd7d52843e7cf80d0134c1172ececa25a0
Teromene <teromene@teromene.fr>
2016-01-19 12:34:38 +0000

    Add enclosures support, see example in DemoBridge.

--- lib/Item.php
+++ lib/Item.php
@@ -2,15 +2,19 @@
 interface ItemInterface{}
 
 /**
-* Object to store datas collect informations
-* FIXME : not sur this logic is the good, I think recast all is necessary
-*/
+ * Object to store datas collect informations
+ * FIXME : not sur this logic is the good, I think recast all is necessary
+ */
 class Item implements ItemInterface{
+
+    // FIXME : use the arrayInterface instead
+    public $enclosures = array();
+    
     public function __set($name, $value){
         $this->$name = $value;
     }
 
     public function __get($name){
-        return isset($this->$name) ? $this->$name : null;
+        return (isset($this->$name) ? $this->$name : null);
     }
 }
