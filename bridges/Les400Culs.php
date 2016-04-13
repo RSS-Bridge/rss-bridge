3d4e3679ada32dad6b80bbd00dd98e2b6b4b7320
unknown <ndelsaux@LFR005669.corp.capgemini.com>
2016-04-13 21:22:09 +0200

    Once again, SimpleXMLElement strikes and makes sure the feed can't be used. So I fix it ... again

--- bridges/Les400Culs.php
+++ bridges/Les400Culs.php
@@ -9,7 +9,7 @@ class Les400Culs extends RssExpander{
 		$this->maintainer = "unknown";
 		$this->name = "Les 400 Culs";
 		$this->uri = "http://sexes.blogs.liberation.fr";
-		$this->description = "La planète sexe vue par Agnès Girard via rss-bridge";
+		$this->description = "La planete sexe vue par Agnes Girard via rss-bridge";
 		$this->update = "20/02/2014";
 
 	}
@@ -21,20 +21,20 @@ class Les400Culs extends RssExpander{
     
     protected function parseRSSItem($newsItem) {
         $item = new Item();
-        $item->title = trim($newsItem->title);
+        $item->title = trim((string) $newsItem->title);
 //        $this->message("browsing item ".var_export($newsItem, true));
         if(empty($newsItem->guid)) {
-            $item->uri = $newsItem->link;
+            $item->uri = (string) $newsItem->link;
         } else {
-            $item->uri = $newsItem->guid;
+            $item->uri = (string) $newsItem->guid;
         }
         // now load that uri from cache
 //        $this->message("now loading page ".$item->uri);
 //        $articlePage = str_get_html($this->get_cached($item->uri));
 
 //        $content = $articlePage->find('.post-container', 0);
-        $item->content = $newsItem->description;
-        $item->name = $newsItem->author;
+        $item->content = (string) $newsItem->description;
+        $item->name = (string) $newsItem->author;
         $item->timestamp = $this->RSS_2_0_time_to_timestamp($newsItem);
         return $item;
     }
