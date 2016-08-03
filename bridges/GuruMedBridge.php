a1a44cd1ff4da8cdefcd4ec3005a75dd4429e947
logmanoriginal <log.man@gmx.de>
2016-08-03 13:30:38 +0200

    Change all nested functions to member functions
    This fixes error "Using $this when not in object context"
    
    Nested functions are not part of the object and therefore don't have
    access to the object instance $this!

--- bridges/GuruMedBridge.php
+++ bridges/GuruMedBridge.php
@@ -7,32 +7,33 @@ class GuruMedBridge extends BridgeAbstract{
 		$this->name = "GuruMed";
 		$this->uri = "http://www.gurumed.org";
 		$this->description = "Returns the 5 newest posts from Gurumed (full text)";
-		$this->update = "03/10/2015";
+		$this->update = "2016-08-03";
 
 	}
-
-    public function collectData(array $param){
 
     function GurumedStripCDATA($string) {
     	$string = str_replace('<![CDATA[', '', $string);
     	$string = str_replace(']]>', '', $string);
     	return $string;
     }
+
     function GurumedExtractContent($url) {
 	$html2 = $this->file_get_html($url);
 	$text = $html2->find('div.entry', 0)->innertext;
 	return $text;
     }
+
+    public function collectData(array $param){
         $html = $this->file_get_html('http://gurumed.org/feed') or $this->returnError('Could not request Gurumed.', 404);
 	$limit = 0;
 
 	foreach($html->find('item') as $element) {
 	 if($limit < 5) {
 	 $item = new \Item();
-	 $item->title = GurumedStripCDATA($element->find('title', 0)->innertext);
-	 $item->uri = GurumedStripCDATA($element->find('guid', 0)->plaintext);
+	 $item->title = $this->GurumedStripCDATA($element->find('title', 0)->innertext);
+	 $item->uri = $this->GurumedStripCDATA($element->find('guid', 0)->plaintext);
 	 $item->timestamp = strtotime($element->find('pubDate', 0)->plaintext);
-	 $item->content = GurumedExtractContent($item->uri);
+	 $item->content = $this->GurumedExtractContent($item->uri);
 	 $this->items[] = $item;
 	 $limit++;
 	 }
