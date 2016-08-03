b344f507847e0eb00bdfbbbb713f7789ac18ee1b
logmanoriginal <log.man@gmx.de>
2016-08-03 14:00:24 +0200

    Fix indentation and remove empty lines

--- bridges/GuruMedBridge.php
+++ bridges/GuruMedBridge.php
@@ -2,48 +2,45 @@
 class GuruMedBridge extends BridgeAbstract{
 
 	public function loadMetadatas() {
-
 		$this->maintainer = "qwertygc";
 		$this->name = "GuruMed";
 		$this->uri = "http://www.gurumed.org";
 		$this->description = "Returns the 5 newest posts from Gurumed (full text)";
 		$this->update = "2016-08-03";
-
 	}
 
-    function GurumedStripCDATA($string) {
-    	$string = str_replace('<![CDATA[', '', $string);
-    	$string = str_replace(']]>', '', $string);
-    	return $string;
-    }
-
-    public function collectData(array $param){
-        $html = $this->file_get_html('http://gurumed.org/feed') or $this->returnError('Could not request Gurumed.', 404);
-	$limit = 0;
-
-	foreach($html->find('item') as $element) {
-	 if($limit < 5) {
-	 $item = new \Item();
-	 $item->title = $this->GurumedStripCDATA($element->find('title', 0)->innertext);
-	 $item->uri = $this->GurumedStripCDATA($element->find('guid', 0)->plaintext);
-	 $item->timestamp = strtotime($element->find('pubDate', 0)->plaintext);
-	 $item->content = $this->GurumedStripCDATA(strip_tags($element->find('description', 0), '<p><a><br>'));
-	 $this->items[] = $item;
-	 $limit++;
-	 }
+	function GurumedStripCDATA($string) {
+		$string = str_replace('<![CDATA[', '', $string);
+		$string = str_replace(']]>', '', $string);
+		return $string;
 	}
-    
-    }
 
-    public function getName(){
-        return 'Gurumed';
-    }
+	public function collectData(array $param){
+		$html = $this->file_get_html('http://gurumed.org/feed') or $this->returnError('Could not request Gurumed.', 404);
+		$limit = 0;
 
-    public function getURI(){
-        return 'http://gurumed.org/';
-    }
+		foreach($html->find('item') as $element) {
+			if($limit < 5) {
+				$item = new \Item();
+				$item->title = $this->GurumedStripCDATA($element->find('title', 0)->innertext);
+				$item->uri = $this->GurumedStripCDATA($element->find('guid', 0)->plaintext);
+				$item->timestamp = strtotime($element->find('pubDate', 0)->plaintext);
+				$item->content = $this->GurumedStripCDATA(strip_tags($element->find('description', 0), '<p><a><br>'));
+				$this->items[] = $item;
+				$limit++;
+			}
+		}
+	}
 
-    public function getCacheDuration(){
-        return 3600; // 1 hour
-    }
+	public function getName(){
+		return 'Gurumed';
+	}
+
+	public function getURI(){
+		return 'http://gurumed.org/';
+	}
+
+	public function getCacheDuration(){
+		return 3600; // 1 hour
+	}
 }
