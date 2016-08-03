924e123ba6069c414864e891576e9d5b417272fb
logmanoriginal <log.man@gmx.de>
2016-08-03 21:14:46 +0200

    Fix indentation and remove empty lines

--- bridges/MsnMondeBridge.php
+++ bridges/MsnMondeBridge.php
@@ -2,13 +2,11 @@
 class MsnMondeBridge extends BridgeAbstract{
 
 	public function loadMetadatas() {
-
 		$this->maintainer = "kranack";
 		$this->name = "MSN Actu Monde";
 		$this->uri = "http://www.msn.com/fr-fr/actualite/monde";
 		$this->description = "Returns the 10 newest posts from MSN Actualités (full text)";
-		$this->update = "2015-01-30";
-
+		$this->update = "2016-08-03";
 	}
 
 	function MsnMondeExtractContent($url, &$item) {
@@ -17,30 +15,30 @@ class MsnMondeBridge extends BridgeAbstract{
 		$item->timestamp = strtotime($html2->find('.authorinfo-txt', 0)->find('time', 0)->datetime);
 	}
 
-    public function collectData(array $param){
-      $html = $this->file_get_html('http://www.msn.com/fr-fr/actualite/monde') or $this->returnError('Could not request MsnMonde.', 404);
-      $limit = 0;
-      foreach($html->find('.smalla') as $article) {
-       if($limit < 10) {
-         $item = new \Item();
-         $item->title = utf8_decode($article->find('h4', 0)->innertext);
-         $item->uri = "http://www.msn.com" . utf8_decode($article->find('a', 0)->href);
-         $this->MsnMondeExtractContent($item->uri, $item);
-         $this->items[] = $item;
-         $limit++;
-       }
-      }
-    }
+	public function collectData(array $param){
+		$html = $this->file_get_html($this->getURI()) or $this->returnError('Could not request MsnMonde.', 404);
+		$limit = 0;
+		foreach($html->find('.smalla') as $article) {
+			if($limit < 10) {
+				$item = new \Item();
+				$item->title = utf8_decode($article->find('h4', 0)->innertext);
+				$item->uri = "http://www.msn.com" . utf8_decode($article->find('a', 0)->href);
+				$this->MsnMondeExtractContent($item->uri, $item);
+				$this->items[] = $item;
+				$limit++;
+			}
+		}
+	}
 
-    public function getName(){
-        return 'MSN Actu Monde';
-    }
+	public function getName(){
+		return 'MSN Actu Monde';
+	}
 
-    public function getURI(){
-        return 'http://www.msn.com/fr-fr/actualite/monde';
-    }
+	public function getURI(){
+		return 'http://www.msn.com/fr-fr/actualite/monde';
+	}
 
-    public function getCacheDuration(){
-        return 3600; // 1 hour
-    }
+	public function getCacheDuration(){
+		return 3600; // 1 hour
+	}
 }
