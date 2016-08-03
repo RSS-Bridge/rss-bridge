58b3261fbbdbd7c3b18eb0e0d42e42bbbb70362e
logmanoriginal <log.man@gmx.de>
2016-08-03 12:42:57 +0200

    Fix indentation and remove empty lines

--- bridges/DeveloppezDotComBridge.php
+++ bridges/DeveloppezDotComBridge.php
@@ -2,13 +2,11 @@
 class DeveloppezDotComBridge extends BridgeAbstract{
 
 	public function loadMetadatas() {
-
 		$this->maintainer = "polopollo";
 		$this->name = "Developpez.com Actus (FR)";
 		$this->uri = "http://www.developpez.com/";
 		$this->description = "Returns the 15 newest posts from DeveloppezDotCom (full text).";
-		$this->update = "2014-07-14";
-
+		$this->update = "2016-08-03";
 	}
 
 	function DeveloppezDotComStripCDATA($string) {
@@ -17,7 +15,9 @@ class DeveloppezDotComBridge extends BridgeAbstract{
 		return $string;
 	}
 
-	function convert_smart_quotes($string)//F***ing quotes from Microsoft Word badly encoded, here was the trick: http://stackoverflow.com/questions/1262038/how-to-replace-microsoft-encoded-quotes-in-php
+	// F***ing quotes from Microsoft Word badly encoded, here was the trick: 
+	// http://stackoverflow.com/questions/1262038/how-to-replace-microsoft-encoded-quotes-in-php
+	function convert_smart_quotes($string)
 	{
 		$search = array(chr(145),
 						chr(146),
@@ -41,35 +41,33 @@ class DeveloppezDotComBridge extends BridgeAbstract{
 		return trim($text);
 	}
 
-    public function collectData(array $param){
+	public function collectData(array $param){
+		$rssFeed = $this->file_get_html('http://www.developpez.com/index/rss') or $this->returnError('Could not request http://www.developpez.com/index/rss', 404);
+		$limit = 0;
 
-        $rssFeed = $this->file_get_html('http://www.developpez.com/index/rss') or $this->returnError('Could not request http://www.developpez.com/index/rss', 404);
-    	$limit = 0;
+		foreach($rssFeed->find('item') as $element) {
+			if($limit < 10) {
+				$item = new \Item();
+				$item->title = $this->DeveloppezDotComStripCDATA($element->find('title', 0)->innertext);
+				$item->uri = $this->DeveloppezDotComStripCDATA($element->find('guid', 0)->plaintext);
+				$item->timestamp = strtotime($element->find('pubDate', 0)->plaintext);
+				$content = $this->DeveloppezDotComExtractContent($item->uri);
+				$item->content = strlen($content) ? $content : $element->description; //In case of it is a tutorial, we just keep the original description
+				$this->items[] = $item;
+				$limit++;
+			}
+		}
+	}
 
-    	foreach($rssFeed->find('item') as $element) {
-            if($limit < 10) {
-                $item = new \Item();
-                $item->title = $this->DeveloppezDotComStripCDATA($element->find('title', 0)->innertext);
-                $item->uri = $this->DeveloppezDotComStripCDATA($element->find('guid', 0)->plaintext);
-                $item->timestamp = strtotime($element->find('pubDate', 0)->plaintext);
-                $content = $this->DeveloppezDotComExtractContent($item->uri);
-                $item->content = strlen($content) ? $content : $element->description;//In case of it is a tutorial, we just keep the original description
-                $this->items[] = $item;
-                $limit++;
-            }
-    	}
+	public function getName(){
+		return 'DeveloppezDotCom';
+	}
 
-    }
+	public function getURI(){
+		return 'http://www.developpez.com/';
+	}
 
-    public function getName(){
-        return 'DeveloppezDotCom';
-    }
-
-    public function getURI(){
-        return 'http://www.developpez.com/';
-    }
-
-    public function getCacheDuration(){
-        return 1800; // 30min
-    }
+	public function getCacheDuration(){
+		return 1800; // 30min
+	}
 }
