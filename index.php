3f83763ff96008458acdbed40b416f862ad1fcb8
logmanoriginal <log.man@gmx.de>
2016-08-07 15:15:55 +0200

    [index] Don't render 'Show inactive bridges' if non inactive

--- index.php
+++ index.php
@@ -188,11 +188,13 @@ $formats = Format::searchInformation();
         <a href="https://github.com/sebsauvage/rss-bridge">RSS-Bridge alpha 0.2 ~ Public Domain</a><br />
 		<?= $activeFoundBridgeCount; ?>/<?= count($bridgeList) ?> active bridges. <br />
         <?php
-            // FIXME: This should be done in pure CSS
-            if(!$showInactive)
-                echo '<a href="?show_inactive=1"><button class="small">Show inactive bridges</button></a><br />';
-            else
-                echo '<a href="?show_inactive=0"><button class="small">Hide inactive bridges</button></a><br />';
+            if($activeFoundBridgeCount !== count($bridgeList)){
+                // FIXME: This should be done in pure CSS
+                if(!$showInactive)
+                    echo '<a href="?show_inactive=1"><button class="small">Show inactive bridges</button></a><br />';
+                else
+                    echo '<a href="?show_inactive=0"><button class="small">Hide inactive bridges</button></a><br />';
+            }
         ?>
     </section>
     </body>
