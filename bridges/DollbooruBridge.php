3df0f299defe443dbc6614dc7ce73a79894c2d88
Pierre Mazière <pierre.maziere@gmx.com>
2016-08-28 14:04:19 +0200

    [DollbooruBridge] fix + code simplification
    Signed-off-by: Pierre Mazière <pierre.maziere@gmx.com>

--- bridges/DollbooruBridge.php
+++ bridges/DollbooruBridge.php
@@ -16,22 +16,18 @@ class DollbooruBridge extends BridgeAbstract{
     ));
 
     public function collectData(){
-	$page = 0;$tags='';
-        if (isset($this->getInput('p'))) {
-            $page = (int)preg_replace("/[^0-9]/",'', $this->getInput('p'));
-        }
-        if (isset($this->getInput('t'))) {
-            $tags = urlencode($this->getInput('t'));
-        }
-        $html = $this->getSimpleHTMLDOM("http://dollbooru.org/post/list/$tags/$page") or $this->returnServerError('Could not request Dollbooru.');
+        $page=$this->getInput('p');
+        $tags = urlencode($this->getInput('t'));
+        $html = $this->getSimpleHTMLDOM($this->uri."post/list/$tags/$page")
+            or $this->returnServerError('Could not request Dollbooru.');
 
 
 	foreach($html->find('div[class=shm-image-list] a') as $element) {
 		$item = array();
-		$item['uri'] = 'http://dollbooru.org'.$element->href;
+		$item['uri'] = $this->uri.$element->href;
 		$item['postid'] = (int)preg_replace("/[^0-9]/",'', $element->getAttribute('data-post-id'));
 		$item['timestamp'] = time();
-		$thumbnailUri = 'http://dollbooru.org'.$element->find('img', 0)->src;
+		$thumbnailUri = $this->uri.$element->find('img', 0)->src;
 		$item['tags'] = $element->getAttribute('data-tags');
 		$item['title'] = 'Dollbooru | '.$item['postid'];
 		$item['content'] = '<a href="' . $item['uri'] . '"><img src="' . $thumbnailUri . '" /></a><br>Tags: '.$item['tags'];
