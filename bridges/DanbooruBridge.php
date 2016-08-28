ebe7084cc2f73cf068177a6ddb11e4776597c3d6
Pierre Mazière <pierre.maziere@gmx.com>
2016-08-28 13:48:26 +0200

    [DanbooruBridge] fix + code simplification
    Signed-off-by: Pierre Mazière <pierre.maziere@gmx.com>

--- bridges/DanbooruBridge.php
+++ bridges/DanbooruBridge.php
@@ -7,25 +7,25 @@ class DanbooruBridge extends BridgeAbstract{
 	public $description = "Returns images from given page";
 
     public $parameters = array( array(
-        'p'=>array('name'=>'page'),
+        'p'=>array(
+            'name'=>'page',
+            'type'=>'number'
+        ),
         't'=>array('name'=>'tags')
     ));
 
     public function collectData(){
-	$page = 1;$tags='';
-        if (isset($this->getInput('p'))) {
-            $page = (int)preg_replace("/[^0-9]/",'', $this->getInput('p'));
-        }
-        if (isset($this->getInput('t'))) {
-            $tags = urlencode($this->getInput('t'));
-        }
-        $html = $this->getSimpleHTMLDOM("http://donmai.us/posts?&page=$page&tags=$tags") or $this->returnServerError('Could not request Danbooru.');
+        $page = $this->getInput('p')?$this->getInput('p'):1;
+        $tags = urlencode($this->getInput('t'));
+
+        $html = $this->getSimpleHTMLDOM($this->uri."posts?&page=$page&tags=$tags")
+            or $this->returnServerError('Could not request Danbooru.');
 	foreach($html->find('div[id=posts] article') as $element) {
 		$item = array();
-		$item['uri'] = 'http://donmai.us'.$element->find('a', 0)->href;
+		$item['uri'] = $this->uri.$element->find('a', 0)->href;
 		$item['postid'] = (int)preg_replace("/[^0-9]/",'', $element->getAttribute('data-id'));
 		$item['timestamp'] = time();
-		$thumbnailUri = 'http://donmai.us'.$element->find('img', 0)->src;
+		$thumbnailUri = $this->uri.$element->find('img', 0)->src;
 		$item['tags'] = $element->find('img', 0)->getAttribute('alt');
 		$item['title'] = 'Danbooru | '.$item['postid'];
 		$item['content'] = '<a href="' . $item['uri'] . '"><img src="' . $thumbnailUri . '" /></a><br>Tags: '.$item['tags'];
