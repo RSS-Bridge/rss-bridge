2cda7edcbd200c39f9c7023969adf0aaab06fa5e
Pierre Mazière <pierre.maziere@gmx.com>
2016-08-30 00:13:57 +0200

    [XbooruBridge] code simplification
    Signed-off-by: Pierre Mazière <pierre.maziere@gmx.com>

--- bridges/XbooruBridge.php
+++ bridges/XbooruBridge.php
@@ -15,21 +15,16 @@ class XbooruBridge extends BridgeAbstract{
     ));
 
     public function collectData(){
-	$page = 0;$tags='';
-        if ($this->getInput('p')) {
-		$page = (int)preg_replace("/[^0-9]/",'', $this->getInput('p'));
-		$page = $page - 1;
-		$page = $page * 50;
-        }
-        if ($this->getInput('t')) {
-            $tags = urlencode($this->getInput('t'));
-        }
-        $html = $this->getSimpleHTMLDOM("http://xbooru.com/index.php?page=post&s=list&tags=$tags&pid=$page") or $this->returnServerError('Could not request Xbooru.');
+        $html = $this->getSimpleHTMLDOM(
+            $this->uri.'index.php?page=post&s=list&'
+            .'&pid='.($this->getInput('p')?($this->getInput('p') -1)*50:'')
+            .'&tags='.urlencode($this->getInput('t'))
+        ) or $this->returnServerError('Could not request Xbooru.');
 
 
 	foreach($html->find('div[class=content] span') as $element) {
 		$item = array();
-		$item['uri'] = 'http://xbooru.com/'.$element->find('a', 0)->href;
+		$item['uri'] = $this->uri.$element->find('a', 0)->href;
 		$item['postid'] = (int)preg_replace("/[^0-9]/",'', $element->getAttribute('id'));
 		$item['timestamp'] = time();
 		$thumbnailUri = $element->find('img', 0)->src;
