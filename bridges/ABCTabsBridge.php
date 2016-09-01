e5a375ab485d01d81c52fad547dad16aea7b1e4e
Pierre Mazière <pierre.maziere@gmx.com>
2016-09-01 23:47:26 +0200

    [ABCTabsBridge] code simplification
    Signed-off-by: Pierre Mazière <pierre.maziere@gmx.com>

--- bridges/ABCTabsBridge.php
+++ bridges/ABCTabsBridge.php
@@ -8,7 +8,7 @@ class ABCTabsBridge extends BridgeAbstract{
 
 	public function collectData(){
 		$html = '';
-        $html = $this->getSimpleHTMLDOM('http://www.abc-tabs.com/tablatures/nouveautes.html') or $this->returnClientError('No results for this query.');
+        $html = $this->getSimpleHTMLDOM(static::URI.'tablatures/nouveautes.html') or $this->returnClientError('No results for this query.');
 		$table = $html->find('table#myTable', 0)->children(1);
 
 		foreach ($table->find('tr') as $tab)
@@ -17,8 +17,8 @@ class ABCTabsBridge extends BridgeAbstract{
 		    $item['author'] = $tab->find('td', 1)->plaintext . ' - ' . $tab->find('td', 2)->plaintext;
 		    $item['title'] = $tab->find('td', 1)->plaintext . ' - ' . $tab->find('td', 2)->plaintext;
 		    $item['content'] = 'Le ' . $tab->find('td', 0)->plaintext . '<br> Par: ' . $tab->find('td', 5)->plaintext . '<br> Type: ' . $tab->find('td', 3)->plaintext;
-		    $item['id'] = 'http://www.abc-tabs.com' . $tab->find('td', 2)->find('a', 0)->getAttribute('href');
-		    $item['uri'] = 'http://www.abc-tabs.com' . $tab->find('td', 2)->find('a', 0)->getAttribute('href');
+		    $item['id'] = static::URI . $tab->find('td', 2)->find('a', 0)->getAttribute('href');
+		    $item['uri'] = static::URI . $tab->find('td', 2)->find('a', 0)->getAttribute('href');
 		    $this->items[] = $item;
 		}
     }
