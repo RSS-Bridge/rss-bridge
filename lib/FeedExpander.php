2744c13735b483c0048fbcf0e2642c04ffc730ec
Pierre Mazière <pierre.maziere@gmx.com>
2016-09-14 13:36:44 +0200

    [FeedExpander] fix feeds using guid tag as item uri provider
    Signed-off-by: Pierre Mazière <pierre.maziere@gmx.com>

--- lib/FeedExpander.php
+++ lib/FeedExpander.php
@@ -151,6 +151,15 @@ abstract class FeedExpander extends BridgeAbstract {
 		$namespaces = $feedItem->getNamespaces(true);
 		if(isset($namespaces['dc'])) $dc = $feedItem->children($namespaces['dc']);
 
+		if(isset($feedItem->guid)){
+			foreach($feedItem->guid->attributes() as $attribute=>$value){
+				if($attribute === 'isPermaLink' && $value === "true"){
+					$item['uri'] = $feedItem->guid;
+					break;
+				}
+			}
+		}
+
 		if(isset($feedItem->pubDate)){
 			$item['timestamp'] = strtotime($feedItem->pubDate);
 		} elseif(isset($dc->date)){
