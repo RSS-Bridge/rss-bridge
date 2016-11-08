2ff422d3122ebb2e7c69986a8d6e3c4636bbab51
Marcus Rohrmoser <mrohrmoser@acm.org>
2016-11-08 22:03:51 +0100

    Revive enclosures.Reverts 0663c95. Refs #198 #175.

--- formats/MrssFormat.php
+++ formats/MrssFormat.php
@@ -30,6 +30,9 @@ class MrssFormat extends FormatAbstract {
 			$itemUri = isset($item['uri']) ? $this->xml_encode($item['uri']) : '';
 			$itemTimestamp = isset($item['timestamp']) ? $this->xml_encode(date(DATE_RFC2822, $item['timestamp'])) : '';
 			$itemContent = isset($item['content']) ? $this->xml_encode($this->sanitizeHtml($item['content'])) : '';
+  		$entryEnclosures = "";
+  		foreach($item['enclosures'] as $enclosure)
+  			$entryEnclosures .= "<enclosure url=\"".xml_encode($enclosure)."\"/>";
 			$items .= <<<EOD
 
 	<item>
@@ -39,6 +42,7 @@ class MrssFormat extends FormatAbstract {
 		<pubDate>{$itemTimestamp}</pubDate>
 		<description>{$itemContent}</description>
 		<author>{$itemAuthor}</author>
+		{$entryEnclosures}
 	</item>
 
 EOD;
