8b2fdb3937aabb9d7031ad068a646c8e3b637265
Teromene <teromene@teromene.fr>
2017-03-03 14:13:29 +0000

    Add a function to convert the background-image attribute to an actual image.

--- lib/html.php
+++ lib/html.php
@@ -278,6 +278,25 @@ $keptText = array()){
 	return $htmlContent;
 }
 
+function backgroundToImg($htmlContent) {
+
+	$regex = '/background-image[ ]{0,}:[ ]{0,}url\([\'"]{0,}(.*?)[\'"]{0,}\)/';
+	$htmlContent = str_get_html($htmlContent);
+
+	foreach($htmlContent->find('*[!b38fd2b1fe7f4747d6b1c1254ccd055e]') as $element) {
+
+		if(preg_match($regex, $element->style, $matches) > 0) {
+
+			$element->outertext = '<img style="display:block;" src="' . $matches[1] . '" />';
+
+		}
+
+	}
+
+	return $htmlContent;
+
+}
+
 function defaultLinkTo($content, $server){
 	foreach($content->find('img') as $image){
 		if(strpos($image->src, 'http') === false
