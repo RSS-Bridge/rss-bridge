a8c23cccdad58594d170c57ae4eb0b9a533bd102
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2017-04-07 21:20:01 +0200

    Fixed sniff violations given by TravisCI

--- bridges/SexactuBridge.php
+++ bridges/SexactuBridge.php
@@ -7,18 +7,18 @@ class SexactuBridge extends BridgeAbstract {
 	const DOMAIN = 'http://www.gqmagazine.fr';
 	const CACHE_TIMEOUT = 7200; // 2h
 	const DESCRIPTION = 'Sexactu via rss-bridge';
-	
+
 	const REPLACED_ATTRIBUTES = array(
 			'href' => 'href',
 			'src' => 'src',
 			'data-original' => 'src'
 	);
-	
-	
+
+
 	public function getURI(){
 		return self::DOMAIN . '/sexactu';
 	}
-	
+
 	public function collectData(){
 		$html = getSimpleHTMLDOM($this->getURI())
 			or returnServerError('Could not request ' . $this->getURI());
@@ -27,7 +27,7 @@ class SexactuBridge extends BridgeAbstract {
 		$rowList = $sexactu->find('.row');
 		foreach($rowList as $row){
 			// only use first list as second one only contains pages numbers
-			
+
 			$title = $row->find('.title', 0);
 			if($title){
 				$item = array();
@@ -35,7 +35,7 @@ class SexactuBridge extends BridgeAbstract {
 				$item['title'] = $title->plaintext;
 				$urlAttribute = "data-href";
 				$uri = $title->$urlAttribute;
-				if($uri===false)
+				if($uri === false)
 					continue;
 				if(substr($uri, 0, 1) === 'h'){ // absolute uri
 					$item['uri'] = $uri;
@@ -46,7 +46,7 @@ class SexactuBridge extends BridgeAbstract {
 				}
 				$article = $this->loadFullArticle($item['uri']);
 				$item['content'] = $this->replaceUriInHtmlElement($article->find('.article_content', 0));
-				
+
 				$publicationDate = $article->find('time[itemprop=datePublished]', 0);
 				$short_date = $publicationDate->datetime;
 				$item['timestamp'] = date_parse($short_date);
@@ -57,7 +57,7 @@ class SexactuBridge extends BridgeAbstract {
 			$this->items[] = $item;
 		}
 	}
-	
+
 	/**
 	 * Loads the full article and returns the contents
 	 * @param $uri The article URI
@@ -65,7 +65,7 @@ class SexactuBridge extends BridgeAbstract {
 	 */
 	private function loadFullArticle($uri){
 		$html = getSimpleHTMLDOMCached($uri);
-		
+
 		$content = $html->find('#article', 0);
 		if($content){
 			return $content;
@@ -73,7 +73,7 @@ class SexactuBridge extends BridgeAbstract {
 		
 		return null;
 	}
-	
+
 	/**
 	 * Replaces all relative URIs with absolute ones
 	 * @param $element A simplehtmldom element
