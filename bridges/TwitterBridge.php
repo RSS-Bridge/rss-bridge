RSS-Bridge/rss-bridge

commit d81b61ccfa19e2c685df7acfb7c4306b7b775846

[TwitterBridge] Fix missing spaces
This commit improves readability of tweets by adding spaces before
anchors in the text.

- Hide "invisible" tags which were not rendered hidden because of
missing CSS.
- Fix spacing between anchors

Reported via #539

bridges/TwitterBridge.php
@@ -100,6 +100,11 @@ class TwitterBridge extends BridgeAbstract {
 				continue;
 			}
 
+			// remove 'invisible' content
+			foreach($tweet->find('.invisible') as $invisible){
+				$invisible->outertext = '';
+			}
+
 			$item = array();
 			// extract username and sanitize
 			$item['username'] = $tweet->getAttribute('data-screen-name');
@@ -116,13 +121,7 @@ class TwitterBridge extends BridgeAbstract {
 			// extract tweet timestamp
 			$item['timestamp'] = $tweet->find('span.js-short-timestamp', 0)->getAttribute('data-time');
 			// generate the title
-			$item['title'] = strip_tags(
-				html_entity_decode(
-					$tweet->find('p.js-tweet-text', 0)->innertext,
-					ENT_QUOTES,
-					'UTF-8'
-				)
-			);
+			$item['title'] = $this->fixAnchorSpacing(strip_tags($tweet->find('p.js-tweet-text', 0), '<a>'));
 
 			$this->processContentLinks($tweet);
 			$this->processEmojis($tweet);
@@ -133,6 +132,9 @@ class TwitterBridge extends BridgeAbstract {
 				'href="' . self::URI,
 				$tweet->find('p.js-tweet-text', 0)->innertext
 			);
+
+			// fix anchors missing spaces in-between
+			$cleanedTweet = $this->fixAnchorSpacing($cleanedTweet);
 
 			// Add picture to content
 			$picture_html = '';
@@ -243,6 +245,15 @@ EOD;
 			$link->removeAttribute('target');
 			$link->removeAttribute('title');
 		}
+	}
+
+	private function fixAnchorSpacing($content){
+		// fix anchors missing spaces in-between
+		return str_replace(
+			'<a',
+			' <a',
+			$content
+		);
 	}
 
 	private function getImageURI($tweet){

commit d7ff8b9ac77b694bb7212862993de4d7dc25d83c

[TwitterBridge] Fix title includes anchors in plaintext format
The title attribute includes tags (anchors) instead of raw text.
While this works fine in a browser, using a raw format like plain-
text or json breaks with expected behavior.

This commit changes the order in which functions are applied. By re-
moving anchors AFTER fixing the title, the final result does not
include tags and the title is still fixed.

This bug was introduced by d81b61ccfa19e2c685df7acfb7c4306b7b775846

References: #546

bridges/TwitterBridge.php
@@ -122,7 +122,7 @@ class TwitterBridge extends BridgeAbstract {
 			// extract tweet timestamp
 			$item['timestamp'] = $tweet->find('span.js-short-timestamp', 0)->getAttribute('data-time');
 			// generate the title
-			$item['title'] = $this->fixAnchorSpacing(strip_tags($tweet->find('p.js-tweet-text', 0), '<a>'));
+			$item['title'] = strip_tags($this->fixAnchorSpacing($tweet->find('p.js-tweet-text', 0), '<a>'));
 
 			$this->processContentLinks($tweet);
 			$this->processEmojis($tweet);