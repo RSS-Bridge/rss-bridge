19422c045da8cbb81e43efd79eccd2a913bd7ccd
Matt DeMoss <demoss.matt@gmail.com>
2018-01-02 15:54:44 -0500

    put list name up front

--- bridges/TwitterBridge.php
+++ bridges/TwitterBridge.php
@@ -82,7 +82,7 @@ class TwitterBridge extends BridgeAbstract {
 			break;
 		default: return parent::getName();
 		}
-		return 'Twitter ' . $specific . ' / ' .  $this->getInput($param);
+		return 'Twitter List ' . $this->getInput($param) . ' by ' $specific;
 	}
 
 	public function getURI(){
