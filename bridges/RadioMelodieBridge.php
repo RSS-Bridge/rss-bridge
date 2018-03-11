d0a66945646e2fd49a676ae3f62b08200e95f674
sysadminstory <sysadminstory@users.noreply.github.com>
2018-03-11 10:29:22 +0100

    [Radio Melodie] Coding policy fix

--- bridges/RadioMelodieBridge.php
+++ bridges/RadioMelodieBridge.php
@@ -12,12 +12,11 @@ class RadioMelodieBridge extends BridgeAbstract {
 		$html = getSimpleHTMLDOM(self::URI . 'actu')
 			or returnServerError('Could not request Radio Melodie.');
 		$list = $html->find('div[class=actuitem]');
-		foreach($list as $element)
-		{
+		foreach($list as $element) {
 			$item = array();
 
 			// Get picture URL
-			$pictureHTML =  $element->find('div[class=picture]');
+			$pictureHTML = $element->find('div[class=picture]');
 			preg_match(
 				'/background-image:url\((.*)\);/',
 				$pictureHTML[0]->getAttribute('style'),
@@ -26,8 +25,8 @@ class RadioMelodieBridge extends BridgeAbstract {
 
 			$item['enclosures'] = array($pictureURL);
 			$item['uri'] = SELF::URI . $element->parent()->href;
-			$item['title'] = $element->find('h3',0)->plaintext;
-			$item['content'] = $element->find('p',0)->plaintext . '<br/><img src="'.$pictureURL.'"/>';
+			$item['title'] = $element->find('h3', 0)->plaintext;
+			$item['content'] = $element->find('p', 0)->plaintext . '<br/><img src="'.$pictureURL.'"/>';
 			$this->items[] = $item;
 		}
 	}
