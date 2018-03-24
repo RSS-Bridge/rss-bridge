3daa02f4605f18c014bf780ea3b138153f0c4071
logmanoriginal <logmanoriginal@users.noreply.github.com>
2018-03-24 21:33:24 +0100

    config: Fix typos

--- config.default.ini.php
+++ config.default.ini.php
@@ -1,24 +1,24 @@
 ; <?php exit; ?> DO NOT REMOVE THIS LINE
 
 ; This file contains the default settings for RSS-Bridge. Do not change this
-; file, it will be replaced on the next update of RSS-Bridge. You can specify
-; your own configuration in 'config.ini.php' (copy of this file).
+; file, it will be replaced on the next update of RSS-Bridge! You can specify
+; your own configuration in 'config.ini.php' (copy this file).
 
 [cache]
 
-; Allow users to specify a custom timeout for specific requests.
+; Allow users to specify custom timeout for specific requests.
 ; true  = enabled
 ; false = disabled (default)
 custom_timeout = false
 
 [proxy]
 
-; Sets the prox url (i.e. "tcp://192.168.0.0:32")
+; Sets the proxy url (i.e. "tcp://192.168.0.0:32")
 ; ""    = Proxy disabled (default)
 url = ""
 
 ; Sets the proxy name that is shown on the bridge instead of the proxy url.
-; ""    = Show prox url
+; ""    = Show proxy url
 name = "Hidden proxy name"
 
 ; Allow users to disable proxy usage for specific requests.
