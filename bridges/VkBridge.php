45c3dcb636e9e63ab17a405491a234b74df7af9b
logmanoriginal <logmanoriginal@users.noreply.github.com>
2018-04-06 20:24:43 +0200

    [VkBridge] Simplify header specification

--- bridges/VkBridge.php
+++ bridges/VkBridge.php
@@ -109,7 +109,7 @@ class VkBridge extends BridgeAbstract
 	{
 		ini_set('user-agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0');
 
-		$header = array("Accept-language: en\r\nCookie: remixlang=3\r\n");
+		$header = array('Accept-language: en', 'Cookie: remixlang=3');
 
 		return getContents($this->getURI(), $header);
 	}
