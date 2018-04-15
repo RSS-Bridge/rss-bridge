208fff801dc42f47c793d9c72e98a9782064a45e
Mitsukarenai <mitsu@suumitsu.eu>
2018-04-15 13:21:48 +0200

    [FDroid] minor fixes for Travis CI

--- bridges/FDroidBridge.php
+++ bridges/FDroidBridge.php
@@ -4,7 +4,7 @@ class FDroidBridge extends BridgeAbstract {
 	const MAINTAINER = 'Mitsukarenai';
 	const NAME = 'F-Droid Bridge';
 	const URI = 'https://f-droid.org/';
-	const CACHE_TIMEOUT = 60*60*2; // 2 hours
+	const CACHE_TIMEOUT = 60 * 60 * 2; // 2 hours
 	const DESCRIPTION = 'Returns latest added/updated apps on the open-source Android apps repository F-Droid';
 
 	const PARAMETERS = array( array(
@@ -25,7 +25,7 @@ class FDroidBridge extends BridgeAbstract {
 			or returnServerError('Could not request F-Droid.');
 
 		// targetting the corresponding widget based on user selection
-		// "updated" is the 4th widget on the page, "added" is the 5th  ← this is pretty sensitive to them adding/shuffling around website widgets
+		// "updated" is the 4th widget on the page, "added" is the 5th
 
 		switch($this->getInput('u')) {
 			case 'updated':
@@ -44,7 +44,10 @@ class FDroidBridge extends BridgeAbstract {
 				$item['title'] = $element->find('h4', 0)->plaintext;
 				$item['icon'] = $element->find('img', 0)->src;
 				$item['summary'] = $element->find('span.package-summary', 0)->plaintext;
-				$item['content'] = '<a href="'.$item['uri'].'"><img alt="" style="max-height:128px" src="'.$item['icon'].'"></a><br>'.$item['summary'];
+				$item['content'] = '
+					<a href="'.$item['uri'].'">
+						<img alt="" style="max-height:128px" src="'.$item['icon'].'">
+					</a><br>'.$item['summary'];
 				$this->items[] = $item;
 		}
 	}
