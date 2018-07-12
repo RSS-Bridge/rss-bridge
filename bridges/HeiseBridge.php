8b65c98bcae5bc7fde59f53c8aca36f0658d8a76
Dreckiger-Dan <Dreckiger-Dan@users.noreply.github.com>
2018-07-13 00:54:42 +0200

    changed urls to https, remove tags/ads, set author

--- bridges/HeiseBridge.php
+++ bridges/HeiseBridge.php
@@ -4,7 +4,7 @@ class HeiseBridge extends FeedExpander {
     const MAINTAINER = 'Dreckiger-Dan';
     const NAME = 'Heise Online Bridge';
     const URI = 'https://heise.de/';
-	const CACHE_TIMEOUT = 1800; // 30min
+    const CACHE_TIMEOUT = 1800; // 30min
     const DESCRIPTION = 'Returns the full articles instead of only the intro';
     const PARAMETERS = array(array(
         'category' => array(
@@ -12,25 +12,33 @@ class HeiseBridge extends FeedExpander {
             'type' => 'list',
             'required' => true,
             'values' => array(
-                'Alle News' => 'www.heise.de/newsticker/heise-atom.xml',
-                'Top-News' => 'www.heise.de/newsticker/heise-top-atom.xml',
+                'Alle News' => 'https://www.heise.de/newsticker/heise-atom.xml',
+                'Top-News' => 'https://www.heise.de/newsticker/heise-top-atom.xml',
                 'Internet-Störungen' => 'https://www.heise.de/netze/netzwerk-tools/imonitor-internet-stoerungen/feed/aktuelle-meldungen/',
-                'Alle News von heise Developer' => 'www.heise.de/developer/rss/news-atom.xml'
+                'Alle News von heise Developer' => 'https://www.heise.de/developer/rss/news-atom.xml'
             )
         )
     ));
 
     public function collectData() {
-        $this->collectExpandableDatas($this->getInput('category')) or returnServerError('Error while downloading the website content');
+        //ini_set('memory_limit', '-1'); //dirty workaround for memory limitation
+        $this->collectExpandableDatas($this->getInput('category')); // or returnServerError('Error while downloading the website content');
     }
 
     protected function parseItem($feedItem) {
         $item = parent::parseItem($feedItem);
-        
+
         $article = getSimpleHTMLDOMCached($item['uri']) or returnServerError('Could not open article: ' . $url);
-        $article = $article->find('div.article-content', 0)->innertext;
-        
-        $item['content'] = $article;
+        $author = $article->find('span.ISI_IGNORE', 0);
+
+        //remove ads
+        foreach ($article->find('section.widget-werbung') as &$ad) {
+            $ad = '';
+        }
+        $article = $article->find('div.article-content', 0);
+
+        $item['author'] = $author;
+        $item['content'] = strip_tags($article, '<span><p><a><br><h1><h2><h3><img><table><tbody><tr><td>');
 
         return $item;
     }
