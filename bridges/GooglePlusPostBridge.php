fb79a6737000d9838aacbb4379b37b462671f260
logmanoriginal <logmanoriginal@users.noreply.github.com>
2018-07-31 18:25:01 +0200

    [GooglePlusPostBridge] Normalize static::URI usage
    This commit fixes a few things related to static::URI
    
    1) Remove trailing slash from the URI to simplify using 'defaultLinkTo'
    2) Use static::URI instead of self::URI for consistency
    3) Remove custom implementation of 'defaultLinkTo'

--- bridges/GooglePlusPostBridge.php
+++ bridges/GooglePlusPostBridge.php
@@ -6,7 +6,7 @@ class GooglePlusPostBridge extends BridgeAbstract{
 
 	const MAINTAINER = 'Grummfy, logmanoriginal';
 	const NAME = 'Google Plus Post Bridge';
-	const URI = 'https://plus.google.com/';
+	const URI = 'https://plus.google.com';
 	const CACHE_TIMEOUT = 600; //10min
 	const DESCRIPTION = 'Returns user public post (without API).';
 
@@ -26,8 +26,10 @@ class GooglePlusPostBridge extends BridgeAbstract{
 		}
 
 		// get content parsed
-		$html = getSimpleHTMLDOMCached(self::URI . urlencode($username) . '/posts')
+		$html = getSimpleHTMLDOMCached(static::URI . '/' . urlencode($username) . '/posts')
 			or returnServerError('No results for this query.');
+
+		$html = defaultLinkTo($html, static::URI);
 
 		// get title, url, ... there is a lot of intresting stuff in meta
 		$this->_title = $html->find('meta[property=og:title]', 0)->getAttribute('content');
@@ -40,7 +42,7 @@ class GooglePlusPostBridge extends BridgeAbstract{
 			$item['author'] = $item['fullname'] = $post->find('div div div div a', 0)->innertext;
 			$item['id'] = $post->find('div div div', 0)->getAttribute('id');
 			$item['avatar'] = $post->find('div img', 0)->src;
-			$item['uri'] = self::URI . $post->find('div div div a', 1)->href;
+			$item['uri'] = $post->find('div div div a', 1)->href;
 
 			$timestamp = $post->find('a.qXj2He span', 0);
 
@@ -51,17 +53,12 @@ class GooglePlusPostBridge extends BridgeAbstract{
 						$timestamp->getAttribute('aria-label')));
 			}
 
-			// hashtag to treat : https://plus.google.com/explore/tag
-			// $hashtags = array();
-			// foreach($post->find('a.d-s') as $hashtag){
-			// 	$hashtags[trim($hashtag->plaintext)] = self::URI . $hashtag->href;
-			// }
-
 			$item['content'] = '';
 
 			// avatar display
 			$item['content'] .= '<div style="float:left; margin: 0 0.5em 0.5em 0;"><a href="'
-			. self::URI
+			. static::URI
+			. '/'
 			. urlencode($this->getInput('username'));
 
 			$item['content'] .= '"><img align="top" alt="'
@@ -75,23 +72,6 @@ class GooglePlusPostBridge extends BridgeAbstract{
 			$item['content_simple'] = $content->plaintext;
 			$item['title'] = substr($item['content_simple'], 0, 72) . '...';
 
-			// XXX ugly but I don't have any idea how to do a better stuff,
-			// str_replace on link doesn't work as expected and ask too many checks
-			foreach($content->find('a') as $link) {
-				$hasHttp = strpos($link->href, 'http');
-				$hasDoubleSlash = strpos($link->href, '//');
-
-				if((!$hasHttp && !$hasDoubleSlash)
-				|| (false !== $hasHttp && strpos($link->href, 'http') != 0)
-				|| (false === $hasHttp && false !== $hasDoubleSlash && $hasDoubleSlash != 0)) {
-					// skipp bad link, for some hashtag or other stuff
-					if(strpos($link->href, '/') == 0) {
-						$link->href = substr($link->href, 1);
-					}
-
-					$link->href = self::URI . $link->href;
-				}
-			}
 			$content = $content->innertext;
 
 			$item['content'] .= '<div style="margin-top: -1.5em">' .  $content . '</div>';
