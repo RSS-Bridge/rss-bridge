bc8c2dc185ee1d885a9f3fdce835f9e050c88cbb
Monsieur Poutounours <30292704+MonsieurPoutounours@users.noreply.github.com>
2018-09-07 17:53:50 +0200

    Adding a bridge for theyetee.com
    The bridge fetches daily shirts at theyetee.com.
    The Yetee offers two new shirts each day, but you can buy them only for a few hours !
    Unfortunately, the site don't provide RSS feed, so the only way to keep up to date on new shirt is their daily mailing ... until now !

---
+++ bridges/TheYeteeBridge.php
@@ -0,0 +1,47 @@
+<?php
+class TheYeteeBridge extends BridgeAbstract {
+	
+	const MAINTAINER = 'Monsieur Poutounours';
+	const NAME = 'TheYetee';
+	const URI = 'https://theyetee.com';
+	const CACHE_TIMEOUT = 14400; // 4 h 
+	const DESCRIPTION = 'Fetch daily shirts from The Yetee';
+	
+	public function collectData(){
+		
+		$html = getSimpleHTMLDOM(self::URI)
+			or returnServerError('Could not request The Yetee.');
+			
+		$div = $html->find('div');
+		foreach($div as $element) {
+			
+			if($element->class === 'hero-col')
+			{
+				//echo $element;
+				
+				$item = array();
+				
+				$title = $element->find('h2',0)->plaintext;
+				$item['title'] = $title;
+				
+				$author = trim($element->find('div[class=credit]',0)->plaintext);
+				$item['author'] = $author;
+				
+				$uri = $element->find('div[class=controls] a',0)->href;
+				$item['uri'] = static::URI.$uri;
+				
+				$content = "<p>".$element->find('section[class=product-listing-info] p',-1)->plaintext."</p>";
+				$photos = $element->find('a[class=js-modaal-gallery] img');
+				foreach($photos as $photo)
+				{
+					$content = $content."<br /><img src='".$photo->src."' />";
+				}
+				$item['content'] = $content;
+				
+				$this->items[] = $item;	
+			} 
+			
+			
+		}
+	}
+}
