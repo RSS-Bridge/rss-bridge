f4c04e49ab0dbfbe529e43c6c9a33290646dc5e5
La Bécasse <maxime@buron.coffee>
2018-09-11 22:11:04 +0200

    tabs correction !

--- bridges/Arte7Bridge.php
+++ bridges/Arte7Bridge.php
@@ -30,8 +30,8 @@ class Arte7Bridge extends BridgeAbstract {
 		),
 		'Collection (Français)' => array(
 			'colfr' => array(
-                'name' => 'Collection id (ex. RC-014095 pour https://www.arte.tv/fr/videos/RC-014095/blow-up/)',
-                'required' => true
+				'name' => 'Collection id (ex. RC-014095 pour https://www.arte.tv/fr/videos/RC-014095/blow-up/)',
+				'required' => true
 			)
 		),
 		'Catégorie (Allemand)' => array(
@@ -54,8 +54,8 @@ class Arte7Bridge extends BridgeAbstract {
 		),
 		'Collection (Allemand)' => array(
 			'colde' => array(
-                'name' => 'Collection id (ex. RC-014095 pour https://www.arte.tv/de/videos/RC-014095/blow-up/)',
-                'required' => true
+				'name' => 'Collection id (ex. RC-014095 pour https://www.arte.tv/de/videos/RC-014095/blow-up/)',
+				'required' => true
 			)
 		)
 	);
@@ -66,24 +66,24 @@ class Arte7Bridge extends BridgeAbstract {
 			$category = $this->getInput('catfr');
 			$lang = 'fr';
 			break;
-        case 'Collection (Français)':
+		case 'Collection (Français)':
 			$lang = 'fr';
-            $collectionId = $this->getInput('colfr');
+			$collectionId = $this->getInput('colfr');
 			break;
 		case 'Catégorie (Allemand)':
 			$category = $this->getInput('catde');
 			$lang = 'de';
 			break;
-        case 'Collection (Allemand)':
+		case 'Collection (Allemand)':
 			$lang = 'de';
-            $collectionId = $this->getInput('colde');
+			$collectionId = $this->getInput('colde');
 			break;
 		}
 
 		$url = 'https://api.arte.tv/api/opa/v3/videos?sort=-lastModified&limit=10&language='
 			. $lang
 			. ($category != null ? '&category.code=' . $category : '')
-            . ($collectionId !=null ? '&collections.collectionId=' . $collectionId : '');
+			 . ($collectionId !=null ? '&collections.collectionId=' . $collectionId : '');
 
 		$header = array(
 			'Authorization: Bearer ' . self::API_TOKEN
