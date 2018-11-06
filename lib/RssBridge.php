4bf45df18e6d5d537dcf4867414c373dbd9d97c1
logmanoriginal <logmanoriginal@users.noreply.github.com>
2018-11-06 18:29:00 +0100

    [RssBridge] Simplify documentation for this file
    - Remove file documentation and license remark (defined in repository
    scope - see README / UNLICENSE)
    
    - Remove example usage (if necessary should be included in the Wiki)

--- lib/RssBridge.php
+++ lib/RssBridge.php
@@ -1,13 +1,9 @@
 <?php
-/* rss-bridge library.
-Foundation functions for rss-bridge project.
-See https://github.com/sebsauvage/rss-bridge
-Licence: Public domain.
-*/
 
-define('PATH_VENDOR', __DIR__ . '/../vendor');
-define('PATH_LIB', __DIR__ . '/../lib');
+define('PATH_VENDOR', __DIR__ . '/../vendor'); // Path for vendor library
+define('PATH_LIB', __DIR__ . '/../lib'); // Path for core library
 
+// Classes
 require_once PATH_LIB . '/Exceptions.php';
 require_once PATH_LIB . '/Format.php';
 require_once PATH_LIB . '/FormatAbstract.php';
@@ -21,32 +17,11 @@ require_once PATH_LIB . '/BridgeCard.php';
 require_once PATH_LIB . '/BridgeList.php';
 require_once PATH_LIB . '/ParameterValidator.php';
 
+// Functions
 require_once PATH_LIB . '/html.php';
 require_once PATH_LIB . '/error.php';
 require_once PATH_LIB . '/contents.php';
 
+// Vendor
 require_once PATH_VENDOR . '/simplehtmldom/simple_html_dom.php';
 require_once PATH_VENDOR . '/php-urljoin/src/urljoin.php';
-
-/* Example use
-
-	require_once __DIR__ . '/lib/RssBridge.php';
-
-	// Data retrieval
-	Bridge::setDir(__DIR__ . '/bridges/');
-	$bridge = Bridge::create('GoogleSearch');
-	$bridge->collectData($_REQUEST);
-
-	// Data transformation
-	Format::setDir(__DIR__ . '/formats/');
-	$format = Format::create('Atom');
-	$format
-		->setItems($bridge->getItems())
-		->setExtraInfos(array(
-			'name' => $bridge->getName(),
-			'uri' => $bridge->getURI(),
-			'icon' => $bridge->getIcon(),
-		))
-		->display();
-
-*/
