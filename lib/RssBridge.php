bfae04d1fe01e2c80bf0d46cad3aab7f43fbcf7a
logmanoriginal <logmanoriginal@users.noreply.github.com>
2018-11-06 18:08:53 +0100

    [RssBridge] Include __DIR__ in PATH_VENDOR

--- lib/RssBridge.php
+++ lib/RssBridge.php
@@ -5,7 +5,7 @@ See https://github.com/sebsauvage/rss-bridge
 Licence: Public domain.
 */
 
-define('PATH_VENDOR', '/../vendor');
+define('PATH_VENDOR', __DIR__ . '/../vendor');
 
 require __DIR__ . '/Exceptions.php';
 require __DIR__ . '/Format.php';
@@ -24,7 +24,7 @@ require __DIR__ . '/html.php';
 require __DIR__ . '/error.php';
 require __DIR__ . '/contents.php';
 
-$vendorLibSimpleHtmlDom = __DIR__ . PATH_VENDOR . '/simplehtmldom/simple_html_dom.php';
+$vendorLibSimpleHtmlDom = PATH_VENDOR . '/simplehtmldom/simple_html_dom.php';
 if(!file_exists($vendorLibSimpleHtmlDom)) {
 	throw new \HttpException('"PHP Simple HTML DOM Parser" library is missing.
  Get it from http://simplehtmldom.sourceforge.net and place the script "simple_html_dom.php" in '
@@ -34,7 +34,7 @@ if(!file_exists($vendorLibSimpleHtmlDom)) {
 }
 require_once $vendorLibSimpleHtmlDom;
 
-$vendorLibPhpUrlJoin = __DIR__ . PATH_VENDOR . '/php-urljoin/src/urljoin.php';
+$vendorLibPhpUrlJoin = PATH_VENDOR . '/php-urljoin/src/urljoin.php';
 if(!file_exists($vendorLibPhpUrlJoin)) {
 	throw new \HttpException('"php-urljoin" library is missing.
  Get it from https://github.com/fluffy-critter/php-urljoin and place the script "urljoin.php" in '
