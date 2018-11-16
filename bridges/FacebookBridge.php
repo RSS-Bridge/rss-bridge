38068950596a866c188f2d7dd06ba2875467e9fc
triatic <42704418+triatic@users.noreply.github.com>
2018-11-16 14:33:54 +0000

    [FacebookBridge] Improve titles (#924)
    A slightly improved version of #454 and #468 . Build titles from content rather than author + pre-content (which doesn't reflect anything useful).

--- bridges/FacebookBridge.php
+++ bridges/FacebookBridge.php
@@ -659,14 +659,8 @@ EOD;
 							$date = 0;
 						}
 
-						// Build title from username and content
-						$title = $author;
-
-						if(strlen($title) > 24)
-							$title = substr($title, 0, strpos(wordwrap($title, 24), "\n")) . '...';
-
-						$title = $title . ' | ' . strip_tags($content);
-
+						// Build title from content
+						$title = strip_tags($post->find('.userContent', 0)->innertext);
 						if(strlen($title) > 64)
 							$title = substr($title, 0, strpos(wordwrap($title, 64), "\n")) . '...';
 
