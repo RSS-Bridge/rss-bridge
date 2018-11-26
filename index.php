6c107c0c0023bc5ecc5f0283b157be13eb458fbf
triatic <42704418+triatic@users.noreply.github.com>
2018-11-26 17:24:53 +0000

    Updated to requested changes

--- index.php
+++ index.php
@@ -224,11 +224,8 @@ try {
 					$item['title'] = 'Bridge returned error ' . $e->getCode() . '! (' . $params['_error_time'] . ')';
 				}
 
-				if (isset($_SERVER['REQUEST_URI']))
-					$item['uri'] = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-				else
-					$item['uri'] = '';
-				$item['uri'] .= '?' . http_build_query($params);
+				$item['uri'] = (isset($_SERVER['REQUEST_URI']) ? parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) : '')
+				. '?' . http_build_query($params);
 				$item['timestamp'] = time();
 				$item['content'] = buildBridgeException($e, $bridge);
 
@@ -241,11 +238,8 @@ try {
 				// Create "new" error message every 24 hours
 				$params['_error_time'] = urlencode((int)(time() / 86400));
 
-				if (isset($_SERVER['REQUEST_URI']))
-					$item['uri'] = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-				else
-					$item['uri'] = '';
-				$item['uri'] .= '?' . http_build_query($params);
+				$item['uri'] = (isset($_SERVER['REQUEST_URI']) ? parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) : '')
+				. '?' . http_build_query($params);
 				$item['title'] = 'Bridge returned error ' . $e->getCode() . '! (' . $params['_error_time'] . ')';
 				$item['timestamp'] = time();
 				$item['content'] = buildBridgeException($e, $bridge);
