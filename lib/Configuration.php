2c44f432b4f88134ca802fa758f9cfa77f66faac
triatic <42704418+triatic@users.noreply.github.com>
2018-12-26 20:42:39 +0000

    Line length fix

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -96,7 +96,8 @@ final class Configuration {
 			die('"simplexml" extension not loaded. Please check "php.ini"');
 
 		// Allow RSS-Bridge to run without curl module in CLI mode without root certificates
-		if(!extension_loaded('curl') && !(php_sapi_name() === 'cli' && (ini_get('curl.cainfo') === '' || ini_get('curl.cainfo') === false)))
+		if(!extension_loaded('curl') && !(php_sapi_name() === 'cli' && (ini_get('curl.cainfo') === ''
+		|| ini_get('curl.cainfo') === false)))
 			die('"curl" extension not loaded. Please check "php.ini"');
 
 		if(!extension_loaded('json'))
