bb9c16415213af28d8d12d283da9342737892ad5
fulmeek <36341513+fulmeek@users.noreply.github.com>
2019-01-12 19:54:16 +0100

    [CacheImplementationTest] added unit tests for cache implementations

---
+++ tests/CacheImplementationTest.php
@@ -0,0 +1,44 @@
+<?php
+require_once __DIR__ . '/../lib/rssbridge.php';
+
+use PHPUnit\Framework\TestCase;
+
+class CacheImplementationTest extends TestCase {
+	private $class;
+	private $obj;
+
+	/**
+	 * @dataProvider dataCachesProvider
+	 */
+	public function testClassName($path) {
+		$this->setCache($path);
+		$this->assertTrue($this->class === ucfirst($this->class), 'class name must start with uppercase character');
+		$this->assertEquals(0, substr_count($this->class, ' '), 'class name must not contain spaces');
+		$this->assertStringEndsWith('Cache', $this->class, 'class name must end with "Cache"');
+	}
+
+	/**
+	 * @dataProvider dataCachesProvider
+	 */
+	public function testClassType($path) {
+		$this->setCache($path);
+		$this->assertInstanceOf(CacheInterface::class, $this->obj);
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+
+	public function dataCachesProvider() {
+		$bridges = array();
+		foreach (glob(PATH_LIB_CACHES . '*.php') as $path) {
+			$bridges[basename($path, '.php')] = array($path);
+		}
+		return $bridges;
+	}
+
+	private function setCache($path) {
+		require_once $path;
+		$this->class = basename($path, '.php');
+		$this->assertTrue(class_exists($this->class), 'class ' . $this->class . ' doesn\'t exist');
+		$this->obj = new $this->class();
+	}
+}
