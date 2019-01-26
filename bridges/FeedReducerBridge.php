158325cdf439c305aac98d4b332118938cb25e6b
Matt DeMoss <demoss.matt@gmail.com>
2019-01-25 21:55:16 -0500

    phpcs on reducer bridge

--- bridges/FeedReducerBridge.php
+++ bridges/FeedReducerBridge.php
@@ -18,43 +18,43 @@ class FeedReducerBridge extends FeedExpander {
 		)
 	));
 	const CACHE_TIMEOUT = 3600;
-    
+
 	public function collectData(){
-		if((preg_match('#^http(s?)://#i', $this->getInput('url')) === 1)){ 
+		if((preg_match('#^http(s?)://#i', $this->getInput('url')) === 1)) {
 			$this->collectExpandableDatas($this->getInput('url'));
 		} else {
 			throw new Exception('URI must begin with http(s)://');
 		}
 	}
-	
+
 	public function getItems(){
 		$filteredItems = array();
 		$intPercentage = (int)preg_replace('/[^0-9]/', '', $this->getInput('percentage'));
-		 
+
 		foreach ($this->items as $thisItem) {
 			// Because the URL is included in the hash:
 			//  - so you can change the output by adding a local-part to the URL
 			//  - so items with the same URI in different feeds won't be correlated
-			
+
 			// $pseudoRandomInteger will be a 16 bit unsigned int mod 100.
 			// This won't be exact, but should be close enough.
-			
+
 			$pseudoRandomInteger = unpack(
 				'S', // unsigned 16-bit int
-				hash( 'sha256', $thisItem['uri'] . '::' . $this->getInput('url'), TRUE )
+				hash( 'sha256', $thisItem['uri'] . '::' . $this->getInput('url'), true )
 			)[1] % 100;
-			
-			if ( $pseudoRandomInteger < $intPercentage ){
+
+			if ($pseudoRandomInteger < $intPercentage) {
 				$filteredItems[] = $thisItem;
 			}
 		}
-		
+
 		return $filteredItems;
 	}
-	
+
 	public function getName(){
 		$trimmedPercentage = preg_replace('/[^0-9]/', '', $this->getInput('percentage'));
 		return parent::getName() . ' [' . $trimmedPercentage . '%]';
 	}
-    
+
 }
