69954d94c272e67ab7fbd5e3cad700dde4c8f151
Klimplant <klimplant@disroot.org>
2019-02-09 19:21:26 +0100

    [CachetBridge] Fixing issue with CachetAPI Pagination
    Fixing issue that only the oldest 20 entries were shown.
    
    _Background:_
    
    _Cachet has a, lets call it odd, system of pagination. On the first page you see the incidents first created, so they are not what you want to see. But on the last page you can have 1 or 20 of the newest incidents. So you have to take the incidents from the last page (call it Pmax) and combine them with the incidents from  Pmax - 1._

--- bridges/CachetBridge.php
+++ bridges/CachetBridge.php
@@ -20,7 +20,7 @@ class CachetBridge extends BridgeAbstract {
 	private $componentCache = [];
 
 	public function getURI() {
-		return $this->getInput('host') === null ? 'https://demo.cachethq.io/' : $this->getInput('host');
+		return $this->getInput('host') === null ? 'https://cachethq.io/' : $this->getInput('host');
 	}
 
 	/**
@@ -70,41 +70,72 @@ class CachetBridge extends BridgeAbstract {
 	}
 
 	public function collectData() {
-		$ping = getContents($this->getURI() . '/api/v1/ping');
+		$ping = getContents(urljoin($this->getURI(), '/api/v1/ping'));
 		if (!$this->validatePing($ping)) {
 			returnClientError('Provided URI is invalid!');
 		}
 
-		$incidents = getContents($this->getURI() . '/api/v1/incidents');
+		$url = urljoin($this->getURI(), '/api/v1/incidents');
+		$incidents = getContents($url);
 		if (!$this->isValidJSON($incidents)) {
 			returnClientError('/api/v1/incidents returned no valid json');
 		}
 
 		$incidents = json_decode($incidents);
 
-		usort($incidents->data, function ($a, $b) {
-			$timeA = strtotime($a->updated_at);
-			$timeB = strtotime($b->updated_at);
-			return $timeA > $timeB ? -1 : 1;
-		});
+		$maxPage = $incidents->meta->pagination->total_pages;
+		$minPage = $maxPage > 1 ? $maxPage - 1 : 1;
+		for ($p = $maxPage; $p >= $minPage; $p--) {
+			if ($p !== 1) {
+				$url = urljoin($this->getURI(), '/api/v1/incidents?page=' . $p);
+				$incidents = getContents($url);
+				if (!$this->isValidJSON($incidents)) {
+					returnClientError('/api/v1/incidents returned no valid json');
+				}
 
-		foreach ($incidents->data as $incident) {
-			$componentName = $this->getComponentName($incident->component_id);
-			$content = str_replace("\r\n", "\n<br>", $incident->message);
-			$uidOrig = $incident->permalink . $incident->created_at;
-			$uid = hash('sha512', $uidOrig);
-			$item = [];
-			$item['uri'] = $incident->permalink;
-			$item['title'] = $incident->name;
-			$item['timestamp'] = strtotime($incident->created_at);
-			$item['content'] = $content;
-			if ($componentName !== '') {
-				$item['categories'] = [
-					$componentName
-				];
+				$incidents = json_decode($incidents);
 			}
-			$item['uid'] = $uid;
-			$this->items[] = $item;
+
+			usort($incidents->data, function ($a, $b) {
+				$timeA = strtotime($a->updated_at);
+				$timeB = strtotime($b->updated_at);
+				return $timeA > $timeB ? -1 : 1;
+			});
+
+			foreach ($incidents->data as $incident) {
+
+				if (isset($incident->permalink)) {
+					$permalink = $incident->permalink;
+				} else {
+					$permalink = urljoin($this->getURI(), '/incident/' . $incident->id);
+				}
+
+				$title = $incident->human_status . ': ' . $incident->name;
+				$content = str_replace("\r\n", "\n<br>", $incident->message);
+				$componentName = $this->getComponentName($incident->component_id);
+				$uidOrig = $permalink . $incident->created_at;
+				$uid = hash('sha512', $uidOrig);
+				$timestamp = strtotime($incident->created_at);
+				$categories = [];
+				$categories[] = $incident->human_status;
+				if ($componentName !== '') {
+					$categories[] = $componentName;
+				}
+
+				$item = [];
+				$item['uri'] = $permalink;
+				$item['title'] = $title;
+				$item['timestamp'] = $timestamp;
+				$item['content'] = $content;
+				$item['uid'] = $uid;
+				$item['categories'] = $categories;
+
+				$this->items[] = $item;
+				if (count($this->items) === 20) {
+					break;
+				}
+			}
 		}
+
 	}
 }
