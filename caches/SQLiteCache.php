5ca6ae922d4d6fd3f680d2831258aabb32196d28
fulmeek <36341513+fulmeek@users.noreply.github.com>
2019-02-14 22:23:48 +0100

    [SQLiteCache] Implemented cache based on SQLite 3

---
+++ caches/SQLiteCache.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Cache based on SQLite 3 <https://www.sqlite.org>
+ */
+class SQLiteCache implements CacheInterface {
+	protected $path;
+	protected $param;
+
+	private $db = null;
+
+	public function loadData(){
+		$this->initDB();
+
+		$Qselect = $this->db->prepare('SELECT value FROM storage WHERE key = :key');
+		$Qselect->bindValue(':key', $this->getCacheKey());
+		$result = $Qselect->execute();
+		if ($result instanceof SQLite3Result) {
+			$data = $result->fetchArray(SQLITE3_ASSOC);
+			if (isset($data['value'])) {
+				return unserialize($data['value']);
+			}
+		}
+
+		return null;
+	}
+
+	public function saveData($datas){
+		$this->initDB();
+
+		$Qupdate = $this->db->prepare('INSERT OR REPLACE INTO storage (key, value, updated) VALUES (:key, :value, :updated)');
+		$Qupdate->bindValue(':key', $this->getCacheKey());
+		$Qupdate->bindValue(':value', serialize($datas));
+		$Qupdate->bindValue(':updated', time());
+		$Qupdate->execute();
+
+		return $this;
+	}
+
+	public function getTime(){
+		$this->initDB();
+
+		$Qselect = $this->db->prepare('SELECT updated FROM storage WHERE key = :key');
+		$Qselect->bindValue(':key', $this->getCacheKey());
+		$result = $Qselect->execute();
+		if ($result instanceof SQLite3Result) {
+			$data = $result->fetchArray(SQLITE3_ASSOC);
+			if (isset($data['updated'])) {
+				return $data['updated'];
+			}
+		}
+
+		return false;
+	}
+
+	public function purgeCache($duration){
+		$this->initDB();
+
+		$Qdelete = $this->db->prepare('DELETE FROM storage WHERE updated < :expired');
+		$Qdelete->bindValue(':expired', time() - $duration);
+		$Qdelete->execute();
+	}
+
+	/**
+	* Set cache path
+	* @return self
+	*/
+	public function setPath($path){
+		$this->path = $path;
+		return $this;
+	}
+
+	/**
+	* Set HTTP GET parameters
+	* @return self
+	*/
+	public function setParameters(array $param){
+		$this->param = array_map('strtolower', $param);
+		return $this;
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+
+	protected function initDB() {
+		// initialize on first use
+		if ($this->db !== null) return;
+
+		$file = PATH_CACHE . 'cache.sqlite';
+
+		if (!is_file($file)) {
+			$this->db = new SQLite3($file);
+			$this->db->enableExceptions(true);
+			$this->db->exec("CREATE TABLE storage ('key' BLOB PRIMARY KEY, 'value' BLOB, 'updated' INTEGER)");
+		} else {
+			$this->db = new SQLite3($file);
+			$this->db->enableExceptions(true);
+		}
+		$this->db->busyTimeout(5000);
+	}
+
+	protected function getCacheKey(){
+		if(is_null($this->param)) {
+			throw new \Exception('Call "setParameters" first!');
+		}
+
+		return hash('sha1', $this->path . http_build_query($this->param), true);
+	}
+}
