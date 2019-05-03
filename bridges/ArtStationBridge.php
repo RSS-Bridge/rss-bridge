34b141828976c5c5f1096514cfe7728aab8e40b6
Tobias Alexander Franke <tobias.franke@siggraph.org>
2019-05-03 19:22:19 +0200

    Fix TravisCI issues

--- bridges/ArtStationBridge.php
+++ bridges/ArtStationBridge.php
@@ -20,11 +20,17 @@ class ArtStationBridge extends BridgeAbstract {
 	}
 
 	public function getName() {
-		return self::NAME . ": " . $this->getInput('q');
+		return self::NAME . ': ' . $this->getInput('q');
 	}
 
 	public function fetchSearch($searchQuery) {
-		$data = '{"query":"' .$searchQuery. '","page":1,"per_page":50,"sorting":"date","pro_first":"1","filters":[],"additional_fields":[]}';
+		$data = '{"query":"' . $searchQuery . 
+				'","page":1,' .
+				'"per_page":50,' .
+				'"sorting":"date",' .
+				'"pro_first":"1", ' .
+				'"filters":[],' .
+				'"additional_fields":[]}';
 
 		$header = array(
 			'Content-Type: application/json',
@@ -38,21 +44,21 @@ class ArtStationBridge extends BridgeAbstract {
 		);
 
 		$jsonSearchURL = self::URI . '/api/v2/search/projects.json';
-		$jsonSearchStr = getContents($jsonSearchURL, $header, $opts) or die("Could not fetch JSON for search query.");;
+		$jsonSearchStr = getContents($jsonSearchURL, $header, $opts) or die('Could not fetch JSON for search query.');
 		return json_decode($jsonSearchStr);
 	}
 
 	public function fetchProject($hashID) {
-		$jsonProjectURL = self::URI . "/projects/" . $hashID . ".json";
-		$jsonProjectStr = getContents($jsonProjectURL) or die("Could not fetch JSON for project.");
+		$jsonProjectURL = self::URI . '/projects/' . $hashID . '.json';
+		$jsonProjectStr = getContents($jsonProjectURL) or die('Could not fetch JSON for project.');
 		return json_decode($jsonProjectStr);
 	}
 
 	public function collectData() {
 		$searchTerm = $this->getInput('q');
 		$jsonQuery = $this->fetchSearch($searchTerm);
-			
-		foreach($jsonQuery->data as $media)
+
+		foreach($jsonQuery->data as $media) 
 		{
 			// get detailed info about media item
 			$jsonProject = $this->fetchProject($media->hash_id);
@@ -63,17 +69,17 @@ class ArtStationBridge extends BridgeAbstract {
 			$item['uri'] = $media->url;
 			$item['timestamp'] = strtotime($jsonProject->published_at);
 			$item['author'] = $media->user->full_name;
-			$item['categories'] = implode(",", $jsonProject->tags);
-			
-			$item['content'] = 
-				'<a href="' .$media->url. '">
-				<img style="max-width: 100%" src="' . $jsonProject->cover_url . '">
-				</a>' . $jsonProject->description;
-				
+			$item['categories'] = implode(',', $jsonProject->tags);
+
+			$item['content'] = '<a href="' . $media->url . '">
+								<img style="max-width: 100%" src="' . $jsonProject->cover_url . '">
+								</a>' . $jsonProject->description;
+
 			$numAssets = count($jsonProject->assets);
 
 			if ($numAssets > 1)
-				$item['content'] .= '<p><a href="' .$media->url. '">Project contains ' . ($numAssets - 1) . ' more item(s).</a></p>';
+				$item['content'] .= '<p><a href="' . $media->url . '">Project contains ' . ($numAssets - 1) . 
+									' more item(s).</a></p>';
 
 			$this->items[] = $item;
 
