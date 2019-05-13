8caf93f7d38ffc2fdca12418630c1685eaa5039d
killruana <killruana@gmail.com>
2019-05-13 15:08:55 +0200

    [MediapartBridge] Reformat for respecting the 80 characters line length

--- bridges/MediapartBridge.php
+++ bridges/MediapartBridge.php
@@ -6,10 +6,10 @@ class MediapartBridge extends FeedExpander {
 	const URI = 'https://www.mediapart.fr/';
 	const PARAMETERS = array(
 		array(
-			'MPSESSID' => array(
+			'mpsessid' => array(
 				'name' => 'MPSESSID',
 				'type' => 'text',
-				'title' => 'Cookie session'
+				'title' => 'Value of the session cookie MPSESSID'
 			)
 		)
 	);
@@ -25,12 +25,13 @@ class MediapartBridge extends FeedExpander {
 		$item = parent::parseItem($newsItem);
 		$item['uri'] .= '?onglet=full';
 
-		$mpsessid = $this->getInput('MPSESSID');
+		$mpsessid = $this->getInput('mpsessid');
 		if (!empty($mpsessid)) {
 			$opt = array();
 			$opt[CURLOPT_COOKIE] = 'MPSESSID=' . $mpsessid;
 			$articlePage = getSimpleHTMLDOM($item['uri'], array(), $opt);
-			$content = sanitize($articlePage->find('div.content-article', 0)->innertext);
+			$content = $articlePage->find('div.content-article', 0)->innertext;
+			$content = sanitize($content);
 			$content = defaultLinkTo($content, static::URI);
 			$item['content'] .= $content;
 		}
