030197fef215dfac6a7a7b1b45dc0d863a99e257
triatic <42704418+triatic@users.noreply.github.com>
2019-06-22 17:26:02 +0100

    Fix line length

--- bridges/TelegramBridge.php
+++ bridges/TelegramBridge.php
@@ -42,7 +42,8 @@ class TelegramBridge extends BridgeAbstract {
 			$item['title'] = $this->itemTitle;
 			$item['timestamp'] = $this->processDate($messageDiv);
 			$item['enclosures'] = $this->enclosures;
-			$item['author'] = html_entity_decode(trim($messageDiv->find('a.tgme_widget_message_owner_name', 0)->plaintext), ENT_QUOTES);
+			$item['author'] = html_entity_decode(trim($messageDiv->find('a.tgme_widget_message_owner_name', 0)->plaintext),
+				ENT_QUOTES);
 
 			$this->items[] = $item;
 		}
