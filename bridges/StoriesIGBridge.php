d6a7c4186bcf4083dd72a1021e733f86f6b4b462
Antoine Turmel <antoine.social@turmel.info>
2019-06-24 21:09:16 +0200

    use tabs indent for StoriesIGBridge

--- bridges/StoriesIGBridge.php
+++ bridges/StoriesIGBridge.php
@@ -1,48 +1,48 @@
 <?php
 class StoriesIGBridge extends BridgeAbstract {
 
-        const NAME = 'Instagram Stories';
-        const URI = 'https://storiesig.com';
-        const DESCRIPTION = 'Display Instagram Stories';
-        const MAINTAINER = 'AntoineTurmel';
-        const PARAMETERS = array(
-                array(
-                        'username' => array(
-                                'name' => 'Instagram username',
-                                'type' => 'text',
-                                'required' => true,
-                                'title' => 'Insert the username here'
-                        ),
-                )
-        );
+	const NAME = 'Instagram Stories';
+	const URI = 'https://storiesig.com';
+	const DESCRIPTION = 'Display Instagram Stories';
+	const MAINTAINER = 'antoineturmel';
+	const PARAMETERS = array(
+		array(
+			'username' => array(
+				'name' => 'Instagram username',
+				'type' => 'text',
+				'required' => true,
+				'title' => 'Insert the username here'
+			),
+		)
+	);
 
-        public function collectData(){
-                $html = getSimpleHTMLDOM($this->getURI())
-                        or returnServerError('Failed to receive ' . $this->getURI());
+	public function collectData(){
+		$html = getSimpleHTMLDOM($this->getURI())
+			or returnServerError('Failed to receive ' . $this->getURI());
 
-                $results = $html->find('article');
+		$results = $html->find('article');
 
-                foreach($results as $result) {
+		foreach($results as $result) {
 
-                        $item = array();
+			$item = array();
 
-                        $item['title'] = $this->getInput('username') . ' story';
-                        $item['uri'] = $result->find('div.download', 0)->find('a', 0)->href;
-                        $item['author'] = $this->getInput('username');
-                        $item['uid'] = $result->find('time', 0)->datetime;
+			$item['title'] = $this->getInput('username') . ' story';
+			$item['uri'] = $result->find('div.download', 0)->find('a', 0)->href;
+			$item['author'] = $this->getInput('username');
+			$item['uid'] = $result->find('time', 0)->datetime;
 
-                        $item['content'] = $result;
+			$item['content'] = $result;
 
-                        $this->items[] = $item;
-                }
-        }
+			$this->items[] = $item;
+		}
+	}
 
-        public function getURI(){
-                $uri = self::URI . '/stories/';
-                $uri .= urlencode($this->getInput('username'));
-                return $uri;
+	public function getURI(){
+		$uri = self::URI . '/stories/';
+		$uri .= urlencode($this->getInput('username'));
+		return $uri;
 
-                return parent::getURI();
-        }
+		return parent::getURI();
+	}
 
 }
