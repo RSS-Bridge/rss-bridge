7bfa3aaaf955eaba0ad0264790823cb41f91c42b
Joseph <joseph@verifiedjoseph.com>
2019-06-25 12:58:11 +0100

    Add function processCollection()

--- bridges/InternetArchiveBridge.php
+++ bridges/InternetArchiveBridge.php
@@ -34,7 +34,7 @@ class InternetArchiveBridge extends BridgeAbstract {
 		'item-ia mobile-header hidden-tiles',
 		'item-ia account-ia'
 	);
-	
+
 	public function collectData() {
 
 		$html = getSimpleHTMLDOM($this->getURI())
@@ -46,7 +46,7 @@ class InternetArchiveBridge extends BridgeAbstract {
 
 			foreach ($html->find('div.results > div[data-id]') as $index => $result) {
 				$item = array();
-				
+
 				if (in_array($result->class, $this->skipClasses)) {
 					continue;
 				}
@@ -94,42 +94,50 @@ class InternetArchiveBridge extends BridgeAbstract {
 
 		return $this->getInput('username');
 	}
-	
+
 	private function processUpload($result) {
 
 		$item = array();
-		
+
 		$collection = $result->find('a.stealth', 0);
 		$collectionLink = self::URI . $collection->href;
 		$collectionTitle = $collection->find('div.item-parent-ttl', 0)->innertext;
-		
+
 		$item['title'] = trim($result->find('div.ttl', 0)->innertext);
 		$item['timestamp'] = strtotime($result->find('div.hidden-tiles.pubdate.C.C3', 0)->children(0)->innertext);
-		$item['uri'] = self::URI . $result->find('div.item-ttl.C.C2 > a' , 0)->href;
-		
+		$item['uri'] = self::URI . $result->find('div.item-ttl.C.C2 > a', 0)->href;
+
 		if ($result->find('div.by.C.C4', 0)->children(2)) {
 			$item['author'] = $result->find('div.by.C.C4', 0)->children(2)->plaintext;
 		}
-		
+
 		$item['content'] = <<<EOD
 <p>Media Type: {$result->attr['data-mediatype']}<br>
 Collection: <a href="{$collectionLink}">{$collectionTitle}</a></p>
 EOD;
-		
+
 		$item['enclosures'][] = self::URI . $result->find('img.item-img', 0)->source;
-		
+
 		return $item;
-	}	
-	
+	}
+
 	private function processCollection($result) {
 
 		$item = array();
-		
+
+		$item['title'] = trim($result->find('div.collection-title.C.C2', 0)->children(0)->plaintext);
+		$item['timestamp'] = strtotime($result->find('div.hidden-tiles.pubdate.C.C3', 0)->children(0)->plaintext);
+		$item['uri'] = self::URI . $result->find('div.collection-title.C.C2 > a', 0)->href;
+
 		$item['content'] = '';
-		
+
+		if ($result->find('img.tile-img', 0)) {
+			$item['enclosures'][] = self::URI . $result->find('img.tile-img', 0)->source;
+		}
+
 		return $item;
 	}
-	
+
 	private function processHiddenDetails($html, $detailsDivNumber, $item) {
 
 		$description = '';
@@ -139,17 +147,17 @@ EOD;
 
 			if ($detailsDiv->find('div.C234', 0)->children(0)) {
 				$description = $detailsDiv->find('div.C234', 0)->children(0)->plaintext;
-				
+
 				$detailsDiv->find('div.C234', 0)->children(0)->innertext = '';
 			}
 
 			$topics = trim($detailsDiv->find('div.C234', 0)->plaintext);
-			
+
 			if (!empty($topics)) {
 				$topics = trim($detailsDiv->find('div.C234', 0)->plaintext);
 				$topics = trim(substr($topics, 7));
 
-				$item['categories'] = explode(',', $topics);	
+				$item['categories'] = explode(',', $topics);
 			}
 
 			$item['content'] = '<p>' . $description . '</p>' . $item['content'];
