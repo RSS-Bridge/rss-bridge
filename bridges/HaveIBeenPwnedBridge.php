23bb51e028156d0a2ca04efca53fb8b23681ed96
Joseph <joseph@verifiedjoseph.com>
2019-07-01 17:31:01 +0100

    Limit number of feed items to 20

--- bridges/HaveIBeenPwnedBridge.php
+++ bridges/HaveIBeenPwnedBridge.php
@@ -98,6 +98,10 @@ class HaveIBeenPwnedBridge extends BridgeAbstract {
 			$item['content'] = $breach['content'];
 
 			$this->items[] = $item;
+			
+			if (count($this->items) >= 20) {
+				break;
+			}
 		}
 	}
 }
