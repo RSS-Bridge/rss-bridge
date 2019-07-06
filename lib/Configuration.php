5a0ea423c42cac3377a5e73a431f6e71b1b1a5ff
logmanoriginal <logmanoriginal@users.noreply.github.com>
2019-07-06 12:35:36 +0200

    [Configuration] Bump version to dev.2019-07-06

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -28,7 +28,7 @@ final class Configuration {
 	 *
 	 * @todo Replace this property by a constant.
 	 */
-	public static $VERSION = '2019-07-06';
+	public static $VERSION = 'dev.2019-07-06';
 
 	/**
 	 * Holds the configuration data.
