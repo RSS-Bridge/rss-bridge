ef00fb798508e15c8a779353caddf8f743e81b19
Joseph <joseph@verifiedjoseph.com>
2019-07-10 13:18:32 +0100

    Change CACHE_TIMEOUT to 3600 seconds (1 hour)

--- bridges/DailymotionBridge.php
+++ bridges/DailymotionBridge.php
@@ -4,7 +4,7 @@ class DailymotionBridge extends BridgeAbstract {
 	const MAINTAINER = 'mitsukarenai';
 	const NAME = 'Dailymotion Bridge';
 	const URI = 'https://www.dailymotion.com/';
-	const CACHE_TIMEOUT = 10800; // 3h
+	const CACHE_TIMEOUT = 3600; // 1h
 	const DESCRIPTION = 'Returns the 5 newest videos by username/playlist or search';
 
 	const PARAMETERS = array (
