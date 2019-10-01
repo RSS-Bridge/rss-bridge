0ba54f15e48d86cfceea9ebf6b7f1706b9a8d798
Florent VIOLLEAU <florent.violleau@opera-cesson.fr>
2019-10-02 00:37:30 +0200

    Fix PHP CS

--- bridges/GithubLastCommitBranchBridge.php
+++ bridges/GithubLastCommitBranchBridge.php
@@ -8,19 +8,19 @@ class GithubLastCommitBranchBridge extends BridgeAbstract {
 	const DESCRIPTION = 'Returns the last commit of a branch of a github project';
 
 	const PARAMETERS = array(array(
-        'u' => array(
-            'name' => 'User name',
-            'required' => true
-        ),
-        'p' => array(
-            'name' => 'Project name',
-            'required' => true
-        ),
-        'b' => array(
-            'name' => 'Branch name',
-            'defaultValue' => 'master'
-        )
-    ));
+		'u' => array(
+			'name' => 'User name',
+			'required' => true
+		),
+		'p' => array(
+			'name' => 'Project name',
+			'required' => true
+		),
+		'b' => array(
+			'name' => 'Branch name',
+			'defaultValue' => 'master'
+		)
+	));
 
 	public function getURI(){
 		if(null !== $this->getInput('u') && null !== $this->getInput('p') && null !== $this->getInput('b')) {
@@ -34,40 +34,36 @@ class GithubLastCommitBranchBridge extends BridgeAbstract {
 	}
 
 	public function collectData(){
-	    $url = $this->getURI();
+		$url = $this->getURI();
 
 		$header = array(
 			'Content-Type: application/json',
 		);
 
-	    $opts = array(
-            CURLOPT_SSL_VERIFYPEER => false
-        );
+		$content = getContents($url, $header, $opts)
+			or returnServerError('Could not request Github api . Tried: ' . $url);
 
-	    $content = getContents($url, $header, $opts)
-            or returnServerError('Could not request Github api . Tried: ' . $url);
+		$commit = json_decode($content);
 
-        $commit = json_decode($content);
+		$item['uri'] = $commit->commit->url;
+		$item['title'] = $commit->commit->message;
+		$item['author'] = $commit->commit->committer->name;
+		$item['content'] = $commit->commit->message;
+		$item['date'] = $commit->commit->committer->date;
+		$item['uid'] = $commit->node_id;
 
-        $item['uri'] = $commit->commit->url;
-        $item['title'] = $commit->commit->message;
-        $item['author'] = $commit->commit->committer->name;
-        $item['content'] = $commit->commit->message;
-        $item['date'] = $commit->commit->committer->date;
-        $item['uid'] = $commit->node_id;
+		if (sizeof($commit->files) > 0) {
+			$item['content'] .= "<br/>Files modified: <br/>";
+		}
 
-        if (sizeof($commit->files) > 0) {
-            $item['content'] .= "<br/>Files modified: <br/>";
-        }
+		foreach($commit->files as $file) {
+			$item['content'] .= "    - $file->additions additions, $file->deletions deletions, $file->changes changes | $file->filename: <br/>";
+		}
 
-        foreach($commit->files as $file) {
-            $item['content'] .= "    - $file->additions additions, $file->deletions deletions, $file->changes changes | $file->filename: <br/>";
-        }
+		$totalAdditions = $commit->stats->additions;
+		$totalDeletions = $commit->stats->deletions;
+		$item['content'] .= '</br>Total ' . count($commit->files) . " changed files with $totalAdditions additions and $totalDeletions deletions.";
 
-        $totalAdditions = $commit->stats->additions;
-        $totalDeletions = $commit->stats->deletions;
-        $item['content'] .= '</br>Total ' . count($commit->files) . " changed files with $totalAdditions additions and $totalDeletions deletions.";
-
-        $this->items[] = $item;
+		$this->items[] = $item;
 	}
 }
