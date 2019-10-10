8cdb552faf2b29c4e41f0ac967a4995382fff7f9
Jacob Mansfield <cyberjacob@gmail.com>
2019-10-10 21:40:56 +0100

    New bridge for The Whiteboard

---
+++ bridges/TheWhiteboardBridge.php
@@ -0,0 +1,21 @@
+<?php
+class TheWhiteboardBridge extends BridgeAbstract {
+        const NAME = 'The Whiteboard';
+        const URI = "https://www.the-whiteboard.com/";
+        const DESCRIPTION = 'Get the latest comic from The Whiteboard';
+        const MAINTAINER = 'CyberJacob';
+
+        public function collectData() {
+                $item = array();
+
+                $html = getSimpleHTMLDOM(self::URI) or returnServerError('Could not load The Whiteboard.');
+
+                $image = $html->find('center', 1)->find('img', 0);
+                $image->src = self::URI.'/'.$image->src;
+
+                $item['title'] = explode("\r\n", $html->find('center', 1)->plaintext)[0];//->find('text')->plaintext;
+                $item['content'] = $image;
+
+                $this->items[] = $item;
+        }
+}
