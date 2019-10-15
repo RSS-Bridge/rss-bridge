77496c41142029d42f28ecd57712bbd1b00f5124
Joseph <joseph@verifiedjoseph.com>
2019-10-15 20:34:56 +0100

    [GizmodoBridge] Update bridge

--- bridges/GizmodoBridge.php
+++ bridges/GizmodoBridge.php
@@ -3,34 +3,85 @@ class GizmodoBridge extends FeedExpander {
 
 	const MAINTAINER = 'polopollo';
 	const NAME = 'Gizmodo';
-	const URI = 'http://gizmodo.com/';
+	const URI = 'https://gizmodo.com';
 	const CACHE_TIMEOUT = 1800; // 30min
-	const DESCRIPTION = 'Returns the newest posts from Gizmodo (full text).';
+	const DESCRIPTION = 'Returns the newest posts from Gizmodo.';
 
-	protected function parseItem($item){
+	protected function parseItem($item) {
 		$item = parent::parseItem($item);
 
-		$articleHTMLContent = getSimpleHTMLDOMCached($item['uri']);
-		if(!$articleHTMLContent) {
-			$text = 'Could not load ' . $item['uri'];
-		} else {
-			$text = $articleHTMLContent->find('div.entry-content', 0)->innertext;
-			foreach($articleHTMLContent->find('pagespeed_iframe') as $element) {
-				$text .= '<p>link to a iframe (could be a video): <a href="'
-				. $element->src
-				. '">'
-				. $element->src
-				. '</a></p><br>';
-			}
+		$articleHtml = getSimpleHTMLDOMCached($item['uri'])
+			or returnServerError('Could not request: ' . $item['uri']);
 
-			$text = strip_tags($text, '<p><b><a><blockquote><img><em>');
+		$articleHtml = $this->stripTags($articleHtml);
+		$articleHtml = $this->handleFigureTags($articleHtml);
+
+		// Get header image
+		$image = $articleHtml->find('meta[property="og:image"]', 0)->content;
+
+		//  Get article body
+		if ($articleHtml->find('div.js_expandable-container', 0)) {
+			$text = $articleHtml->find('div.js_expandable-container', 0)->innertext;
+
+		} else {
+			$text = $articleHtml->find('div.post-content', 0)->innertext;
 		}
 
-		$item['content'] = $text;
+		$item['content'] = <<<EOD
+<img src="{$image}">$text
+EOD;
+
+		// Get categories
+		$categories = explode(',', $articleHtml->find('meta[name="keywords"]', 0)->content);
+		$item['categories'] = array_map('trim', $categories);
+
+		$item['enclosures'][] = $image;
+
 		return $item;
 	}
 
-	public function collectData(){
-		$this->collectExpandableDatas('http://feeds.gawker.com/gizmodo/full');
+	public function collectData() {
+		$this->collectExpandableDatas(self::URI . '/rss', 20);
+	}
+
+	private function stripTags($articleHtml) {
+
+		foreach ($articleHtml->find('aside') as $index => $aside) {
+			$articleHtml->find('aside', $index)->outertext = '';
+		}
+
+		foreach ($articleHtml->find('div.ad-unit') as $index => $aside) {
+			$articleHtml->find('div.ad-unit', $index)->outertext = '';
+		}
+
+		return $articleHtml;
+	}
+
+	private function handleFigureTags($articleHtml) {
+
+		foreach ($articleHtml->find('figure') as $index => $figure) {
+
+			if (isset($figure->attr['data-id'])) {
+				$id = $figure->attr['data-id'];
+				$format = $figure->attr['data-format'];
+
+			} else {
+
+				$img = $figure->find('img', 0);
+				$id = $img->attr['data-chomp-id'];
+				$format = $img->attr['data-format'];
+				$figure->find('div.img-permalink-sub-wrapper', 0)->style = ''; 
+			}
+
+			$imageUrl = 'https://i.kinja-img.com/gawker-media/image/upload/' . $id . '.' . $format;
+
+			$figure->find('span', 0)->outertext = <<<EOD
+<img src="{$imageUrl}">
+EOD;
+		
+			$articleHtml->find('figure', $index)->outertext = $figure->outertext;
+		}
+		
+		return $articleHtml;
 	}
 }
