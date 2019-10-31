7ff800f687f81ed6c43750ec7d0086b9501f8094
logmanoriginal <logmanoriginal@users.noreply.github.com>
2019-10-31 21:40:44 +0100

    [Connectivity] Import Bootstrap v4.3.1 CSS

--- actions/ConnectivityAction.php
+++ actions/ConnectivityAction.php
@@ -106,11 +106,7 @@ class ConnectivityAction extends ActionAbstract {
 
 <html>
 	<head>
-		<link
-			rel="stylesheet"
-			href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-			integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-			crossorigin="anonymous">
+		<link rel="stylesheet" href="static/bootstrap.min.css">
 		<link
 			rel="stylesheet"
 			href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
