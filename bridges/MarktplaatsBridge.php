1679254f732ca616c77d138001f1cdda71656cba
Park0 <Park0@users.noreply.github.com>
2020-05-26 12:45:10 +0200

    Support Marktplaats.nl search

---
+++ bridges/MarktplaatsBridge.php
@@ -0,0 +1,37 @@
+<?php
+
+class MarktplaatsBridge extends BridgeAbstract {
+	const NAME = 'Marktplaats';
+	const URI = 'https://marktplaats.nl';
+	const DESCRIPTION = 'Read search queries from marktplaats'; 
+	const PARAMETERS = array(
+		'Search' => array(
+			'n' => array(
+				'name' => 'query',
+				'type' => 'text',
+				'required' => true,
+				'title' => 'The search string for marktplaats',
+			)
+		)
+	);
+	const CACHE_TIMEOUT = 900;
+
+	public function collectData() {
+	$url = 'https://www.marktplaats.nl/lrp/api/search?query=' . urlencode($this->getInput('n'));
+		$jsonString = getSimpleHTMLDOM($url, 900) or returnServerError('No contents received!');
+		$jsonObj = json_decode($jsonString);
+		foreach($jsonObj->listings as $listing) {
+			$item = array();
+			$item['uri'] = 'https://marktplaats.nl' . $listing->vipUrl;
+			$item['title'] = $listing->title;
+			$item['timestamp'] = $listing->date;
+			$item['author'] = $listing->sellerInformation->sellerName;
+			$item['content'] = $listing->description . "\n\n\nSellerID: " . $listing->sellerInformation->sellerId;
+			$item['enclosures'] = $listing->imageUrls;
+			$item['categories'] = $listing->verticals;
+			$item['uid'] = $listing->itemId;
+			$this->items[] = $item;
+		}
+
+	}
+}
