572fdce77a79c8237c289ee2592d9647cfa1e0c1
Park0 <Park0@users.noreply.github.com>
2020-05-27 17:47:44 +0200

    More parameters added
    Added option to filter on distance & price
    Added option to filter global items without location

--- bridges/MarktplaatsBridge.php
+++ bridges/MarktplaatsBridge.php
@@ -3,35 +3,82 @@
 class MarktplaatsBridge extends BridgeAbstract {
 	const NAME = 'Marktplaats';
 	const URI = 'https://marktplaats.nl';
-	const DESCRIPTION = 'Read search queries from marktplaats';
+	const DESCRIPTION = 'Read search queries from marktplaats.nl';
 	const PARAMETERS = array(
 		'Search' => array(
-			'n' => array(
+			'q' => array(
 				'name' => 'query',
 				'type' => 'text',
 				'required' => true,
 				'title' => 'The search string for marktplaats',
+			),
+			'z' => array(
+				'name' => 'zipcode',
+				'type' => 'text',
+				'required' => false,
+				'title' => 'Zip code for location limited searches',
+			),
+			'd' => array(
+				'name' => 'distance',
+				'type' => 'number',
+				'required' => false,
+				'title' => 'The distance in meters from the zipcode',
+			),
+			'f' => array(
+				'name' => 'priceFrom',
+				'type' => 'number',
+				'required' => false,
+				'title' => 'The minimal price in cents',
+			),
+			't' => array(
+				'name' => 'priceTo',
+				'type' => 'number',
+				'required' => false,
+				'title' => 'The maximal price in cents',
+			),
+			's' => array(
+				'name' => 'showGlobal',
+				'type' => 'checkbox',
+				'required' => false,
+				'title' => 'Include result with negative distance',
 			)
 		)
 	);
 	const CACHE_TIMEOUT = 900;
 
 	public function collectData() {
-	$url = 'https://www.marktplaats.nl/lrp/api/search?query=' . urlencode($this->getInput('n'));
+		$query = '';
+		$excludeGlobal = true;
+		if(!is_null($this->getInput('z')) && !is_null($this->getInput('d'))) {
+			$query = '&postcode=' . $this->getInput('z') . '&distanceMeters=' . $this->getInput('d');
+		}
+		if(!is_null($this->getInput('f'))) {
+			$query .= '&PriceCentsFrom=' . $this->getInput('f');
+		}
+		if(!is_null($this->getInput('t'))) {
+			$query .= '&PriceCentsTo=' . $this->getInput('t');
+		}
+		if(!is_null($this->getInput('s'))) {
+			if($this->getInput('s')) {
+				$excludeGlobal = false;
+			}
+		}
+		$url = 'https://www.marktplaats.nl/lrp/api/search?query=' . urlencode($this->getInput('q')) . $query;
 		$jsonString = getSimpleHTMLDOM($url, 900) or returnServerError('No contents received!');
 		$jsonObj = json_decode($jsonString);
 		foreach($jsonObj->listings as $listing) {
-			$item = array();
-			$item['uri'] = 'https://marktplaats.nl' . $listing->vipUrl;
-			$item['title'] = $listing->title;
-			$item['timestamp'] = $listing->date;
-			$item['author'] = $listing->sellerInformation->sellerName;
-			$item['content'] = $listing->description . "\n\n\nSellerID: " . $listing->sellerInformation->sellerId;
-			$item['enclosures'] = $listing->imageUrls;
-			$item['categories'] = $listing->verticals;
-			$item['uid'] = $listing->itemId;
-			$this->items[] = $item;
+			if(!$excludeGlobal || $listing->location->distanceMeters >= 0) {
+				$item = array();
+				$item['uri'] = 'https://marktplaats.nl' . $listing->vipUrl;
+				$item['title'] = $listing->title;
+				$item['timestamp'] = $listing->date;
+				$item['author'] = $listing->sellerInformation->sellerName;
+				$item['content'] = $listing->description . "\n\n\nSellerID: " . $listing->sellerInformation->sellerId;
+				$item['enclosures'] = $listing->imageUrls;
+				$item['categories'] = $listing->verticals;
+				$item['uid'] = $listing->itemId;
+				$this->items[] = $item;
+			}
 		}
-
 	}
 }
