30b341f65ea0ffd3b7978c2b815dca50c0b43e7d
Adam R Grey <git@adamrgrey.com>
2020-07-21 10:43:17 -0400

    follow formatting guides
    attempt 1, don't have the stuff to run the checks myself

--- bridges/ImgurBridge.php
+++ bridges/ImgurBridge.php
@@ -34,12 +34,14 @@ class ImgurBridge extends BridgeAbstract {
 
 	public function collectData() {
 
-		$url = "https://api.imgur.com/3/";
+		$url = 'https://api.imgur.com/3/';
 		
 		switch($this->queriedContext) {
 
 			case 'User':
-				$url .= "account/" . $this->getInput('u') . "/submissions/0/newest"; 
+				$url .= 'account/'
+					. $this->getInput('u')
+					. '/submissions/0/newest';
 
 				$response = $this->simpleGetFromJson($url);
 
@@ -57,7 +59,7 @@ class ImgurBridge extends BridgeAbstract {
 				break;
 
 			case 'Most Viral': //todo: allow
-				$url .= "gallery/hot/viral";
+				$url .= 'gallery/hot/viral';
 
 				$response = $this->simpleGetFromJson($url);
 
@@ -66,7 +68,11 @@ class ImgurBridge extends BridgeAbstract {
 				break;
 
 			default:
-				returnClientError('Unknown context: "' . $this->queriedContext . '"!');
+				returnClientError(
+					'Unknown context: "'
+					. $this->queriedContext 
+					. '"!'
+				);
 
 		}
 
@@ -81,19 +87,20 @@ class ImgurBridge extends BridgeAbstract {
 			$item['title'] = $album->title;
 			$item['timestamp'] = $album->datetime;
 			$item['author'] = $album->account_url;
-			$item['content'] = "";
+			$item['content'] = '';
 			$item['categories'] = array();
 
 			foreach ($album->tags as $tag) {
 				$item['categories'][] = $tag->name;
 			}
 
-			if(property_exists($album, "images")){
+			if(property_exists($album, 'images')) {
 				foreach ($album->images as $image) {
 					$item['content'] .= $this->albumImage2Html($image);
 				}
-			}else if(property_exists($album, "link")){
-				$item['content'] .= $this->albumImage2Html($album); //the album is only 1 image
+			}elseif(property_exists($album, 'link')) {
+				//the album is only 1 image
+				$item['content'] .= $this->albumImage2Html($album);
 			}
 
 			$this->items[] = $item;
@@ -101,59 +108,29 @@ class ImgurBridge extends BridgeAbstract {
 	}
 
 	private function albumImage2Html($img){
-		$txt = "";
-		if(substr($img->type, 0, 6) !== "image/"){
-			$txt .= "<video src=\"" . $img->link . "\" controls></video><br />";
+		$txt = '';
+		if(substr($img->type, 0, 6) !== 'image/') {
+			$txt .= '<video src="'
+				. $img->link 
+				. '" controls></video><br />';
 		}else{
-			$txt .= "<img src=\"" . $img->link . "\" /><br />";
+			$txt .= '<img src="' . $img->link . '" /><br />';
 		}
-		if(null !== $img->description && trim($img->description) !== ''){
+		if(null !== $img->description && trim($img->description) !== '') {
 			$desc = htmlentities($img->description);
-			$desc = str_replace("\n", "<br />", $desc);
-			$txt .= $desc . "<br />";
+			$desc = str_replace('\n', '<br />', $desc);
+			$txt .= $desc . '<br />';
 		}
 		return $txt;
 	}
 
-	/*
-	private function sanitizeUser($user) {
-		if (filter_var($user, FILTER_VALIDATE_URL)) {
-
-			$urlparts = parse_url($user);
-
-			if($urlparts['host'] !== parse_url(self::URI)['host']) {
-				returnClientError('The host you provided is invalid! Received "'
-				. $urlparts['host']
-				. '", expected "'
-				. parse_url(self::URI)['host']
-				. '"!');
-			}
-
-			if(!array_key_exists('path', $urlparts)
-			|| $urlparts['path'] === '/') {
-				returnClientError('The URL you provided doesn\'t contain the user name!');
-			}
-
-			return explode('/', $urlparts['path'])[2];
-
-		} else {
-
-			// First character cannot be a forward slash
-			if(strpos($user, '/') === 0) {
-				returnClientError('Remove leading slash "/" from the username!');
-			}
-
-			return $user;
-
-		}
-	}
-	*/
-
 	private function simpleGetFromJson($url){
-		$context = stream_context_create([ 
-			'http' => [ 
-				'method' => 'GET', 
-				'header' => ['Authorization: Client-ID ' . $this->getInput("clientId")] 
+		$context = stream_context_create([
+			'http' => [
+				'method' => 'GET',
+				'header' => ['Authorization: Client-ID '
+					. $this->getInput('clientId')
+				]
 			]
 		]);
 		$result = file_get_contents($url, false, $context);
