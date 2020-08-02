ab29dcb08796f2bddf0807d37f04f2e94f786d8d
t0stiman <18124323+t0stiman@users.noreply.github.com>
2020-08-02 18:01:28 +0200

    make Travis happy and fix an error

--- bridges/OMGUbuntuBridge.php
+++ bridges/OMGUbuntuBridge.php
@@ -1,9 +1,10 @@
 <?php
 class OMGUbuntuBridge extends FeedExpander {
-	const NAME = "OMG! Ubuntu! News";
-	const URI = "https://omgubuntu.com/"; 
-	const DESCRIPTION = "News about Ubuntu, Linux and open-source software.";
-	const MAINTAINER = "t0stiman";
+	const NAME = 'OMG! Ubuntu! News';
+	const URI = 'https://omgubuntu.com/';
+	const DESCRIPTION = 'News about Ubuntu, Linux and open-source software.';
+	const MAINTAINER = 't0stiman';
+	const CACHE_TIMEOUT = 20;
 
 	public function collectData() {
 		$this->collectExpandableDatas('http://feeds.feedburner.com/d0od');
@@ -16,8 +17,9 @@ class OMGUbuntuBridge extends FeedExpander {
 		$article = $articlePage->find('div.post-content', 0);
 
 		//get rid of some elements we don't need
-		$article = str_replace('<ul class="omg-socials">', '<ul class="omg-socials" style="display: none;">', $article);
-		$article = str_replace('<div class="post-links"', '<!-- ', $article);
+		$article = str_replace('<ul class="omg-socials">', '<ul style="display: none;">', $article);
+		$article = str_replace('<div class="post-links"', '<div style="display: none;">', $article);
+		$article = str_replace('<div class="post-links post-links--tags"', '<div style="display: none;">', $article);
 
 		$item['content'] = $article;
 
