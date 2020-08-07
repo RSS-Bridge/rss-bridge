c642652fea6fd7ea27eb264c88aeef3d5d65ef0e
ORelio <oreliogitantispam.l0gin@spamgourmet.com>
2020-08-07 15:19:14 +0200

    [GBAtemp] Fix tutorial mode Title extraction

--- bridges/GBAtempBridge.php
+++ bridges/GBAtempBridge.php
@@ -114,7 +114,7 @@ class GBAtempBridge extends BridgeAbstract {
 		case 'T':
 			foreach($html->find('li.portal-tutorial') as $tutorialItem) {
 				$url = self::URI . $tutorialItem->find('a', 1)->href;
-				$title = $tutorialItem->find('a', 0)->plaintext;
+				$title = $tutorialItem->find('a', 1)->plaintext;
 				$time = $this->findItemDate($tutorialItem);
 				$author = $tutorialItem->find('a.username', 0)->plaintext;
 				$content = $this->fetchPostContent($url, self::URI);
