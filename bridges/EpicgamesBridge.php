d86db221aa7cdded1bc336bf02aaff591f6159d3
Alexander Fadeev <af@clodo.ru>
2020-08-19 17:05:34 +0300

    [EpicgamesBridge] fixes

--- bridges/EpicgamesBridge.php
+++ bridges/EpicgamesBridge.php
@@ -1,7 +1,7 @@
 <?php
 class EpicgamesBridge extends BridgeAbstract {
 
-	const NAME = 'Epicgames News';
+	const NAME = 'Epic Games Store News';
 	const MAINTAINER = 'otakuf';
 	const URI = 'https://www.epicgames.com';
 	const DESCRIPTION = 'Returns the latest posts from epicgames.com';
@@ -53,7 +53,7 @@ class EpicgamesBridge extends BridgeAbstract {
 		$url = $api . $this->getInput('language') . '/content/blog?limit=' . $this->getInput('postcount');
 
 		$data = getContents($url)
-			or returnServerError('Unable to get the news pages from epicgames !');
+			or returnServerError('Unable to get the news pages from epicgames.com!');
 		$decodedData = json_decode($data);
 
 		foreach($decodedData as $key => $value) {
@@ -61,14 +61,12 @@ class EpicgamesBridge extends BridgeAbstract {
 			$item['uri'] = self::URI . $value->url;
 			$item['title'] = $value->title;
 			$item['timestamp'] = $value->date;
-			$item['author'] = 'EpicGames';
+			$item['author'] = 'Epic Games Store';
 			if(!empty($value->author)) {
 				$item['author'] = $value->author;
 			}
 			if(!empty($value->content)) {
-				$content = $value->content;
-				$content = str_replace('href="/', 'href="' . self::URI . '/', $content);
-				$item['content'] = $content;
+				$item['content'] = defaultLinkTo($value->content, self::URI);
 			}
 			if(!empty($value->image)) {
 				$item['enclosures'][] = $value->image;
