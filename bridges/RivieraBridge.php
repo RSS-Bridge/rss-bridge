67f10a801d6cedda5df5fd92d0816dd50bb34177
danoloan10 <danoloan10@tutanota.com>
2020-08-21 01:52:57 +0200

    [RivieraBridge] Add new bridge
    RSS bridge for concerts in Sala La Riviera, in Madrid

---
+++ bridges/RivieraBridge.php
@@ -0,0 +1,63 @@
+<?php
+
+class RivieraBridge extends BridgeAbstract {
+	const NAME = 'Riviera';
+	const URI = 'https://salariviera.com/conciertos-riviera/';
+	const DESCRIPTION = 'No description provided';
+	const MAINTAINER = 'No maintainer';
+	const PARAMETERS = array(); // Can be omitted!
+	const CACHE_TIMEOUT = 1;
+
+	private function eventToItem($event, string $month) : array {
+		$event_date_day = $event->find('.event-date-day', 0);
+		$event_title    = $event->find('.event-title', 0);
+		$event_status_wrapper = $event->find('.event-status-wrapper', 0);
+
+		$date  = $event_date_day->innertext . ' ' . $month;
+		$title = $event_title->innertext;
+		$link  = $event_title->find('a', 0)->href;
+		//$buy   = $event_status_wrapper->innertext;
+
+		$item = array(
+			'title'   => $date . ' - ' . $title,
+			//'content' => $date . '<br><div style=\'font-size:150%; font-weigth:bold\'>' . $title . '</div><br>' . $buy,
+			'uri'     => $link,
+		);
+
+		return $item;
+	}
+
+	private function getEventItemsPage(int $page) {
+		$events = array();
+
+		$html = getSimpleHTMLDOM(self::URI . '/page/' . $page);
+		$month = '';
+
+		foreach ($html->find('.gdlr-list-event, .gdlr-list-by-month-header')
+			as $event) {
+			if ($event->class == 'gdlr-list-by-month-header') {
+				$month = $event->innertext;
+			} else {
+				$item = $this->eventToItem($event, $month);
+				$events[] = $item;
+			}
+		}
+
+		return array_reverse($events);
+	}
+
+	private function getEventItems() : array {
+		$page = 0;
+		$events = array();
+		do {
+			$current = $this->getEventItemsPage($page);
+			$events  = array_merge($current, $events);
+			$page++;
+		} while (sizeof($current) > 0);
+		return $events;
+	}
+
+	public function collectData() {
+		$this->items = array_merge($this->items, $this->getEventItems());
+	}
+}
