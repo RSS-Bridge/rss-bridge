5a4742486fad98b723814ad90a34bcdf42bcb5ce
Simounet <contact@simounet.net>
2020-08-22 15:42:40 +0200

    [MondeDiplo] Title and content updated

--- bridges/MondeDiploBridge.php
+++ bridges/MondeDiploBridge.php
@@ -7,18 +7,22 @@ class MondeDiploBridge extends BridgeAbstract {
 	const CACHE_TIMEOUT = 21600; //6h
 	const DESCRIPTION = 'Returns most recent results from MondeDiplo.';
 
+        private function cleanText($text) {
+            return trim(str_replace(['&nbsp;', '&nbsp'], ' ', $text));
+        }
+
 	public function collectData(){
 		$html = getSimpleHTMLDOM(self::URI)
 			or returnServerError('Could not request MondeDiplo. for : ' . self::URI);
 
 		foreach($html->find('div.unarticle') as $article) {
 			$element = $article->parent();
+			$title = $element->find('h3', 0)->plaintext;
+			$datesAuteurs = $element->find('div.dates_auteurs', 0)->plaintext;
 			$item = array();
 			$item['uri'] = self::URI . $element->href;
-			$item['title'] = $element->find('h3', 0)->plaintext;
-			$item['content'] = $element->find('div.dates_auteurs', 0)->plaintext
-			. '<br>'
-			. strstr($element->find('div', 0)->plaintext, $element->find('div.dates_auteurs', 0)->plaintext, true);
+			$item['title'] = $this->cleanText($title) . ' - ' . $this->cleanText($datesAuteurs);
+			$item['content'] = $this->cleanText(str_replace([$title, $datesAuteurs], '', $element->plaintext));
 
 			$this->items[] = $item;
 		}
