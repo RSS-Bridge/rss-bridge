9ce1b9bd4f8e62e80f1a19e24e214985fe52d9a9
langfingaz <daniel@systemli.org>
2020-09-18 22:12:08 +0200

    extend UnsplashBridge:
    + filter photo stream for username
    + change image preview quality
    + add link to user, location (if present), link to photo on unsplash to $item['content']
    
    Signed-off-by: langfingaz <daniel@systemli.org>

--- bridges/UnsplashBridge.php
+++ bridges/UnsplashBridge.php
@@ -1,68 +1,113 @@
 <?php
-class UnsplashBridge extends BridgeAbstract {
 
-	const MAINTAINER = 'nel50n';
-	const NAME = 'Unsplash Bridge';
-	const URI = 'https://unsplash.com/';
-	const CACHE_TIMEOUT = 43200; // 12h
-	const DESCRIPTION = 'Returns the latests photos from Unsplash';
+class UnsplashBridge extends BridgeAbstract
+{
+    const MAINTAINER = 'nel50n, langfingaz';
+    const NAME = 'Unsplash Bridge';
+    const URI = 'https://unsplash.com/';
+    const CACHE_TIMEOUT = 43200; // 12h
+    const DESCRIPTION = 'Returns the latest photos from Unsplash';
 
-	const PARAMETERS = array( array(
-		'm' => array(
-			'name' => 'Max number of photos',
-			'type' => 'number',
-			'defaultValue' => 20
-		),
-		'w' => array(
-			'name' => 'Width',
-			'exampleValue' => '1920, 1680, …',
-			'defaultValue' => '1920'
-		),
-		'q' => array(
-			'name' => 'JPEG quality',
-			'type' => 'number',
-			'defaultValue' => 75
-		)
-	));
+    const PARAMETERS = array(array(
+        'u' => array(
+            'name' => 'Filter by username (optional)',
+            'exampleValue' => 'danielsessler',
+            'type' => 'text',
+            'defaultValue' => ''
+        ),
+        'm' => array(
+            'name' => 'Max number of photos',
+            'type' => 'number',
+            'defaultValue' => 20,
+            'required' => true
+        ),
+        'prev_q' => array(
+            'name' => 'Preview quality',
+            'type' => 'list',
+            'values' => array(
+                'full' => 'full',
+                'regular' => 'regular',
+                'small' => 'small',
+                'thumb' => 'thumb',
+            ),
+            'defaultValue' => 'regular',
+            'required' => true
+        ),
+        'w' => array(
+            'name' => 'Max download width (optional)',
+            'exampleValue' => 1920,
+            'type' => 'number',
+        ),
+        'jpg_q' => array(
+            'name' => 'Max JPEG quality (optional)',
+            'exampleValue' => 75,
+            'type' => 'number',
+        )
+    ));
 
-	public function collectData(){
-		$width = $this->getInput('w');
-		$max = $this->getInput('m');
-		$quality = $this->getInput('q');
+    public function collectData()
+    {
+        $filteredUser = $this->getInput('u');
+        $width = $this->getInput('w');
+        $max = $this->getInput('m');
+        $previewQuality = $this->getInput('prev_q');
+        $jpgQuality = $this->getInput('jpg_q');
 
-		$api_response = getContents('https://unsplash.com/napi/photos?page=1&per_page=' . $max)
-			or returnServerError('Could not request Unsplash API.');
-		$json = json_decode($api_response, true);
+        $url = 'https://unsplash.com/napi';
+        if (strlen($filteredUser) > 0) $url .= '/users/' . $filteredUser;
+        $url .= '/photos?page=1&per_page=' . $max;
 
-		foreach ($json as $json_item) {
-			$item = array();
+        $api_response = getContents($url)
+        or returnServerError('Could not request Unsplash API.');
+        $json = json_decode($api_response, true);
 
-			// Get image URI
-			$uri = $json_item['urls']['regular'] . '.jpg'; // '.jpg' only for format hint
-			$uri = str_replace('q=80', 'q=' . $quality, $uri);
-			$uri = str_replace('w=1080', 'w=' . $width, $uri);
-			$item['uri'] = $uri;
+        foreach ($json as $json_item) {
+            $item = array();
 
-			// Get title from description
-			if (is_null($json_item['alt_description'])) {
-				if (is_null($json_item['description'])) {
-					$item['title'] = 'Unsplash picture from ' . $json_item['user']['name'];
-				} else {
-					$item['title'] = $json_item['description'];
-				}
-			} else {
-				$item['title'] = $json_item['alt_description'];
-			}
+            // Get image URI
+            $uri = $json_item['urls']['raw'] . '&fm=jpg';
+            if ($jpgQuality > 0) $uri .= '&q=' . $jpgQuality;
+            if ($width > 0) $uri .= '&w=' . $width . '&fit=max';
+            $uri .= '.jpg'; // only for format hint
+            $item['uri'] = $uri;
 
-			$item['timestamp'] = time();
-			$item['content'] = $item['title']
-				. '<br><a href="'
-				. $item['uri']
-				. '"><img src="'
-				. $json_item['urls']['thumb']
-				. '" /></a>';
+            // Get title from description
+            if (is_null($json_item['description'])) {
+                $item['title'] = 'Unsplash picture from ' . $json_item['user']['name'];
+            } else {
+                $item['title'] = $json_item['description'];
+            }
 
-			$this->items[] = $item;
-		}
-	}
+            $item['timestamp'] = $json_item['created_at'];
+
+            $content = 'User: <a href="'
+                . $json_item['user']['links']['html']
+                . '">@'
+                . $json_item['user']['username']
+                . '</a>';
+            if (!is_null($json_item['location']['name'])) {
+                $content .= ' | Location: ' . $json_item['location']['name'];
+            }
+            $content .= ' | Image on <a href="'
+                . $json_item['links']['html']
+                .'">Unsplash</a><br>'
+                . '<a href="'
+                . $uri
+                . '"><img src="'
+                . $json_item['urls'][$previewQuality]
+                . '" /></a>';
+            $item['content'] = $content;
+
+            $this->items[] = $item;
+        }
+    }
+
+    public function getName(){
+        $filteredUser = $this->getInput('u');
+        if (strlen($filteredUser) > 0){
+            return $filteredUser . ' - ' . self::NAME;
+        } else {
+            return self::NAME;
+        }
+    }
 }
