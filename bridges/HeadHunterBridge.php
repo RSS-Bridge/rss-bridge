9c7059e90f9294814b9676a7b12d104ffc98792e
Eugene Molotov <em92@users.noreply.github.com>
2020-10-19 16:19:21 +0500

    [HeadHunterBridge] New bridge

---
+++ bridges/HeadHunterBridge.php
@@ -0,0 +1,44 @@
+<?php
+class HeadHunterBridge extends FeedExpander {
+
+	const NAME = 'HeadHunter';
+	const DESCRIPTION = 'Расширяет ленту путем добавления полного описания вакансии';
+	const MAINTAINER = 'em92';
+
+	const PARAMETERS = array(
+		array('url' => array(
+			'name' => 'Ссылка на ленту',
+			'required' => true,
+		)),
+	);
+
+	protected function parseItem($newItem) {
+		$item = parent::parseItem($newItem);
+
+		$html = getSimpleHTMLDOMCached($item['uri']);
+
+		$vacancy_header = $html->find('.vacancy-salary', 0)->outertext;
+		$vacancy_company = $html->find('.vacancy-company-wrapper', 0)->outertext;
+		$vacancy_description = $html->find('.vacancy-description', 0);
+
+		$unwanted_element_selectors = array('script', 'style', '.tmpl_hh_head', '.tmpl_hh_slider');
+		foreach($unwanted_element_selectors as $unwanted_element_selector) {
+			foreach($vacancy_description->find($unwanted_element_selector) as $el) {
+				$el->outertext = '';
+			}
+		}
+		$vacancy_description = $vacancy_description->outertext;
+
+		$content = $vacancy_header . $vacancy_company . $vacancy_description;
+
+		$item['content'] = $content;
+		return $item;
+
+	}
+
+	public function collectData(){
+		// TODO: check if host ends with .hh.ru
+		$this->collectExpandableDatas($this->getInput('url'));
+	}
+
+}
