ff50e4918c6fb5ffbf0d11dd378c3a68ace535ed
Eugene Molotov <em92@users.noreply.github.com>
2020-11-10 16:26:08 +0500

    Bump version to dev.2020-11-10

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -28,7 +28,7 @@ final class Configuration {
 	 *
 	 * @todo Replace this property by a constant.
 	 */
-	public static $VERSION = 'dev.2020-02-26';
+	public static $VERSION = 'dev.2020-11-10';
 
 	/**
 	 * Holds the configuration data.
