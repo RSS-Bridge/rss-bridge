a04d8277002b3165eebf65c91fd055eca0ff27cf
Jim <hoaianro@gmail.com>
2020-11-30 15:37:24 +0700

    Fix "Cannot read property 'match' of null"

--- static/search.js
+++ static/search.js
@@ -1,60 +1,54 @@
 function search() {
+  var searchTerm = document.getElementById("searchfield").value;
+  var searchableElements = document.getElementsByTagName("section");
 
-	var searchTerm = document.getElementById('searchfield').value;
-	var searchableElements = document.getElementsByTagName('section');
+  var regexMatch = new RegExp(searchTerm, "i");
 
-	var regexMatch = new RegExp(searchTerm, 'i');
+  // Attempt to create anchor from search term (will default to 'localhost' on failure)
+  var searchTermUri = document.createElement("a");
+  searchTermUri.href = searchTerm;
 
-	// Attempt to create anchor from search term (will default to 'localhost' on failure)
-	var searchTermUri = document.createElement('a');
-	searchTermUri.href = searchTerm;
+  if (searchTermUri.hostname == "localhost") {
+    searchTermUri = null;
+  } else {
+    // Ignore "www."
+    if (searchTermUri.hostname.indexOf("www.") === 0) {
+      searchTermUri.hostname = searchTermUri.hostname.substr(4);
+    }
+  }
 
-	if(searchTermUri.hostname == 'localhost') {
-		searchTermUri = null;
-	} else {
+  for (var i = 0; i < searchableElements.length; i++) {
+    if (
+      searchableElements[i].className == "searchbar" ||
+      searchableElements[i].className == "footer"
+    ) {
+      continue; // Ignore the search bar and footer section.
+    }
+    var textValue = searchableElements[i].getAttribute("data-ref");
+    var anchors = searchableElements[i].getElementsByTagName("a");
 
-		// Ignore "www."
-		if(searchTermUri.hostname.indexOf('www.') === 0) {
-			searchTermUri.hostname = searchTermUri.hostname.substr(4);
-		}
+    if (anchors != null && anchors.length > 0) {
+      var uriValue = anchors[0]; // First anchor is bridge URI
 
-	}
-
-	for(var i = 0; i < searchableElements.length; i++) {
-
-		var textValue = searchableElements[i].getAttribute('data-ref');
-		var anchors = searchableElements[i].getElementsByTagName('a');
-
-		if(anchors != null && anchors.length > 0) {
-
-			var uriValue = anchors[0]; // First anchor is bridge URI
-
-			// Ignore "www."
-			if(uriValue.hostname.indexOf('www.') === 0) {
-				uriValue.hostname = uriValue.hostname.substr(4);
-			}
-
-		}
-
-		if(textValue != null || uriValue != null) {
-
-			if(textValue.match(regexMatch) != null ||
-				uriValue.hostname.match(regexMatch) ||
-				searchTermUri != null &&
-				uriValue.hostname != 'localhost' && (
-					uriValue.href.match(regexMatch) != null ||
-					uriValue.hostname == searchTermUri.hostname)) {
-
-				searchableElements[i].style.display = 'block';
-
-			} else {
-
-				searchableElements[i].style.display = 'none';
-
-			}
-
-		}
-
-	}
-
+      // Ignore "www."
+      if (uriValue.hostname.indexOf("www.") === 0) {
+        uriValue.hostname = uriValue.hostname.substr(4);
+      }
+    }
+    console.log(textValue);
+    if (textValue != null || uriValue != null) {
+      if (
+        textValue.match(regexMatch) != null ||
+        uriValue.hostname.match(regexMatch) ||
+        (searchTermUri != null &&
+          uriValue.hostname != "localhost" &&
+          (uriValue.href.match(regexMatch) != null ||
+            uriValue.hostname == searchTermUri.hostname))
+      ) {
+        searchableElements[i].style.display = "block";
+      } else {
+        searchableElements[i].style.display = "none";
+      }
+    }
+  }
 }
