64be1d01709c9da969d1525f78da36a77c5100ea
Jan-Lukas Else <jlelse@users.noreply.github.com>
2020-12-22 09:37:12 +0100

    Use tabs

--- bridges/HetznerBridge.php
+++ bridges/HetznerBridge.php
@@ -10,26 +10,24 @@ class HetznerBridge extends BridgeAbstract {
 	const PARAMETERS = array();
 
 	public function collectData(){
-
-        $html = getSimpleHTMLDOM(self::URI) or returnServerError('Could not request Hetzner News: ' . self::URI);
-
-        foreach(array_slice($html->find('ul.list-unstyled.list-card li'), 0, 5) as $element) {
-            $url = 'https://www.hetzner.com' . $element->find('a', 0)->href;
-            $title = $element->find('h4', 0)->plaintext;
-            $date = strtotime($element->find('p small', 0)->plaintext);
-
-            $contenthtml = getSimpleHTMLDOM($url) or returnServerError('Could not request Hetzner News: ' . $url);
-            $content = $contenthtml->find('ul.list-unstyled.list-card li p');
-            array_shift($content);
-            array_shift($content);
-
-            $item = array();
-            $item['uri'] = $url;
-            $item['title'] = $title;
-            $item['author'] = 'Hetzner';
-            $item['timestamp'] = $date;
-            $item['content'] = implode('', $content);
-            $this->items[] = $item;
-        }
+		$html = getSimpleHTMLDOM(self::URI) or returnServerError('Could not request Hetzner News: ' . self::URI);
+		foreach(array_slice($html->find('ul.list-unstyled.list-card li'), 0, 5) as $element) {
+			$url = 'https://www.hetzner.com' . $element->find('a', 0)->href;
+			$title = $element->find('h4', 0)->plaintext;
+			$date = strtotime($element->find('p small', 0)->plaintext);
+			
+			$contenthtml = getSimpleHTMLDOM($url) or returnServerError('Could not request Hetzner News: ' . $url);
+			$content = $contenthtml->find('ul.list-unstyled.list-card li p');
+			array_shift($content);
+			array_shift($content);
+			
+			$item = array();
+			$item['uri'] = $url;
+			$item['title'] = $title;
+			$item['author'] = 'Hetzner';
+			$item['timestamp'] = $date;
+			$item['content'] = implode('', $content);
+			$this->items[] = $item;
+		}
 	}
 }
