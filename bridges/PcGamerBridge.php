7d0be2431b96dd10ba31e251e5289a76ab8a6946
Matt DeMoss <demoss.matt@gmail.com>
2021-02-14 16:51:27 -0500

    revert and fetch only metadata

--- bridges/PcGamerBridge.php
+++ bridges/PcGamerBridge.php
@@ -2,43 +2,22 @@
 class PcGamerBridge extends BridgeAbstract
 {
 	const NAME = 'PC Gamer';
-	const URI = 'https://www.pcgamer.com/archive/';
+	const URI = 'https://www.pcgamer.com/';
 	const DESCRIPTION = 'PC Gamer Most Read Stories';
-	const CACHE_TIMEOUT = 3600;
-	const MAINTAINER = 'IceWreck, mdemoss';
+	const MAINTAINER = 'mdemoss';
 
 	public function collectData()
 	{
 		$html = getSimpleHTMLDOMCached($this->getURI(), 300);
-		$stories = $html->find('ul.basic-list li.day-article');
-		$i = 0;
-		// Find induvidual stories in the archive page
+		$stories = $html->find('div#popularcontent li.most-popular-item');
 		foreach ($stories as $element) {
-			if($i == 15) break;
 			$item['uri'] = $element->find('a', 0)->href;
-			// error_log(print_r($item['uri'], TRUE));
 			$articleHtml = getSimpleHTMLDOMCached($item['uri']);
-			$item['title'] = $element->find('a', 0)->plaintext;
+			$item['title'] = $articleHtml->find('meta[property=og:title]', 0)->content;
 			$item['timestamp'] = strtotime($articleHtml->find('meta[name=pub_date]', 0)->content);
-			$item['author'] = $articleHtml->find('span.by-author a', 0)->plaintext;
-
-			// Get the article content
-			$articleContents = $articleHtml->find('#article-body', 0);
-
-			/*
-				By default the img src has a link to an error image and then the actual image
-				is added in by JS. So we replace the error image with the actual full size image
-				whoose link is in one of the attributes of the img tag
-			*/
-			foreach($articleContents->find('img') as $img) {
-				$imgsrc = $img->getAttribute('data-original-mos');
-				// error_log($imgsrc);
-				$img->src = $imgsrc;
-			}
-
-			$item['content'] = $articleContents;
+			$item['content'] = $articleHtml->find('meta[property=og:description]', 0)->content;
+			$item['author'] = $articleHtml->find('meta[name=parsely-author', 0)->content;
 			$this->items[] = $item;
-			$i++;
 		}
 	}
 }
