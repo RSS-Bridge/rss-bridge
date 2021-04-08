b23e0f2e2fc42f15e451804cc84c241a0ae9b978
Mthmgcn05 <72888828+Mthmgcn05@users.noreply.github.com>
2021-04-08 16:53:21 -0500

    Update FacebookBridge.php

--- bridges/FacebookBridge.php
+++ bridges/FacebookBridge.php
@@ -4,7 +4,7 @@ class FacebookBridge extends BridgeAbstract {
 	const MAINTAINER = 'teromene, logmanoriginal';
 	const NAME = 'Facebook Bridge | Main Site';
 	const URI = 'https://www.facebook.com/';
-	const CACHE_TIMEOUT = 86400; // 24hrs
+	const CACHE_TIMEOUT = 43200; // 12hrs
 	const DESCRIPTION = 'Input a page title or a profile log. For a profile log,
  please insert the parameter as follow : myExamplePage/132621766841117';
 
