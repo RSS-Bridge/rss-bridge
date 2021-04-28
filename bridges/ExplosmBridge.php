07262b3feb16b562dc7d41a722cd2c14468c5072
Henning Bocklage <henning@bocklage.com>
2021-04-28 21:57:44 +0200

    [Explosm] Fixed indenting

--- bridges/ExplosmBridge.php
+++ bridges/ExplosmBridge.php
@@ -11,12 +11,12 @@ class ExplosmBridge extends FeedExpander {
 		$this->collectExpandableDatas('https://feeds.feedburner.com/Explosm');
 	}
 
-    protected function parseItem($feedItem){
-        $item = parent::parseItem($feedItem);
+	protected function parseItem($feedItem){
+		$item = parent::parseItem($feedItem);
 		$comicpage = getSimpleHTMLDOM($item['uri']);
 		$image = $comicpage->find('div[id=comic-wrap]', 0)->find('img', 0)->getAttribute('src');
-        $item['content'] = '<img src="http:' . $image . '" />';
-    
-        return $item;
+		$item['content'] = '<img src="http:' . $image . '" />';
+
+		return $item;
     }
 }
