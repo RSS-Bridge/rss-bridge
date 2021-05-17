5b9e4cf6882dbeb3aecf5ddd7f78a7ee035882bf
dotter-ak <github.com@dott.su>
2021-05-17 22:43:09 +0300

    Update Drive2ruBridge.php

--- bridges/Drive2ruBridge.php
+++ bridges/Drive2ruBridge.php
@@ -16,15 +16,23 @@ class Drive2ruBridge extends BridgeAbstract {
 			),
 		)
 	);
-
+	
+	private $title;
+	
 	public function collectData(){
+		$url = $this->getInput('url');
+		$validUrl = '/^https:\/\/www.drive2.ru\/experience/';
+		if (!preg_match($validUrl, $url)) {
+			returnServerError('Invalid url');
+		}
 		$html = getSimpleHTMLDOM($this->getInput('url'));
+		$this->title = $html->find('title', 0)->innertext;
 		$articles = $html->find('div.js-entity');
 		foreach ($articles as $article) {
 			$item = array();
 			$item['title'] = $article->find('a.c-link--text', 0)->plaintext;
 			$item['uri'] = self::URI . $article->find('a.c-link--text', 0)->href;
-			$item['content'] = str_replace("<button class=\"c-post-preview__more r-button-unstyled c-link c-link--text\" data-action=\"post.show\" data-ym-target=\"post_read\">Читать дальше</button>","",$article->find('div.c-post-preview__lead', 0)) ."<br><a href=\"".$item['uri']."\">Читать далее</a>";
+			$item['content'] = str_replace('<button class="c-post-preview__more r-button-unstyled c-link c-link--text" data-action="post.show" data-ym-target="post_read">Читать дальше</button>', '', $article->find('div.c-post-preview__lead', 0)) . '<br><a href="' . $item['uri'] . '">Читать далее</a>';
 			$item['author'] = $article->find('a.c-username--wrap', 0)->plaintext;
 			$item['enclosures'][] = $article->find('img', 1)->src;
 			$this->items[] = $item;
@@ -32,11 +40,7 @@ class Drive2ruBridge extends BridgeAbstract {
 	}
 
 	public function getName() {
-		if($this->getInput('url')) {
-			$html = getSimpleHTMLDOM($this->getInput('url'));
-			return $html->find('title', 0)->plaintext;
-		}
-		return parent::getName();
+		return $this->title ?: parent::getName();
 	}
 
 	public function getIcon() {
