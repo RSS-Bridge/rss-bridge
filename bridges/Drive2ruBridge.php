5b052c67285396739ab58e05cbb260f52bd6f7c9
dotter-ak <github.com@dott.su>
2021-05-18 22:07:05 +0300

    Update Drive2ruBridge.php

--- bridges/Drive2ruBridge.php
+++ bridges/Drive2ruBridge.php
@@ -33,23 +33,20 @@ class Drive2ruBridge extends BridgeAbstract {
 		foreach ($articles as $article) {
 			$item = array();
 			$item['title'] = $article->find('a.c-link--text', 0)->plaintext;
-			$item['uri'] = self::URI . $article->find('a.c-link--text', 0)->href;
+			$item['uri'] = urljoin(self::URI, $article->find('a.c-link--text', 0)->href);
 			if($this->getInput('full_articles')) {
 				$content = getSimpleHTMLDOM($item['uri'])->find('div.c-post__body', 0);
-				foreach($content->find('div') as $div)
-					foreach ($div->getAllAttributes() as $attr => $val)
-						$div->removeAttribute($attr);
-				foreach($content->find('span') as $span)
-					foreach ($span->getAllAttributes() as $attr => $val)
-						$span->removeAttribute($attr);
+			foreach($content->find('div, span') as $ds)
+					foreach ($ds->getAllAttributes() as $attr => $val)
+						$ds->removeAttribute($attr);
 				foreach ($content->find('script') as $node)
 					$node->outertext = '';
 				foreach ($content->find('iframe') as $node)
-					$node->outertext = '<a href="' . $node->src.'">' . $node->src . '</a>';
+					$node->outertext = '<a href="' . $node->src . '">' . $node->src . '</a>';
 				$item['content'] = $content->innertext;
 			} else {
 				$content = $article->find('div.c-post-preview__lead', 0);
-				if (!is_null($content)) 
+				if (!is_null($content))
 					$item['content'] = preg_replace('!\s+!', ' ', str_replace('Читать дальше', '', $content->plaintext)) .
 						'<br><a href="' . $item['uri'] . '">Читать далее</a>';
 				else $item['content'] = '';
