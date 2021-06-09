b3bc2d7247f181bde442302dd3b1aadf56447971
Jim <hoaianro@gmail.com>
2021-06-09 13:26:16 +0700

    [ReutersBridge] Add support for Youtube, Instagram embed.
    - Use switch case instead of if else on line 212.
    - Add support for Youtube, Instagram embed.

--- bridges/ReutersBridge.php
+++ bridges/ReutersBridge.php
@@ -209,16 +209,32 @@ class ReutersBridge extends BridgeAbstract
 					$media_type = $content['media_type'];
 					$cid = $content['cid'];
 					$embed = '';
-					if($media_type == 'tweet') {
-						$embed .= "<iframe 
-													src=\"https://platform.twitter.com/embed/Tweet.html?id=$cid\" 
-													title=\"Twitter Tweet\"
-													scrolling=\"no\" 
+					switch ($media_type) {
+						case 'tweet':
+							$embed .= "<iframe 
+														src=\"https://platform.twitter.com/embed/Tweet.html?id=$cid\" 
+														title=\"Twitter Tweet\"
+														scrolling=\"no\" 
+														frameborder=\"0\" 
+														allowtransparency=\"true\" 
+														allowfullscreen=\"true\" 
+														style=\"width: 550px;height: 225px;\">
+												</iframe>";
+							break;
+						case 'instagram':
+							$embed .= "<img 
+													src=\"https://instagram.com/p/$cid/media/?size=l\" 
+													alt=\"instagram-image-$cid\"
+												>";
+							break;
+						case 'youtube':
+							$embed .= "<‌iframe 
+													width=\"560\" 
+													height=\"315\" 
+													src=\"https://www.youtube.com/embed/MOKiCxV2tMc\" 
 													frameborder=\"0\" 
-													allowtransparency=\"true\" 
-													allowfullscreen=\"true\" 
-													style=\"width: 550px;height: 225px;\">
-											</iframe>";
+													allowfullscreen>";
+							break;
 					}
 					$description .= $embed;
 			}
