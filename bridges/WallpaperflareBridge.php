341c6aad4e988df8a828443e6c579e09e221abe6
dhuschde <85834680+dhuschde@users.noreply.github.com>
2021-06-23 14:08:06 +0200

    Add files via upload
    Added Wallpaperflare Bridge

---
+++ bridges/WallpaperflareBridge.php
@@ -0,0 +1,39 @@
+<?php
+
+class WallpaperflareBridge extends XPathAbstract {
+
+	const NAME = 'Wallpaperflare';
+	const URI = 'https://wallpaperflare.com';
+	const DESCRIPTION = 'Wallpapers from Wallpaperflare';
+	const MAINTAINER = 'dhuschde';
+	const PARAMETERS = array(
+		'' => array(
+			'search' => array(
+				'name' => 'Search',
+	                        'required' => true
+                )
+        ));
+
+
+	const CACHE_TIMEOUT = 3600;
+
+	const XPATH_EXPRESSION_ITEM = './/figure';
+	const XPATH_EXPRESSION_ITEM_TITLE = './/img/@title';
+	const XPATH_EXPRESSION_ITEM_CONTENT = '';
+	const XPATH_EXPRESSION_ITEM_URI = './/a[@itemprop="url"]/@href';
+	const XPATH_EXPRESSION_ITEM_AUTHOR = '/html[1]/body[1]/main[1]/section[1]/h1[1]';
+	const XPATH_EXPRESSION_ITEM_TIMESTAMP = 'N/A';
+	const XPATH_EXPRESSION_ITEM_ENCLOSURES = './/img[@class="lazy"]/@data-src';
+	const XPATH_EXPRESSION_ITEM_CATEGORIES = './/figcaption[@itemprop="caption description"]';
+	const SETTING_FIX_ENCODING = true;
+
+	/**
+	 * Source Web page URL (should provide either HTML or XML content)
+	 * @return string
+	 */
+	protected function getSourceUrl(){
+
+		$search = $this->getInput('search');
+		return 'https://www.wallpaperflare.com/search?wallpaper=' . $search;
+	}
+}
