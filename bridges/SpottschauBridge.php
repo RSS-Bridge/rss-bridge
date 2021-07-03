59204db6a2013b5977bf0e989680a54fb195a947
Max <msal.coding@gmail.com>
2021-07-03 03:15:24 +0200

    [SpottschauBridge] New bridge

---
+++ bridges/SpottschauBridge.php
@@ -0,0 +1,30 @@
+<?php
+class SpottschauBridge extends BridgeAbstract {
+	const NAME = 'Härringers Spottschau Bridge';
+	const URI = 'https://spottschau.com/';
+	const DESCRIPTION = 'Der Fußball-Comic';
+	const MAINTAINER = 'sal0max';
+	const PARAMETERS = array();
+
+	const CACHE_TIMEOUT = 3600; // 1 hour
+
+	public function collectData() {
+		$html = getSimpleHTMLDOM(self::URI)
+			or returnServerError('Could not request: ' . self::URI);
+
+		$item = array();
+		$item['uri'] = self::URI . $html->find('div.strip>a', 0)->attr['href'];
+		$item['title'] = $html->find('div.text>h2', 0)->innertext;
+		$item['timestamp'] = $item['title'];
+
+		$image = $html->find('div.strip>a>img', 0);
+		$imageUrl = self::URI . $image->attr['src'];
+		$imageAlt = $image->attr['alt'];
+
+		$item['content'] = <<<EOD
+<img src="{$imageUrl}" alt="{$imageAlt}"/>
+<br/>
+EOD;
+		$this->items[] = $item;
+	}
+}
