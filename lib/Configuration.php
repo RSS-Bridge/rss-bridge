b831e5a538cb99881f749d7c36c2173ac00a23dc
Eugene Molotov <em92@users.noreply.github.com>
2021-07-28 00:19:06 +0500

    drop php 5.6 and php 7.0 support. drop phpunit 6 in unit tests

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -79,8 +79,8 @@ final class Configuration {
 	public static function verifyInstallation() {
 
 		// Check PHP version
-		if(version_compare(PHP_VERSION, '5.6.0') === -1)
-			self::reportError('RSS-Bridge requires at least PHP version 5.6.0!');
+		if(version_compare(PHP_VERSION, '7.1.0') === -1)
+			self::reportError('RSS-Bridge requires at least PHP version 7.1.0!');
 
 		// extensions check
 		if(!extension_loaded('openssl'))
