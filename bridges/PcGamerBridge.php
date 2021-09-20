6c1f86dc6d94244116a0e336e233172a36f3480b
Matt DeMoss <demoss.matt@gmail.com>
2021-09-20 18:10:03 -0400

    remove 2 tags that don't make sense

--- bridges/PcGamerBridge.php
+++ bridges/PcGamerBridge.php
@@ -20,7 +20,12 @@ class PcGamerBridge extends BridgeAbstract
 			$item['content'] = html_entity_decode($articleHtml->find('meta[name=description]', 0)->content);
 			$item['author'] = $articleHtml->find('meta[name=parsely-author]', 0)->content;
 			$item['enclosures'][] = $articleHtml->find('meta[name=parsely-image-url]', 0)->content;
-			$item['categories'] = explode(",", $articleHtml->find('meta[name=parsely-tags]', 0)->content);
+			/* I don't know why every article has two extra tags, but because 
+			one matches another common tag, "guide," it needs to be removed. */ 
+			$item['categories'] = array_diff(
+				explode(",", $articleHtml->find('meta[name=parsely-tags]', 0)->content),
+				["van_buying_guide_progressive", "serversidehawk"] 
+			);
 			$item['timestamp'] = strtotime($articleHtml->find('meta[name=pub_date]', 0)->content);
 			$this->items[] = $item;
 		}
