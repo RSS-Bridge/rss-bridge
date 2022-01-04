2908a1fbf6e9889e8dabb340ce42e88734259d4e
somini <somini@users.noreply.github.com>
2022-01-04 22:59:03 +0000

    [ComboiosDePortugalBridge]: Temporarily ignore certificate checks
    Fix #2397

--- bridges/ComboiosDePortugalBridge.php
+++ bridges/ComboiosDePortugalBridge.php
@@ -7,7 +7,11 @@ class ComboiosDePortugalBridge extends BridgeAbstract {
 	const MAINTAINER = 'somini';
 
 	public function collectData() {
-		$html = getSimpleHTMLDOM($this->getURI() . '/consultar-horarios/avisos');
+		# Do not verify SSL certificate (the server doesn't send the intermediate)
+		# https://github.com/RSS-Bridge/rss-bridge/issues/2397
+		$html = getSimpleHTMLDOM($this->getURI() . '/consultar-horarios/avisos', [], [
+			CURLOPT_SSL_VERIFYPEER => 0,
+		]);
 
 		foreach($html->find('.warnings-table a') as $element) {
 			$item = array();
