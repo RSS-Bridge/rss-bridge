2e889556489fa267033fb66a8985f8ee2eb915d1
Tomasz Kane <tomasz.kane@gmail.com>
2022-03-09 22:03:21 +0100

    [CdactionBridge] Add missing channels (#2477)

--- bridges/CdactionBridge.php
+++ bridges/CdactionBridge.php
@@ -2,21 +2,50 @@
 
 class CdactionBridge extends BridgeAbstract {
 	const NAME = 'CD-ACTION bridge';
-	const URI = 'https://cdaction.pl/newsy';
-	const DESCRIPTION = 'Fetches the latest news.';
+	const URI = 'https://cdaction.pl';
+	const DESCRIPTION = 'Fetches the latest posts from given category.';
 	const MAINTAINER = 'tomaszkane';
+	const PARAMETERS = array( array(
+		'category' => array(
+			'name' => 'Kategoria',
+			'type' => 'list',
+			'values' => array(
+				'Najnowsze (wszystkie)' => 'najnowsze',
+				'Newsy' => 'newsy',
+				'Recenzje' => 'recenzje',
+				'Teksty' => array(
+					'Publicystyka' => 'publicystyka',
+					'Zapowiedzi' => 'zapowiedzi',
+					'Już graliśmy' => 'juz-gralismy',
+					'Poradniki' => 'poradniki',
+				),
+				'Kultura' => 'kultura',
+				'Wideo' => 'wideo',
+				'Czasopismo' => 'czasopismo',
+				'Technologie' => array(
+					'Artykuły' => 'artykuly',
+					'Testy' => 'testy',
+				),
+				'Na luzie' => array(
+					'Konkursy' => 'konkursy',
+					'Nadgodziny' => 'nadgodziny',
+				)
+			)
+		))
+	);
 
 	public function collectData() {
-		$html = getSimpleHTMLDOM($this->getURI());
+		$html = getSimpleHTMLDOM($this->getURI() . '/' . $this->getInput('category'));
 
 		$newsJson = $html->find('script#__NEXT_DATA__', 0)->innertext;
 		if (!$newsJson = json_decode($newsJson)) {
 			return;
 		}
 
-		foreach ($newsJson->props->pageProps->dehydratedState->queries[1]->state->data->results as $news) {
+		$queriesIndex = $this->getInput('category') === 'najnowsze' ? 0 : 1;
+		foreach ($newsJson->props->pageProps->dehydratedState->queries[$queriesIndex]->state->data->results as $news) {
 			$item = array();
-			$item['uri'] = $this->getURI() . '/' . $news->slug;
+			$item['uri'] = $this->getURI() . '/' . $news->category->slug . '/' . $news->slug;
 			$item['title'] = $news->title;
 			$item['timestamp'] = $news->publishedAt;
 			$item['author'] = $news->editor->fullName;
