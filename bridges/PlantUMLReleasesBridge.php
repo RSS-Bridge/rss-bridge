366961d768c5f5bbedaafed9ac532f11ebbfd5fa
Yaman Qalieh <ybq987@gmail.com>
2022-03-25 10:31:49 -0400

    Fix issues from review

--- bridges/PlantUMLReleasesBridge.php
+++ bridges/PlantUMLReleasesBridge.php
@@ -13,6 +13,7 @@ class PlantUMLReleasesBridge extends BridgeAbstract {
 
 	const CACHE_TIMEOUT = 7200; // 2h
 	const DESCRIPTION = 'PlantUML releases bridge, showing for each release the changelog';
+	const ITEM_LIMIT = 10;
 
 	public function getURI() {
 		return self::URI;
@@ -21,14 +22,19 @@ class PlantUMLReleasesBridge extends BridgeAbstract {
 	public function collectData() {
 		$html = getSimpleHTMLDOM($this->getURI());
 
+		$num_items = 0;
 		$main = $html->find('div[id=root]', 0);
 		foreach ($main->find('h2') as $release) {
+			// Limit to $ITEM_LIMIT number of results
+			if ($num_items++ >= self::ITEM_LIMIT) {
+				break;
+			}
 			$item = array();
 			$item['author'] = self::AUTHOR;
 			$release_text = $release->innertext;
 			if (preg_match('/(.+) \((.*)\)/', $release_text, $matches)) {
 				$item['title'] = $matches[1];
-				$item['timestamp'] = $matches[2];
+				$item['timestamp'] = preg_replace('/(\d+) (\w{3})\w*, (\d+)/', '${1} ${2} ${3}', $matches[2]);
 			} else {
 				$item['title'] = $release_text;
 			}
