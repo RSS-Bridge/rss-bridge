972fa1354747a704ec2951878d68e127e33c47c3
Yaman Qalieh <ybq987@gmail.com>
2022-04-01 15:08:20 -0400

    [ParksOnTheAirBridge] Fix links

--- bridges/ParksOnTheAirBridge.php
+++ bridges/ParksOnTheAirBridge.php
@@ -3,7 +3,8 @@
 class ParksOnTheAirBridge extends BridgeAbstract {
 	const MAINTAINER = 's0lesurviv0r';
 	const NAME = 'Parks On The Air Spots';
-	const URI = 'https://api.pota.app/spot/activator';
+	const URI = 'https://pota.app';
+	const API_URI = 'https://api.pota.app/spot/activator';
 	const CACHE_TIMEOUT = 60; // 1m
 	const DESCRIPTION = 'Parks On The Air Activator Spots';
 
@@ -11,7 +12,7 @@ class ParksOnTheAirBridge extends BridgeAbstract {
 
 		$header = array('Content-type:application/json');
 		$opts = array(CURLOPT_HTTPGET => 1);
-		$json = getContents($this->getURI(), $header, $opts);
+		$json = getContents(self::API_URI, $header, $opts);
 
 		$spots = json_decode($json, true);
 
@@ -20,7 +21,7 @@ class ParksOnTheAirBridge extends BridgeAbstract {
 				$spot['frequency'] . ' kHz';
 
 			$content = <<<EOL
-<a href="https://pota.us/#/parks/{$spot['reference']}">
+<a href="https://pota.app/#/park/{$spot['reference']}">
 {$spot['reference']}, {$spot['name']}</a><br />
 Location: {$spot['locationDesc']}<br />
 Frequency: {$spot['frequency']} kHz<br />
@@ -29,7 +30,7 @@ Comments: {$spot['comments']}
 EOL;
 
 			$this->items[] = array(
-				'uri' => 'https://pota.us/#/',
+				'uri' => 'https://pota.app/#/park/' . $spot['reference'],
 				'title' => $title,
 				'content' => $content,
 				'timestamp' => $spot['spotTime']
