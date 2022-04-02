94004c5104de6b84bd4c72356806d994c05a0119
somini <somini@users.noreply.github.com>
2022-04-02 20:31:52 +0100

    Configure a default fallback for getInput function

--- lib/BridgeAbstract.php
+++ lib/BridgeAbstract.php
@@ -291,11 +291,12 @@ abstract class BridgeAbstract implements BridgeInterface {
 	 * Returns the value for the provided input
 	 *
 	 * @param string $input The input name
-	 * @return mixed|null The input value or null if the input is not defined
+	 * @param string $default The default value, when input is not defined. Default to null
+	 * @return mixed The input value or the default value if the input is not defined
 	 */
-	protected function getInput($input){
+	protected function getInput($input, $default = null){
 		if(!isset($this->inputs[$this->queriedContext][$input]['value'])) {
-			return null;
+			return $default;
 		}
 		return $this->inputs[$this->queriedContext][$input]['value'];
 	}
