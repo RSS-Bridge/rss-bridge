9b07a5097c4c32715838d6d3a482ad6acbe827fa
User123698745 <usr123698745+git@gmail.com>
2022-04-04 20:40:20 +0200

    [GiphyBridge] use not rate limited public api key

--- bridges/GiphyBridge.php
+++ bridges/GiphyBridge.php
@@ -56,12 +56,15 @@ HTML
 
 	public function collectData() {
 		/**
-		 * This uses a public beta key which has severe rate limiting.
+		 * This uses Giphy's own undocumented public prod api key,
+		 * which should not have any rate limiting.
+		 * There is a documented public beta api key (dc6zaTOxFJmzC),
+		 * but it has severe rate limiting.
 		 *
 		 * https://giphy.api-docs.io/1.0/welcome/access-and-api-keys
 		 * https://giphy.api-docs.io/1.0/gifs/search-1
 		 */
-		$apiKey = 'dc6zaTOxFJmzC';
+		$apiKey = 'Gc7131jiJuvI7IdN0HZ1D7nh0ow5BU6g';
 		$limit = min($this->getInput('n') ?: 10, 50);
 		$endpoints = array();
 		if (empty($this->getInput('noGif'))) {
