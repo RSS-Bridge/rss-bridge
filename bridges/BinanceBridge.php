0f4fa80dc8c5e26426bb6b9d782d750ef97c2954
Tobias Alexander Franke <tobias.franke@siggraph.org>
2022-04-05 17:51:25 +0200

    [BinanceBridge] Remove announcements category (because of Cloudflare)

--- bridges/BinanceBridge.php
+++ bridges/BinanceBridge.php
@@ -2,40 +2,25 @@
 class BinanceBridge extends BridgeAbstract {
 	const NAME = 'Binance';
 	const URI = 'https://www.binance.com';
-	const DESCRIPTION = 'Subscribe to the Binance blog or the Binance Zendesk announcements.';
+	const DESCRIPTION = 'Subscribe to the Binance blog.';
 	const MAINTAINER = 'thefranke';
 	const CACHE_TIMEOUT = 3600; // 1h
-
-	const PARAMETERS = array( array(
-		'category' => array(
-			'name' => 'category',
-			'type' => 'list',
-			'exampleValue' => 'Blog',
-			'title' => 'Select a category',
-			'values' => array(
-				'Blog' => 'Blog',
-				'Announcements' => 'Announcements'
-			)
-		)
-	));
 
 	public function getIcon() {
 		return 'https://bin.bnbstatic.com/static/images/common/favicon.ico';
 	}
 
 	public function getName() {
-		return self::NAME . ' ' . $this->getInput('category');
+		return self::NAME . ' blog';
 	}
 
 	public function getURI() {
-		if ($this->getInput('category') == 'Blog')
-			return self::URI . '/en/blog';
-		else
-			return 'https://binance.zendesk.com/hc/en-us/categories/115000056351-Announcements';
+		return self::URI . '/en/blog';
 	}
 
 	protected function collectBlogData() {
-		$html = getSimpleHTMLDOM($this->getURI());
+		$html = getSimpleHTMLDOM($this->getURI())
+			or returnServerError('Could not fetch Binance blog data.');
 
 		$appData = $html->find('script[id="__APP_DATA"]');
 		$appDataJson = json_decode($appData[0]->innertext);
@@ -62,36 +47,7 @@ class BinanceBridge extends BridgeAbstract {
 		}
 	}
 
-	protected function collectAnnouncementData() {
-		$html = getSimpleHTMLDOM($this->getURI());
-
-		foreach($html->find('a.article-list-link') as $a) {
-			$title = $a->innertext;
-			$uri = 'https://binance.zendesk.com' . $a->href;
-
-			$full = getSimpleHTMLDOMCached($uri);
-			$content = $full->find('div.article-body', 0);
-			$date = $full->find('time', 0)->getAttribute('datetime');
-
-			$item = array();
-
-			$item['title'] = $title;
-			$item['uri'] = $uri;
-			$item['timestamp'] = strtotime($date);
-			$item['author'] = 'Binance';
-			$item['content'] = $content;
-
-			$this->items[] = $item;
-
-			if (count($this->items) >= 10)
-				break;
-		}
-	}
-
 	public function collectData() {
-		if ($this->getInput('category') == 'Blog')
-			$this->collectBlogData();
-		else
-			$this->collectAnnouncementData();
+		$this->collectBlogData();
 	}
 }
