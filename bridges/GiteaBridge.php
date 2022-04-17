f950d2b1c904be57737200a1c74b6542d2dd730a
gileri <e+pgp9225@linuxw.info>
2022-04-17 15:11:17 +0200

    [GiteaBridge] Split parameters from GogsBridge

--- bridges/GiteaBridge.php
+++ bridges/GiteaBridge.php
@@ -13,6 +13,55 @@ class GiteaBridge extends GogsBridge {
 	const MAINTAINER = 'logmanoriginal';
 	const CACHE_TIMEOUT = 300; // 5 minutes
 
+	const PARAMETERS = array(
+		'global' => array(
+			'host' => array(
+				'name' => 'Host',
+				'exampleValue' => 'notabug.org',
+				'required' => true,
+				'title' => 'Host name without trailing slash',
+			),
+			'user' => array(
+				'name' => 'Username',
+				'exampleValue' => 'PDModdingCommunity',
+				'required' => true,
+				'title' => 'User name as it appears in the URL',
+			),
+			'project' => array(
+				'name' => 'Project name',
+				'exampleValue' => 'PD-Loader',
+				'required' => true,
+				'title' => 'Project name as it appears in the URL',
+			),
+		),
+		'Commits' => array(
+			'branch' => array(
+				'name' => 'Branch name',
+				'defaultValue' => 'master',
+				'required' => true,
+				'title' => 'Branch name as it appears in the URL',
+			),
+		),
+		'Issues' => array(
+			'include_description' => array(
+				'name' => 'Include issue description',
+				'type' => 'checkbox',
+				'title' => 'Activate to include the issue description',
+			),
+		),
+		'Single issue' => array(
+			'issue' => array(
+				'name' => 'Issue number',
+				'type' => 'number',
+				'exampleValue' => 100,
+				'required' => true,
+				'title' => 'Issue number from the issues list',
+			),
+		),
+		'Releases' => array(),
+	);
+
+
 	protected function collectReleasesData($html) {
 		$releases = $html->find('#release-list > li')
 			or returnServerError('Unable to find releases');
