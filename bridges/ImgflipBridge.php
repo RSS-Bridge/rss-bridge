8fbc5b6898a06e552f6154e9dec78088f5dd3b9f
Yaman Qalieh <ybq987@gmail.com>
2022-05-10 19:22:51 -0400

    Add JSONAbstract and example ImgflipBridge

---
+++ bridges/ImgflipBridge.php
@@ -0,0 +1,17 @@
+<?php
+
+class ImgflipBridge extends JSONAbstract {
+
+	const NAME = 'Imgflip Bridge';
+	const DESCRIPTION = 'Returns the latest memes';
+	const URI = 'https://imgflip.com/';
+	const MAINTAINER = 'Yaman Qalieh';
+
+	const FEED_SOURCE_URL = 'https://api.imgflip.com/get_memes';
+	const USER_EXPRESSION_ITEM = 'data.memes';
+	const USER_EXPRESSION_ITEM_TITLE = 'name';
+	const USER_EXPRESSION_ITEM_CONTENT = 'url';
+	const USER_EXPRESSION_ITEM_URI = 'url';
+	const USER_EXPRESSION_ITEM_ENCLOSURES = 'url';
+	const USER_EXPRESSION_UID = 'id';
+}
--- lib/JSONAbstract.php
+++ lib/JSONAbstract.php
@@ -1,19 +1,64 @@
 <?php
 
-/**
- * An alternative abstract class for bridges utilizing JSON input
- *
- * This class is meant as an alternative base class for bridge implementations.
- * It offers preliminary functionality for generating feeds based on XPath
- * expressions.
- * As a minimum, extending classes should define XPath expressions pointing
- * to the feed items contents in the class constants below. In case there is
- * more manual fine tuning required, it offers a bunch of methods which can
- * be overridden, for example in order to specify formatting of field values
- * or more flexible definition of dynamic XPath expressions.
- *
- * This class extends {@see BridgeAbstract}, which means it incorporates and
- * extends all of its functionality.
- **/
+abstract class JSONAbstract extends UserDefinedAbstract {
 
-abstract class XPathAbstract extends BridgeAbstract {
+	const USER_EXPRESSION_DELIMITER = '.';
+
+	protected function provideWebsiteContent() {
+		$header = array(
+			'Content-Type: application/json'
+		);
+		return getContents($this->getURI(), $header);
+	}
+
+	protected function provideWebsiteData() {
+		return json_decode($this->provideWebsiteContent(), true);
+	}
+
+	protected function getExpressionDelimiter() {
+		return static::USER_EXPRESSION_DELIMITER;
+	}
+
+	protected function convertUserQuery($json, $query, $context) {
+		if (empty($query))
+			return null;
+
+		if (!is_null($context))
+			$json = $context;
+
+		foreach(explode($this->getExpressionDelimiter(), $query) as $key) {
+			if (empty($key))
+				return $json;
+			$json = @$json[$key];
+		}
+		return $json;
+	}
+
+	protected function isEmpty($result) {
+		return is_null($result)
+			|| (is_array($result) && count($result) === 0)
+			|| (is_string($result) && strlen($result) === 0);
+	}
+
+	protected function transformWebsiteToData($html) {
+		return json_decode($this->provideWebsiteContent(), true);
+	}
+
+	protected function getDataValue($result) {
+		return $result;
+	}
+
+	protected function formatItemCategories($value) {
+		if (is_array($value)) {
+			return $value;
+		}
+		return array($value);
+	}
+
+	protected function formatItemEnclosures($value) {
+		if (is_array($value)) {
+			return $value;
+		}
+		return array($value);
+	}
+}
