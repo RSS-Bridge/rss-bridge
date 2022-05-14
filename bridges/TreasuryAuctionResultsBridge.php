948367d5192ba45cdc303712d201d5cfbf2a02cd
Kevin Saylor, Jr <kevin@kevinsaylor.me>
2022-05-14 16:18:00 -0400

    simpler...works just as well

--- bridges/TreasuryAuctionResultsBridge.php
+++ bridges/TreasuryAuctionResultsBridge.php
@@ -23,8 +23,7 @@ class TreasuryAuctionResultsBridge extends FeedExpander {
 		 * we want a fresh view of the RSS stream each time
 		 */
 		$content = getContents($url, array(
-			'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
-			'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+			'Accept: 1/1,*/*'
 		), array(CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1))
 		or returnServerError('Could not request ' . $url);
 		$rssContent = simplexml_load_string(trim($content));
