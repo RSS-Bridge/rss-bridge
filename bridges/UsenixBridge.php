ee46687e4f07e26105f48358418e5319100d9b00
Yaman Qalieh <ybq987@gmail.com>
2022-06-08 22:37:44 -0400

    [UsenixBridge] Add item author, tags, and content

--- bridges/UsenixBridge.php
+++ bridges/UsenixBridge.php
@@ -18,7 +18,7 @@ final class UsenixBridge extends BridgeAbstract
 			$this->collectLoginOnlineItems();
 			return;
 		}
-		throw new Exception('Illegal context');
+		returnClientError('Illegal Context');
 	}
 
 	private function collectLoginOnlineItems(): void
@@ -29,16 +29,40 @@ final class UsenixBridge extends BridgeAbstract
 
 		foreach ($items as $item) {
 			$title = $item->find('.views-field-title > span', 0);
+			$author = $item->find('.views-field-pseudo-author-list > span.field-content', 0);
 			$relativeUrl = $item->find('.views-field-nothing-1 > span > a', 0);
+			$uri = sprintf('https://www.usenix.org%s', $relativeUrl->href);
 			// June 2, 2022
 			$createdAt = $item->find('div.views-field-field-lv2-publication-date > div > span', 0);
 
-			$this->items[] = [
+			$item = [
 				'title' => $title->innertext,
-				'uri' => sprintf('https://www.usenix.org%s', $relativeUrl->href),
+				'author' => strstr($author->plaintext, ',', true) ?: $author->plaintext,
+				'uri' => $uri,
 				'timestamp' => $createdAt->innertext,
-				'content' => $item->find('.views-field-field-lv2-article-teaser > div', 0)->innertext,
 			];
+
+			$this->items[] = array_merge($item, $this->getItemContent($uri));
 		}
+	}
+
+	private function getItemContent(string $uri) : array
+	{
+		$html = getSimpleHTMLDOMCached($uri);
+		$content = $html->find('.paragraphs-items-full', 0)->innertext;
+		$extra = $html->find('fieldset', 0);
+		if (!empty($extra)) {
+			$content .= $extra->innertext;
+		}
+
+		$tags = [];
+		foreach($html->find('.field-name-field-lv2-tags div.field-item') as $tag) {
+			$tags[] = $tag->plaintext;
+		}
+
+		return [
+			'content' => $content,
+			'categories' => $tags
+		];
 	}
 }
