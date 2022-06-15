faf5dc160f61217ef0f0eb3a51789a4c34fc459f
Yaman Qalieh <ybq987@gmail.com>
2022-06-14 22:38:30 -0400

    Update Kanali6Bridge

--- bridges/Kanali6Bridge.php
+++ bridges/Kanali6Bridge.php
@@ -7,13 +7,13 @@ class Kanali6Bridge extends XPathAbstract {
 	const MAINTAINER = 'Yaman Qalieh';
 
 	const FEED_SOURCE_URL = 'https://kanali6.com.cy/mp3/TOC.xml';
-	const XPATH_EXPRESSION_ITEM = '//recording[position() <= 50]';
-	const XPATH_EXPRESSION_ITEM_TITLE = './title';
-	const XPATH_EXPRESSION_ITEM_CONTENT = './durationvisual';
-	const XPATH_EXPRESSION_ITEM_URI = './filename';
-	const XPATH_EXPRESSION_ITEM_AUTHOR = './/producersname';
-	const XPATH_EXPRESSION_ITEM_TIMESTAMP = './recfinisheddatetime';
-	const XPATH_EXPRESSION_ITEM_ENCLOSURES = './filename';
+	const USER_EXPRESSION_ITEM = '//recording[position() <= 50]';
+	const USER_EXPRESSION_ITEM_TITLE = './title';
+	const USER_EXPRESSION_ITEM_CONTENT = './durationvisual';
+	const USER_EXPRESSION_ITEM_URI = './filename';
+	const USER_EXPRESSION_ITEM_AUTHOR = './/producersname';
+	const USER_EXPRESSION_ITEM_TIMESTAMP = './recfinisheddatetime';
+	const USER_EXPRESSION_ITEM_ENCLOSURES = './filename';
 
 	public function getURI() {
 		return self::URI;
