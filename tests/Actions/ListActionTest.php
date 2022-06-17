4ce4a4447ea37589e3f88fe1c74aac9e9c462f8d
Dag <me@dvikan.no>
2022-06-17 22:47:19 +0200

    refactor: ActionFactory

--- tests/Actions/ListActionTest.php
+++ tests/Actions/ListActionTest.php
@@ -78,7 +78,6 @@ class ListActionTest extends TestCase {
 
 	private function initAction() {
 		$actionFac = new ActionFactory();
-		$actionFac->setWorkingDir(PATH_LIB_ACTIONS);
 
 		$action = $actionFac->create('list');
 		$action->setUserData(array()); /* no user data required */
