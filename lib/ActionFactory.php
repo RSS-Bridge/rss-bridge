18acbc7339c927f455bd09dfa19880ebf6d60269
Dag <me@dvikan.no>
2022-06-19 18:12:34 +0200

    docs

--- lib/ActionFactory.php
+++ lib/ActionFactory.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * This file is part of RSS-Bridge, a PHP project capable of generating RSS and
+ * Atom feeds for websites that don't have one.
+ *
+ * For the full license information, please view the UNLICENSE file distributed
+ * with this source code.
+ *
+ * @package	Core
+ * @license	http://unlicense.org/ UNLICENSE
+ * @link	https://github.com/rss-bridge/rss-bridge
+ */
 
 class ActionFactory
 {
@@ -9,7 +20,10 @@ class ActionFactory
 		$this->folder = $folder;
 	}
 
-	public function create(string $name)
+	/**
+	 * @param string $name The name of the action e.g. "Display", "List", or "Connectivity"
+	 */
+	public function create(string $name): ActionInterface
 	{
 		$name = ucfirst(strtolower($name)) . 'Action';
 		$filePath = $this->folder . $name . '.php';
