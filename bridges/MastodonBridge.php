6d9b531a98560417e3c220ebc5c5dceebe0ef174
Austin Huang <im@austinhuang.me>
2022-06-18 21:13:36 -0400

    [Mastodon] add username cache, fix try-catch, rename

--- bridges/MastodonBridge.php
+++ bridges/MastodonBridge.php
@@ -2,15 +2,16 @@
 
 class MastodonBridge extends BridgeAbstract {
 	// This script attempts to imitiate the behaviour of a read-only ActivityPub server
-	// to read the outbox.
+	// to read the outbox. This does not support instances that require HTTP signatures
+	// for ActivityPub endpoints.
 
 	// Note: Most PixelFed instances have ActivityPub outbox disabled,
-	// use the official feed: https://pixelfed.instance/users/username.atom (Posts only)
+	// so use the official feed: https://pixelfed.instance/users/username.atom (Posts only)
 
 	const MAINTAINER = 'Austin Huang';
-	const NAME = 'Mastodon Bridge';
+	const NAME = 'ActivityPub Bridge';
 	const CACHE_TIMEOUT = 900; // 15mn
-	const DESCRIPTION = 'Returns recent statuses. May support other ActivityPub-compatible accounts.';
+	const DESCRIPTION = 'Returns recent statuses. Supports ActivityPub-compatible platforms, including Mastodon, Pleroma and Misskey.';
 	const URI = 'https://mastodon.social';
 
 	const PARAMETERS = array(array(
@@ -80,14 +81,15 @@ class MastodonBridge extends BridgeAbstract {
 		$content = json_decode(getContents($url, self::AP_HEADER), true);
 		if ($content['id'] === $url) {
 			foreach ($content['orderedItems'] as $status) {
-				$this->items[] = $this->parseItem($status);
+				$users = array();
+				$this->items[] = $this->parseItem($status, $users);
 			}
 		} else {
 			throw new \Exception('Unexpected response from server.');
 		}
 	}
 
-	protected function parseItem($content) {
+	protected function parseItem($content, &$users) {
 		$item = array();
 		switch ($content['type']) {
 			case 'Announce': // boost
@@ -97,21 +99,31 @@ class MastodonBridge extends BridgeAbstract {
 				// We fetch the boosted content.
 				try {
 					$rtContent = json_decode(getContents($content['object'], self::AP_HEADER), true);
-					// We fetch the author, since we cannot always assume the format of the URL.
-					$user = json_decode(getContents($rtContent['attributedTo'], self::AP_HEADER), true);
-					preg_match('/http(|s):\/\/([a-z0-9-\.]{0,})\//', $rtContent['attributedTo'], $matches);
-					$rtUser = '@' . $user['preferredUsername'] . '@' . $matches[2];
-					$item['author'] = $rtUser;
-					$item['title'] = 'Shared a status by ' . $rtUser . ': ';
-					$item = $this->parseObject($rtContent, $item);
-				} catch (Throwable $th) {
+					if ($rtContent['attributedTo'] && isset($users[$rtContent['attributedTo']])) {
+						$item['author'] = $users[$rtContent['attributedTo']];
+						$item['title'] = 'Shared a status by ' . $item['author'] . ': ';
+						$item = $this->parseObject($rtContent, $item);
+					} else {
+						// We fetch the author, since we cannot always assume the format of the URL.
+						$user = json_decode(getContents($rtContent['attributedTo'], self::AP_HEADER), true);
+						preg_match('/http(|s):\/\/([a-z0-9-\.]{0,})\//', $rtContent['attributedTo'], $matches);
+						// We assume that the server name as indicated by the path is the actual server name,
+						// since using webfinger to delegate domains is not officially supported, and it only
+						// seems to work in one way.
+						$rtUser = '@' . $user['preferredUsername'] . '@' . $matches[2];
+						$users[$rtContent['attributedTo']] = $rtUser;
+						$item['author'] = $rtUser;
+						$item['title'] = 'Shared a status by ' . $rtUser . ': ';
+						$item = $this->parseObject($rtContent, $item);
+					}
+				} catch (UnexpectedResponseException $th) {
 					$item['title'] = 'Shared an unreachable status: ' . $content['object'];
 					$item['content'] = $content['object'];
 					$item['uri'] = $content['object'];
 				}
 				break;
 			case 'Create':
-				if ($this->getInput('norep') && $content['object']['inReplyTo']) {
+				if ($this->getInput('norep') && isset($content['object']['inReplyTo'])) {
 					return null;
 				}
 				$item['author'] = $this->getInput('canusername');
