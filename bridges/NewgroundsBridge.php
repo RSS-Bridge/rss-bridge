1985ffef445a05d2a277f2eb324dc481bd9faa0e
Dag <me@dvikan.no>
2022-06-19 22:42:12 +0200

    reformat code

--- bridges/NewgroundsBridge.php
+++ bridges/NewgroundsBridge.php
@@ -1,5 +1,7 @@
 <?php
-class NewgroundsBridge extends BridgeAbstract {
+
+class NewgroundsBridge extends BridgeAbstract
+{
 	const NAME = 'Newgrounds';
 	const URI = 'https://www.newgrounds.com';
 	const DESCRIPTION = 'Get the latest art from a given user';
@@ -9,42 +11,44 @@ class NewgroundsBridge extends BridgeAbstract {
 			'name' => 'Username',
 			'type' => 'text',
 			'required' => true,
-			'exampleValue'	=>	'TomFulp'
+			'exampleValue' => 'TomFulp'
 		)
 	));
 
-	public function getName(){
-		if(!empty($this-> getInput('username'))) {
-			return $this-> getInput('username') . ' - ' . self::NAME;
+	public function getName()
+	{
+		if (!empty($this->getInput('username'))) {
+			return $this->getInput('username') . ' - ' . self::NAME;
 		}
 		return parent::getName();
 	}
 
-	public function getURI(){
-		if(!empty($this-> getInput('username'))) {
-			return 'https://' . $this-> getInput('username') . '.newgrounds.com/art';
+	public function getURI()
+	{
+		if (!empty($this->getInput('username'))) {
+			return 'https://' . $this->getInput('username') . '.newgrounds.com/art';
 		}
 		return parent::getURI();
 	}
 
 	public function collectData()
 	{
-		$userlink = 'https://' . $this-> getInput('username') . '.newgrounds.com/art';
+		$userlink = 'https://' . $this->getInput('username') . '.newgrounds.com/art';
 		$html = getSimpleHTMLDOM($userlink);
 
-		$posts = $html -> find('.item-portalitem-art-medium');
+		$posts = $html->find('.item-portalitem-art-medium');
 
-		foreach($posts as $post) {
+		foreach ($posts as $post) {
 			$item = array();
 
-			$item['author'] = $this-> getInput('username');
-			$item['uri'] = $post -> href;
+			$item['author'] = $this->getInput('username');
+			$item['uri'] = $post->href;
 			$item['uid'] = $item['uri'];
 
-			$titleOrRestricted = $post -> find('h4')[0] -> innertext;
+			$titleOrRestricted = $post->find('h4')[0]->innertext;
 
 			//Newgrounds doesn't show public previews for NSFW content.
-			if($titleOrRestricted == 'Restricted Content: Sign in to view!') {
+			if ($titleOrRestricted == 'Restricted Content: Sign in to view!') {
 				$item['title'] = 'NSFW: ' . $item['uri'];
 				$item['content'] = <<<EOD
 <a href="{$item['uri']}">
@@ -58,7 +62,7 @@ EOD;
 <img
     style="align:top; width:270px; border:1px solid black;"
     alt="{$item['title']}"
-    src="{$post -> find('img')[0] -> src}"
+    src="{$post->find('img')[0]->src}"
     title="{$item['title']}" />
 </a>
 EOD;
