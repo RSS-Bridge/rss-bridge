599591b4d54c42fd5a602f79badcea4d44683eb9
Dag <me@dvikan.no>
2022-07-08 16:59:10 +0200

    fix

--- templates/bridge-error.html.php
+++ templates/bridge-error.html.php
@@ -18,7 +18,7 @@
         Query string: <?= e($_SERVER['QUERY_STRING'] ?? '') ?>
     </p>
     <p>
-        Version: <?= Configuration::getVersion() ?>
+        Version: <?= e(Configuration::getVersion()) ?>
     </p>
 
     <div class="advice">
@@ -29,10 +29,15 @@
             <li>Open a <a href="<?= raw($issueUrl) ?>">GitHub Issue</a> if this error persists</li>
         </ul>
     </div>
+
     <a href="<?= raw($searchUrl) ?>" title="Opens GitHub to search for similar issues">
         <button>Search GitHub Issues</button>
     </a>
-    <a href="<?= raw($issueUrl) ?>" title="After clicking this button you can review
-	the issue before submitting it"><button>Open GitHub Issue</button></a>
-    <p class="maintainer"><?= $bridge->getMaintainer() ?></p>
+
+    <a href="<?= raw($issueUrl) ?>" title="After clicking this button you can review the issue before submitting it">
+        <button>Open GitHub Issue</button>
+    </a>
+
+    <p class="maintainer"><?= e($bridge->getMaintainer()) ?></p>
 </section>
+
--- templates/error.html.php
+++ templates/error.html.php
@@ -16,3 +16,4 @@
     <?php endif; ?>
 
 </div>
+
