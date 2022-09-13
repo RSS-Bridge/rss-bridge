2c9a065290138fdc77b511d8134c67d068ef33dc
Dag <me@dvikan.no>
2022-09-13 20:18:55 +0200

    fix: theguardian

--- bridges/TheGuardianBridge.php
+++ bridges/TheGuardianBridge.php
@@ -50,52 +50,43 @@ class TheGuardianBridge extends FeedExpander
 
     public function collectData()
     {
-        $feed = $this->getInput('feed');
-        $feedURL = 'https://feeds.theguardian.com/theguardian/' . $feed;
-        $this->collectExpandableDatas($feedURL, 10);
+        $url = sprintf('https://feeds.theguardian.com/theguardian/%s', $this->getInput('feed'));
+        $this->collectExpandableDatas($url, 10);
     }
 
     protected function parseItem($newsItem)
     {
         $item = parent::parseItem($newsItem);
-
-        // --- Recovering the article ---
-
-        // $articlePage gets the entire page's contents
-        $articlePage = getSimpleHTMLDOM($newsItem->link);
+        $dom = getSimpleHTMLDOM($newsItem->link);
         // figure contain's the main article image
-        $article = $articlePage->find('figure', 0);
-        // content__article-body has the actual article
-        foreach ($articlePage->find('.content__article-body') as $element) {
-            $article = $article . $element;
+        $figure = $dom->find('figure', 0);
+        $mainImage = $figure->find('img', 0);
+        $article = '';
+        if ($mainImage->src) {
+            $article .= $mainImage . '<br>';
         }
-
-        // --- Fixing ugly elements ---
-
+        $body = $dom->find('.article-body-commercial-selector', 0);
+        $article .= $body;
         // Replace the image viewer and BS with the image itself
-        foreach ($articlePage->find('a.article__img-container') as $uslElementLoc) {
+        foreach ($body->find('a.article__img-container') as $uslElementLoc) {
             $main_img = $uslElementLoc->find('img', 0);
             $article = str_replace($uslElementLoc, $main_img, $article);
         }
-
-        // List of all the crap in the article
-        $uselessElements = [
+        // Remove annoying stuff
+        $annoyingStuffs = [
             '#show-caption',
             '.element-atom',
             '.submeta',
             'youtube-media-atom',
-            'svg'
+            'svg',
+            'figcaption',
         ];
-
-        // Remove the listed crap
-        foreach ($uselessElements as $uslElement) {
-            foreach ($articlePage->find($uslElement) as $uslElementLoc) {
-                $article = str_replace($uslElementLoc, '', $article);
+        foreach ($annoyingStuffs as $annoyingStuff) {
+            foreach ($dom->find($annoyingStuff) as $annoying) {
+                $article = str_replace($annoying, '', $article);
             }
         }
-
         $item['content'] = $article;
-
         return $item;
     }
 }
