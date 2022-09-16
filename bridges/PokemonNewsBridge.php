995e84cb4972d1037f78daf2bbae4af2eca3f380
Dag <me@dvikan.no>
2022-09-17 01:27:59 +0200

    fix

--- bridges/PokemonNewsBridge.php
+++ bridges/PokemonNewsBridge.php
@@ -11,21 +11,28 @@ final class PokemonNewsBridge extends BridgeAbstract
 
     public function collectData()
     {
-        $dom = getSimpleHTMLDOM('https://www.pokemon.com/us/pokemon-news');
+        // todo: parse json api instead: https://www.pokemon.com/api/1/us/news/get-news.json
+        $url = 'https://www.pokemon.com/us/pokemon-news';
+        $dom = getSimpleHTMLDOM($url);
+
         foreach ($dom->find('.news-list ul li') as $item) {
             $title = $item->find('h3', 0)->plaintext;
             $description = $item->find('p.hidden-mobile', 0);
             $dateString = $item->find('p.date', 0)->plaintext;
             // e.g. September 15, 2022
             $createdAt = date_create_from_format('F d, Y', $dateString);
-            $tag = $item->find('p.tags', 0)->plaintext;
+            // todo:
+            $tagsString = $item->find('p.tags', 0)->plaintext;
             $path = $item->find('a', 0)->href;
             $imagePath = $item->find('img', 0)->src;
+            $tags = explode('&', $tagsString);
+            $tags = array_map('trim', $tags);
+
             $this->items[] = [
                 'title' => $title,
                 'uri' => sprintf('https://www.pokemon.com%s', $path),
                 'timestamp' => $createdAt ? $createdAt->getTimestamp() : time(),
-                'categories' => [$tag],
+                'categories' => $tags,
                 'content' => sprintf(
                     '<img src="https://pokemon.com%s"><br><br>%s',
                     $imagePath,
