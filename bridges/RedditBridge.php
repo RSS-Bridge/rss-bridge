30d964b356d9ed697949a2007ff0798d485e7092
joshinat0r <info@joshuahahn.de>
2022-10-02 07:34:20 +0200

    [RedditBridge] Search for specific flairs (#3067)
    * flair filter
    
    * syntax
    
    * fix multi & user feeds
    
    * '
    
    * dont replace ,

--- bridges/RedditBridge.php
+++ bridges/RedditBridge.php
@@ -41,6 +41,12 @@ class RedditBridge extends BridgeAbstract
                 'required' => true,
                 'exampleValue' => 'selfhosted',
                 'title' => 'SubReddit name'
+            ],
+            'f' => [
+                'name' => 'Flair',
+                'required' => false,
+                'exampleValue' => 'Proxy',
+                'title' => 'Flair filter'
             ]
         ],
         'multi' => [
@@ -136,11 +142,22 @@ class RedditBridge extends BridgeAbstract
             $keywords = '';
         }
 
+        if (!($this->getInput('f') === '') && $this->queriedContext == 'single') {
+            $flair = $this->getInput('f');
+            $flair = str_replace(' ', '%20', $flair);
+            $flair = 'flair%3A%22' . $flair . '%22%20';
+        } else {
+            $flair = '';
+        }
+
+
+
         foreach ($subreddits as $subreddit) {
             $name = trim($subreddit);
             $values = getContents(self::URI
                     . '/search.json?q='
                     . $keywords
+                    . $flair
                     . ($user ? 'author%3A' : 'subreddit%3A')
                     . $name
                     . '&sort='
