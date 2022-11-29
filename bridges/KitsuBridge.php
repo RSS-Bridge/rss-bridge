f3decba9a5a037cd0a0fedb8642082b939596428
Nils Gotzhein <nils.gotzhein@googlemail.com>
2022-11-29 23:09:02 +0100

    Adds new Bridge for kitso.io episode feed

---
+++ bridges/KitsuBridge.php
@@ -0,0 +1,47 @@
+<?php
+
+class KitsuBridge extends BridgeAbstract {
+    const NAME = 'Kitsu Episode Updates';
+    const URI = 'https://kitsu.io/api/edge/episodes?filter[mediaType]=Anime&sort=-airdate&include=media&page[limit]=20';
+    const DESCRIPTION = 'Lists latest upcoming episodes';
+    //const PARAMETERS = array();
+    const CACHE_TIMEOUT = 3600;
+
+    public function collectData() {
+
+        $feedContent = json_decode(getContents(self::URI), true);
+
+        $animeList = array();
+
+        foreach($feedContent['included'] as $included) {
+            if($included['type'] === 'anime') {
+                $animeList[(int)$included['id']] = $included['attributes'];
+            }
+        }
+
+        foreach ($feedContent['data'] as $episode) {
+            $item = array();
+
+            $item['title'] = $animeList[(int)$episode['relationships']['media']['data']['id']]['canonicalTitle'] . ': Season '
+             . $episode['attributes']['seasonNumber'] . ' Episode ' . $episode['attributes']['number'];
+            $item['content'] = $episode['attributes']['canonicalTitle'];
+            if($episode['attributes']['description']) {
+                $item['content'] .= '<br/><br/>'
+                 . $episode['attributes']['description'];
+            }
+            $item['content'] .= '<br/><br/>Airdate: ' . $episode['attributes']['airdate'];
+            $item['uri'] = 'https://kitsu.io/anime/' . $animeList[(int)$episode['relationships']['media']['data']['id']]['slug']
+             . '/episodes/' . $episode['attributes']['number'];
+            $item['author'] = $episode['attributes']['canonicalTitle'];
+            $item['timestamp'] = strtotime($episode['attributes']['createdAt']);
+            $item['uid'] = $episode['id'];
+
+            if(is_array($episode['attributes']['thumbnail'])) {
+                $item['enclosures'][] = $episode['attributes']['thumbnail']['original'];
+            }
+
+            $this->items[] = $item;
+        }
+
+    }
+}
