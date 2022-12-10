933be15a772e76d0d67e5853b90117420a6493b9
Jan Justi <9977169+janjusti@users.noreply.github.com>
2022-12-10 17:25:25 -0300

    [docker-entrypoint.sh] Add custom formats (#3179)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
-# - Find custom files (bridges, whitelist, config.ini) in the /config folder
+# - Find custom files (bridges, formats, whitelist, config.ini) in the /config folder
 # - Copy them to the respective folders in /app
-# This will overwrite previous configs and bridges of same name
+# This will overwrite previous configs, bridges and formats of same name
 # If there are no matching files, rss-bridge works like default.
 
 find /config/ -type f -name '*' -print0 2> /dev/null |
 while IFS= read -r -d '' file; do
     file_name="$(basename "$file")" # Strip leading path
     if [[ $file_name = *" "* ]]; then
-        printf 'Custom Bridge %s has a space in the name and will be skipped.\n' "$file_name"
+        printf 'Custom file %s has a space in the name and will be skipped.\n' "$file_name"
         continue
     fi
     case "$file_name" in
     *Bridge.php)    yes | cp "$file" /app/bridges/ ;
                     chown www-data:www-data "/app/bridges/$file_name";
                     printf "Custom Bridge %s added.\n" $file_name;;
+    *Format.php)    yes | cp "$file" /app/formats/ ;
+                    chown www-data:www-data "/app/formats/$file_name";
+                    printf "Custom Format %s added.\n" $file_name;;
     config.ini.php) yes | cp "$file" /app/ ;
                     chown www-data:www-data "/app/$file_name";
                     printf "Custom config.ini.php added.\n";;
