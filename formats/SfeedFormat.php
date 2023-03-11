20af4800cf83f9c79f8ea0a4182605e5f28dc7d0
Mad Reyk <madreyk@madreyk.xyz>
2023-03-12 02:07:50 +0300

    [Sfeed] Move all global functions to class and fix phpcs warnings

--- formats/SfeedFormat.php
+++ formats/SfeedFormat.php
@@ -1,52 +1,63 @@
 <?PHP
-function escape(string $str) {
-    $str = str_replace('\\', '\\\\', $str);
-    $str = str_replace('\n', '\\n', $str);
-    return str_replace('\t', '\\t', $str);
-}
 
-function getFirstEnclosure(array $enclosures) {
-    if(count($enclosures) >= 1)
-        return $enclosures[0];
-    return '';
-}
-
-function getCategories(array $cats) {
-    $toReturn = '';
-    $i = 0;
-    foreach($cats as $cat) {
-        $toReturn .= $cat;
-        if(count($cats) < $i)
-            $toReturn .= '|';
-        $i++;
-    }
-    return $toReturn;
-}
-
-class SfeedFormat extends FormatAbstract {
+class SfeedFormat extends FormatAbstract
+{
     const MIME_TYPE = 'text/plain';
 
-    public function stringify() {
+    private function escape(string $str)
+    {
+        $str = str_replace('\\', '\\\\', $str);
+        $str = str_replace("\n", "\\n", $str);
+        return str_replace("\t", "\\t", $str);
+    }
+
+    private function getFirstEnclosure(array $enclosures)
+    {
+        if (count($enclosures) >= 1) {
+            return $enclosures[0];
+        }
+        return '';
+    }
+
+    private function getCategories(array $cats)
+    {
+        $toReturn = '';
+        $i = 0;
+        foreach ($cats as $cat) {
+            $toReturn .= $cat;
+            if (count($cats) < $i) {
+                $toReturn .= '|';
+            }
+            $i++;
+        }
+        return $toReturn;
+    }
+
+    public function stringify()
+    {
         $items = $this->getItems();
 
         $toReturn = '';
-        foreach($items as $item) {
+        foreach ($items as $item) {
             $toReturn .= sprintf(
-                    '%s\t%s\t%s\t%s\thtml\t\t%s\t%s\t%s\n',
-                    $item->toArray()['timestamp'],
-                    escape($item->toArray()['title']),
-                    $item->toArray()['uri'],
-                    escape($item->toArray()['content']),
-                    $item->toArray()['author'],
-                    getFirstEnclosure($item->toArray()['enclosures']),
-                    getCategories($item->toArray()['categories'])
-                    );
+                "%s\t%s\t%s\t%s\thtml\t\t%s\t%s\t%s\n",
+                $item->toArray()['timestamp'],
+                $this->escape($item->toArray()['title']),
+                $item->toArray()['uri'],
+                $this->escape($item->toArray()['content']),
+                $item->toArray()['author'],
+                $this->getFirstEnclosure($item->toArray()['enclosures']),
+                $this->getCategories($item->toArray()['categories'])
+            );
         }
 
         // Remove invalid non-UTF8 characters
         ini_set('mbstring.substitute_character', 'none');
-        $toReturn = mb_convert_encoding($toReturn, 
-                $this->getCharset(), 'UTF-8');
+        $toReturn = mb_convert_encoding(
+            $toReturn,
+            $this->getCharset(),
+            'UTF-8'
+        );
         return $toReturn;
     }
 }
