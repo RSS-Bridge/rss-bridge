c5cd2294456c6a118885003f7beb6f32bb98bc68
Ryan Stafford <ryan@nanorex.net>
2023-06-01 15:26:47 -0400

    [YoutubeBridge] Set icon (#3416)

--- bridges/YoutubeBridge.php
+++ bridges/YoutubeBridge.php
@@ -336,6 +336,7 @@ class YoutubeBridge extends BridgeAbstract
                 $html = $this->ytGetSimpleHTMLDOM($url_listing);
                 $jsonData = $this->getJSONData($html);
                 $url_feed = $jsonData->metadata->channelMetadataRenderer->rssUrl;
+                $this->iconURL = $jsonData->metadata->channelMetadataRenderer->avatar->thumbnails[0]->url;
             }
             if (!$this->skipFeeds()) {
                 $html = $this->ytGetSimpleHTMLDOM($url_feed);
@@ -442,6 +443,15 @@ class YoutubeBridge extends BridgeAbstract
                 return htmlspecialchars_decode($this->feedName) . ' - YouTube'; // We already know it's a bridge, right?
             default:
                 return parent::getName();
+        }
+    }
+
+    public function getIcon()
+    {
+        if (empty($this->iconURL)) {
+            return parent::getIcon();
+        } else {
+            return $this->iconURL;
         }
     }
 }
