550f12d8d7bc767d6868c62c7959303d3b9b49a1
Jisagi <Jisagi@users.noreply.github.com>
2023-06-06 21:54:21 +0200

    Update NyaaTorrentsBridge.php

--- bridges/NyaaTorrentsBridge.php
+++ bridges/NyaaTorrentsBridge.php
@@ -2,10 +2,11 @@
 
 class NyaaTorrentsBridge extends FeedExpander
 {
-    const MAINTAINER = 'ORelio';
+    const MAINTAINER = 'ORelio & Jisagi';
     const NAME = 'NyaaTorrents';
     const URI = 'https://nyaa.si/';
     const DESCRIPTION = 'Returns the newest torrents, with optional search criteria.';
+    const MAX_ITEMS = 20;
     const PARAMETERS = [
         [
             'f' => [
@@ -64,24 +65,48 @@ class NyaaTorrentsBridge extends FeedExpander
     {
         return self::URI . 'static/favicon.png';
     }
-
-    public function collectData()
+    
+    public function getURI()
     {
-        $this->collectExpandableDatas(
-            self::URI . '?page=rss&s=id&o=desc&'
+        return self::URI . '?page=rss&s=id&o=desc&'
             . http_build_query([
                 'f' => $this->getInput('f'),
                 'c' => $this->getInput('c'),
                 'q' => $this->getInput('q'),
                 'u' => $this->getInput('u')
-            ]),
-            20
-        );
+            ]);
+    }
+
+    public function collectData()
+    {
+        $content = getContents($this->getURI());
+        $content = $this->fixCustomFields($content);
+        $rssContent = simplexml_load_string(trim($content));
+        $this->collectRss2($rssContent, self::MAX_ITEMS);
+    }
+    
+    private function fixCustomFields($content)
+    {
+        $broken = ["nyaa:seeders", "nyaa:leechers", "nyaa:downloads", "nyaa:infoHash", "nyaa:categoryId", "nyaa:category", "nyaa:size", "nyaa:comments", "nyaa:trusted", "nyaa:remake"];
+        $fixed = ["seeders", "leechers", "downloads", "infoHash", "categoryId", "category", "size", "comments", "trusted", "remake"];
+        return str_replace($broken, $fixed, $content);
     }
 
     protected function parseItem($newItem)
     {
-        $item = parent::parseItem($newItem);
+        $item = parent::parseRss2Item($newItem);
+
+        // Add nyaa custom fields
+        $item['seeders'] = (string) $newItem->{'seeders'};
+        $item['leechers'] = (string) $newItem->{'leechers'};
+        $item['downloads'] = (string) $newItem->{'downloads'};
+        $item['infoHash'] = (string) $newItem->{'infoHash'};
+        $item['categoryId'] = (string) $newItem->{'categoryId'};
+        $item['category'] = (string) $newItem->{'category'};
+        $item['size'] = (string) $newItem->{'size'};
+        $item['comments'] = (string) $newItem->{'comments'};
+        $item['trusted'] = (string) $newItem->{'trusted'};
+        $item['remake'] = (string) $newItem->{'remake'};
 
         //Convert URI from torrent file to web page
         $item['uri'] = str_replace('/download/', '/view/', $item['uri']);
