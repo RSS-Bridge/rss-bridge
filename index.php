eb2186d41336f04ee0c8b31bd7315708e5ccabb1
Dag <me@dvikan.no>
2023-06-11 17:50:24 +0200

    include public/index.php instead of duplicating code

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 
 # This file is kept for legacy reasons in order to not break existing installations.
-# If you have access to nginx config you should instead use the public folder as document root.
+# If you have access to web server config you should instead use the "public" folder as document root.
+# Also remember to alias the "static" directory. See the README.md
 
-require_once __DIR__ . '/lib/bootstrap.php';
-
-$rssBridge = new RssBridge();
-
-$rssBridge->main($argv ?? []);
+require_once __DIR__ . '/public/index.php';
