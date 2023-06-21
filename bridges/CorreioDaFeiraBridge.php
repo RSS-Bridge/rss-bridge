97a31fc7a4b91685e8b7974adb4e89cfe1509b09
Ricardo Coelho <ricms_coelho@hotmail.com>
2023-06-22 00:42:44 +0100

    [CorreioDaFeiraBridge] getName() fix

--- bridges/CorreioDaFeiraBridge.php
+++ bridges/CorreioDaFeiraBridge.php
@@ -37,11 +37,7 @@ class CorreioDaFeiraBridge extends BridgeAbstract
 
     public function getName()
     {
-        $feed = $this->getInput('feed');
-        if ($this->getInput('feed') !== null && $this->getInput('feed') !== '') {
-            return self::NAME . ' | ' . ucfirst($feed);
-        }
-        return self::NAME;
+        return !is_null($this->getKey('feed')) ? self::NAME . ' | ' . $this->getKey('feed') : self::NAME;
     }
 
     public function getURI()
