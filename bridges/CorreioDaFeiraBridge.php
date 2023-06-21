29c76dfd9ce9f0aa897fca2c126fe488d00ca606
Ricardo Coelho <ricms_coelho@hotmail.com>
2023-06-21 23:09:51 +0100

    [CorreioDaFeiraBridge] cache timeout fix

--- bridges/CorreioDaFeiraBridge.php
+++ bridges/CorreioDaFeiraBridge.php
@@ -6,7 +6,7 @@ class CorreioDaFeiraBridge extends BridgeAbstract
     const URI = 'https://www.correiodafeira.pt/';
     const DESCRIPTION = 'Returns news from the Portuguese local newspaper Correio da Feira';
     const MAINTAINER = 'rmscoelho';
-    const CACHE_TIMEOUT = 43200;
+    const CACHE_TIMEOUT = 86400;
     const PARAMETERS = [
         [
             'feed' => [
