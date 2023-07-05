a21d496bc724057dbc6fc75a0286f24ed19cc048
Dag <me@dvikan.no>
2023-07-05 05:33:22 +0200

    feat: add default arg to Configuration::getConfig (#3331)

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -220,9 +220,9 @@ final class Configuration
         }
     }
 
-    public static function getConfig(string $section, string $key)
+    public static function getConfig(string $section, string $key, $default = null)
     {
-        return self::$config[strtolower($section)][strtolower($key)] ?? null;
+        return self::$config[strtolower($section)][strtolower($key)] ?? $default;
     }
 
     private static function setConfig(string $section, string $key, $value): void
