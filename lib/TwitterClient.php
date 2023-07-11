32a247eecc8f3bb05a03bb8d46b3aef8930e10dc
csisoap <33269526+csisoap@users.noreply.github.com>
2023-07-11 19:41:36 +0700

    Replace token for Twitter.

--- lib/TwitterClient.php
+++ lib/TwitterClient.php
@@ -11,7 +11,7 @@ class TwitterClient
     public function __construct(CacheInterface $cache)
     {
         $this->cache = $cache;
-        $this->authorization = 'AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA';
+        $this->authorization = 'AAAAAAAAAAAAAAAAAAAAAGHtAgAAAAAA%2Bx7ILXNILCqkSGIzy6faIHZ9s3Q%3DQy97w6SIrzE7lQwPJEYQBsArEE2fC25caFwRBvAGi456G09vGR';
         $this->data = $cache->loadData() ?? [];
     }
 
