43ae240c1f64e03d841ecd4652ba505803a0be90
Ryan Stafford <ryan@nanorex.net>
2023-07-14 23:37:38 -0400

    lint fixes

--- bridges/QwantzBridge.php
+++ bridges/QwantzBridge.php
@@ -9,7 +9,7 @@ class QwantzBridge extends FeedExpander
     protected function parseItem($feedItem)
     {
         $item = parent::parseItem($feedItem);
-        $item['author'] = "Ryan North";
+        $item['author'] = 'Ryan North';
 
         preg_match('/title="(.*?)"/', $item['content'], $matches);
         $title = $matches[1];
@@ -17,7 +17,7 @@ class QwantzBridge extends FeedExpander
         $content = str_get_html(html_entity_decode($item['content']));
         $comicURL = $content->find('img')[0]->{'src'};
         $subject = $content->find('a')[1]->{'href'};
-        $subject = urldecode(substr($subject, strpos($subject, "subject") + 8));
+        $subject = urldecode(substr($subject, strpos($subject, 'subject') + 8));
         $p = (string)$content->find('P')[0];
 
         $item['content'] = "{$subject}<figure><img src=\"{$comicURL}\"><figcaption><p>{$title}</p></figcaption></figure>{$p}";
@@ -30,7 +30,8 @@ class QwantzBridge extends FeedExpander
         $this->collectExpandableDatas(self::URI . 'rssfeed.php');
     }
 
-    public function getIcon() {
+    public function getIcon()
+    {
         return self::URI . 'favicon.ico';
     }
 }
