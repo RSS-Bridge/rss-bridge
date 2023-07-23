77ce3010cee3f2544e11b4c5fff8c1b1a26a3c9b
csisoap <33269526+csisoap@users.noreply.github.com>
2023-07-23 19:48:32 +0700

    clear whitespace, change single quote

--- lib/TwitterClient.php
+++ lib/TwitterClient.php
@@ -79,18 +79,18 @@ class TwitterClient
                     continue;
                 }
                 $tweets[] = $entry->content->itemContent->tweet_results->result->legacy;
-    
+
                 $userIds[] = $entry->content->itemContent->tweet_results->result->core->user_results->result;
             } else {
                 if (!isset($entry->content->content->tweetResult->result->legacy)) {
                     continue;
                 }
                 $tweets[] = $entry->content->content->tweetResult->result->legacy;
-    
+
                 $userIds[] = $entry->content->content->tweetResult->result->core->user_result->result;
             }
         }
-       
+
         return (object) [
             'userIds' => $userIds,
             'tweets' => $tweets,
@@ -137,7 +137,7 @@ class TwitterClient
         ];
     }
 
-    public function fetchListTweets($query, $operation = '') 
+    public function fetchListTweets($query, $operation = '')
     {
         $id = '';
         $this->fetchGuestToken();
@@ -291,50 +291,50 @@ class TwitterClient
         }
 
         $features = [
-            "android_graphql_skip_api_media_color_palette" => false,
-            "blue_business_profile_image_shape_enabled" => false,
-            "creator_subscriptions_subscription_count_enabled" => false,
-            "creator_subscriptions_tweet_preview_api_enabled" => true,
-            "freedom_of_speech_not_reach_fetch_enabled" => false,
-            "graphql_is_translatable_rweb_tweet_is_translatable_enabled" => false,
-            "hidden_profile_likes_enabled" => false,
-            "highlights_tweets_tab_ui_enabled" => false,
-            "interactive_text_enabled" => false,
-            "longform_notetweets_consumption_enabled" => true,
-            "longform_notetweets_inline_media_enabled" => false,
-            "longform_notetweets_richtext_consumption_enabled" => true,
-            "longform_notetweets_rich_text_read_enabled" => false,
-            "responsive_web_edit_tweet_api_enabled" => false,
-            "responsive_web_enhance_cards_enabled" => false,
-            "responsive_web_graphql_exclude_directive_enabled" => true,
-            "responsive_web_graphql_skip_user_profile_image_extensions_enabled" => false,
-            "responsive_web_graphql_timeline_navigation_enabled" => false,
-            "responsive_web_media_download_video_enabled" => false,
-            "responsive_web_text_conversations_enabled" => false,
-            "responsive_web_twitter_article_tweet_consumption_enabled" => false,
-            "responsive_web_twitter_blue_verified_badge_is_enabled" => true,
-            "rweb_lists_timeline_redesign_enabled" => true,
-            "spaces_2022_h2_clipping" => true,
-            "spaces_2022_h2_spaces_communities" => true,
-            "standardized_nudges_misinfo" => false,
-            "subscriptions_verification_info_enabled" => true,
-            "subscriptions_verification_info_reason_enabled" => true,
-            "subscriptions_verification_info_verified_since_enabled" => true,
-            "super_follow_badge_privacy_enabled" => false,
-            "super_follow_exclusive_tweet_notifications_enabled" => false,
-            "super_follow_tweet_api_enabled" => false,
-            "super_follow_user_api_enabled" => false,
-            "tweet_awards_web_tipping_enabled" => false,
-            "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled" => false,
-            "tweetypie_unmention_optimization_enabled" => false,
-            "unified_cards_ad_metadata_container_dynamic_card_content_query_enabled" => false,
-            "verified_phone_label_enabled" => false,
-            "vibe_api_enabled" => false,
-            "view_counts_everywhere_api_enabled" => false
+            'android_graphql_skip_api_media_color_palette' => false,
+            'blue_business_profile_image_shape_enabled' => false,
+            'creator_subscriptions_subscription_count_enabled' => false,
+            'creator_subscriptions_tweet_preview_api_enabled' => true,
+            'freedom_of_speech_not_reach_fetch_enabled' => false,
+            'graphql_is_translatable_rweb_tweet_is_translatable_enabled' => false,
+            'hidden_profile_likes_enabled' => false,
+            'highlights_tweets_tab_ui_enabled' => false,
+            'interactive_text_enabled' => false,
+            'longform_notetweets_consumption_enabled' => true,
+            'longform_notetweets_inline_media_enabled' => false,
+            'longform_notetweets_richtext_consumption_enabled' => true,
+            'longform_notetweets_rich_text_read_enabled' => false,
+            'responsive_web_edit_tweet_api_enabled' => false,
+            'responsive_web_enhance_cards_enabled' => false,
+            'responsive_web_graphql_exclude_directive_enabled' => true,
+            'responsive_web_graphql_skip_user_profile_image_extensions_enabled' => false,
+            'responsive_web_graphql_timeline_navigation_enabled' => false,
+            'responsive_web_media_download_video_enabled' => false,
+            'responsive_web_text_conversations_enabled' => false,
+            'responsive_web_twitter_article_tweet_consumption_enabled' => false,
+            'responsive_web_twitter_blue_verified_badge_is_enabled' => true,
+            'rweb_lists_timeline_redesign_enabled' => true,
+            'spaces_2022_h2_clipping' => true,
+            'spaces_2022_h2_spaces_communities' => true,
+            'standardized_nudges_misinfo' => false,
+            'subscriptions_verification_info_enabled' => true,
+            'subscriptions_verification_info_reason_enabled' => true,
+            'subscriptions_verification_info_verified_since_enabled' => true,
+            'super_follow_badge_privacy_enabled' => false,
+            'super_follow_exclusive_tweet_notifications_enabled' => false,
+            'super_follow_tweet_api_enabled' => false,
+            'super_follow_user_api_enabled' => false,
+            'tweet_awards_web_tipping_enabled' => false,
+            'tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled' => false,
+            'tweetypie_unmention_optimization_enabled' => false,
+            'unified_cards_ad_metadata_container_dynamic_card_content_query_enabled' => false,
+            'verified_phone_label_enabled' => false,
+            'vibe_api_enabled' => false,
+            'view_counts_everywhere_api_enabled' => false
         ];
         $variables = [
-            "screenName" => $screenName,
-            "listSlug" => $listSlug
+            'screenName' => $screenName,
+            'listSlug' => $listSlug
         ];
 
         $url = sprintf(
@@ -360,50 +360,50 @@ class TwitterClient
     private function fetchListTimeline($listId)
     {
         $features = [
-            "android_graphql_skip_api_media_color_palette" => false,
-            "blue_business_profile_image_shape_enabled" => false,
-            "creator_subscriptions_subscription_count_enabled" => false,
-            "creator_subscriptions_tweet_preview_api_enabled" => true,
-            "freedom_of_speech_not_reach_fetch_enabled" => false,
-            "graphql_is_translatable_rweb_tweet_is_translatable_enabled" => false,
-            "hidden_profile_likes_enabled" => false,
-            "highlights_tweets_tab_ui_enabled" => false,
-            "interactive_text_enabled" => false,
-            "longform_notetweets_consumption_enabled" => true,
-            "longform_notetweets_inline_media_enabled" => false,
-            "longform_notetweets_richtext_consumption_enabled" => true,
-            "longform_notetweets_rich_text_read_enabled" => false,
-            "responsive_web_edit_tweet_api_enabled" => false,
-            "responsive_web_enhance_cards_enabled" => false,
-            "responsive_web_graphql_exclude_directive_enabled" => true,
-            "responsive_web_graphql_skip_user_profile_image_extensions_enabled" => false,
-            "responsive_web_graphql_timeline_navigation_enabled" => false,
-            "responsive_web_media_download_video_enabled" => false,
-            "responsive_web_text_conversations_enabled" => false,
-            "responsive_web_twitter_article_tweet_consumption_enabled" => false,
-            "responsive_web_twitter_blue_verified_badge_is_enabled" => true,
-            "rweb_lists_timeline_redesign_enabled" => true,
-            "spaces_2022_h2_clipping" => true,
-            "spaces_2022_h2_spaces_communities" => true,
-            "standardized_nudges_misinfo" => false,
-            "subscriptions_verification_info_enabled" => true,
-            "subscriptions_verification_info_reason_enabled" => true,
-            "subscriptions_verification_info_verified_since_enabled" => true,
-            "super_follow_badge_privacy_enabled" => false,
-            "super_follow_exclusive_tweet_notifications_enabled" => false,
-            "super_follow_tweet_api_enabled" => false,
-            "super_follow_user_api_enabled" => false,
-            "tweet_awards_web_tipping_enabled" => false,
-            "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled" => false,
-            "tweetypie_unmention_optimization_enabled" => false,
-            "unified_cards_ad_metadata_container_dynamic_card_content_query_enabled" => false,
-            "verified_phone_label_enabled" => false,
-            "vibe_api_enabled" => false,
-            "view_counts_everywhere_api_enabled" => false
+            'android_graphql_skip_api_media_color_palette' => false,
+            'blue_business_profile_image_shape_enabled' => false,
+            'creator_subscriptions_subscription_count_enabled' => false,
+            'creator_subscriptions_tweet_preview_api_enabled' => true,
+            'freedom_of_speech_not_reach_fetch_enabled' => false,
+            'graphql_is_translatable_rweb_tweet_is_translatable_enabled' => false,
+            'hidden_profile_likes_enabled' => false,
+            'highlights_tweets_tab_ui_enabled' => false,
+            'interactive_text_enabled' => false,
+            'longform_notetweets_consumption_enabled' => true,
+            'longform_notetweets_inline_media_enabled' => false,
+            'longform_notetweets_richtext_consumption_enabled' => true,
+            'longform_notetweets_rich_text_read_enabled' => false,
+            'responsive_web_edit_tweet_api_enabled' => false,
+            'responsive_web_enhance_cards_enabled' => false,
+            'responsive_web_graphql_exclude_directive_enabled' => true,
+            'responsive_web_graphql_skip_user_profile_image_extensions_enabled' => false,
+            'responsive_web_graphql_timeline_navigation_enabled' => false,
+            'responsive_web_media_download_video_enabled' => false,
+            'responsive_web_text_conversations_enabled' => false,
+            'responsive_web_twitter_article_tweet_consumption_enabled' => false,
+            'responsive_web_twitter_blue_verified_badge_is_enabled' => true,
+            'rweb_lists_timeline_redesign_enabled' => true,
+            'spaces_2022_h2_clipping' => true,
+            'spaces_2022_h2_spaces_communities' => true,
+            'standardized_nudges_misinfo' => false,
+            'subscriptions_verification_info_enabled' => true,
+            'subscriptions_verification_info_reason_enabled' => true,
+            'subscriptions_verification_info_verified_since_enabled' => true,
+            'super_follow_badge_privacy_enabled' => false,
+            'super_follow_exclusive_tweet_notifications_enabled' => false,
+            'super_follow_tweet_api_enabled' => false,
+            'super_follow_user_api_enabled' => false,
+            'tweet_awards_web_tipping_enabled' => false,
+            'tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled' => false,
+            'tweetypie_unmention_optimization_enabled' => false,
+            'unified_cards_ad_metadata_container_dynamic_card_content_query_enabled' => false,
+            'verified_phone_label_enabled' => false,
+            'vibe_api_enabled' => false,
+            'view_counts_everywhere_api_enabled' => false
         ];
         $variables = [
-            "rest_id" => $listId,
-            "count" => 20
+            'rest_id' => $listId,
+            'count' => 20
         ];
 
         $url = sprintf(
