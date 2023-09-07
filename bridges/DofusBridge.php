bf2247dcc1546f9a6e4d450e3b7fdf08c6745322
Jisagi <Jisagi@users.noreply.github.com>
2023-09-07 15:15:41 +0200

    fix indentation

--- bridges/DofusBridge.php
+++ bridges/DofusBridge.php
@@ -2,53 +2,53 @@
 
 class DofusBridge extends FeedExpander
 {
-  const MAINTAINER = 'Jisagi';
-  const NAME = 'Dofus';
-  const URI = 'https://www.dofus.com/';
-  const DESCRIPTION = 'Returns latest news for the MMORPG dofus.';
+    const MAINTAINER = 'Jisagi';
+    const NAME = 'Dofus';
+    const URI = 'https://www.dofus.com/';
+    const DESCRIPTION = 'Returns latest news for the MMORPG dofus.';
 
-  const PARAMETERS = [
-    [
-      'c' => [
-        'name' => 'Category',
-        'type' => 'list',
-        'required' => true,
-        'values' => [
-          'News' => 'news',
-          'Changelog' => 'changelog',
-          'Dev Blog' => 'devblog'
+    const PARAMETERS = [
+        [
+            'c' => [
+                'name' => 'Category',
+                'type' => 'list',
+                'required' => true,
+                'values' => [
+                    'News' => 'news',
+                    'Changelog' => 'changelog',
+                    'Dev Blog' => 'devblog'
+                ]
+            ],
+            'l' => [
+                'name' => 'Language',
+                'type' => 'list',
+                'required' => true,
+                'values' => [
+                    'English' => 'en',
+                    'French' => 'fr',
+                    'Spanish' => 'es',
+                    'Portuguese' => 'pt'
+                ]
+            ]
         ]
-      ],
-      'l' => [
-        'name' => 'Language',
-        'type' => 'list',
-        'required' => true,
-        'values' => [
-          'English' => 'en',
-          'French' => 'fr',
-          'Spanish' => 'es',
-          'Portuguese' => 'pt'
-        ]
-      ]
-    ]
-  ];
+    ];
 
-  public function getIcon()
-  {
-    return self::URI . 'favicon.ico';
-  }
+    public function getIcon()
+    {
+        return self::URI . 'favicon.ico';
+    }
 
-  public function getURI()
-  {
-    return self::URI
-      . $this->getInput('l')
-      . '/rss/'
-      . $this->getInput('c')
-      . '.xml';
-  }
+    public function getURI()
+    {
+        return self::URI
+            . $this->getInput('l')
+            . '/rss/'
+            . $this->getInput('c')
+            . '.xml';
+    }
 
-  public function collectData()
-  {
-    $this->collectExpandableDatas($this->getURI(), 10);
-  }
+    public function collectData()
+    {
+        $this->collectExpandableDatas($this->getURI(), 10);
+    }
 }
