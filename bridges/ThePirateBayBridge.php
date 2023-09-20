b5bdafbc113ce8c4480492d3e5f014bb5b473693
Dag <me@dvikan.no>
2023-09-20 03:08:31 +0200

    tweaks

--- bridges/ThePirateBayBridge.php
+++ bridges/ThePirateBayBridge.php
@@ -65,6 +65,7 @@ class ThePirateBayBridge extends BridgeAbstract
         '207' => 'HD Movies',
         '208' => 'HD TV-Shows',
         '209' => '3D',
+        '211' => 'UHD/4k Movies',
         '212' => 'UHD/4k TV-Shows',
         '299' => 'Other',
         '301' => 'Windows',
