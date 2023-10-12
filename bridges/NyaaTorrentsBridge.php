3d8ef7f1bf5a403ff1d84f0e15bd6ad1b62120d2
Dag <me@dvikan.no>
2023-10-13 00:59:59 +0200

    refactor

--- bridges/NyaaTorrentsBridge.php
+++ bridges/NyaaTorrentsBridge.php
@@ -1,6 +1,6 @@
 <?php
 
-class NyaaTorrentsBridge extends FeedExpander
+class NyaaTorrentsBridge extends BridgeAbstract
 {
     const MAINTAINER = 'ORelio & Jisagi';
     const NAME = 'NyaaTorrents';
@@ -62,44 +62,57 @@ class NyaaTorrentsBridge extends FeedExpander
 
     public function collectData()
     {
-        $this->collectExpandableDatas($this->getURI(), 20);
-    }
+        // Manually parsing because we need to acccess the nyaa namespace in the xml
+        $xml = simplexml_load_string(getContents($this->getURI()));
+        $channel = $xml->channel[0];
+        $feed = [];
+        $feed['title'] = trim((string)$channel->title);
+        $feed['uri'] = trim((string)$channel->link);
+        if (!empty($channel->image)) {
+            $feed['icon'] = trim((string)$channel->image->url);
+        }
+        $items = $xml->channel[0]->item;
+        foreach ($items as $feedItem) {
+            $item = [
+                'title' => (string) $feedItem->title,
+                'uri' => (string) $feedItem->link,
+            ];
 
-    protected function parseItem($newsItem)
-    {
-        $item = parent::parseItem($newsItem);
 
-        $nyaaFields = (array)($newsItem->children('nyaa', true));
+            $item['id'] = str_replace(['https://nyaa.si/download/', '.torrent'], '', $item['uri']);
 
-        $item['id'] = str_replace(['https://nyaa.si/download/', '.torrent'], '', $item['uri']);
+            $nyaaNamespace = (array)($feedItem->children('nyaa', true));
+            $item = array_merge($item, $nyaaNamespace);
 
-        $item = array_merge($item, $nyaaFields);
+            // Convert URI from torrent file to web page
+            $item['uri'] = str_replace('/download/', '/view/', $item['uri']);
+            $item['uri'] = str_replace('.torrent', '', $item['uri']);
 
-        // Convert URI from torrent file to web page
-        $item['uri'] = str_replace('/download/', '/view/', $item['uri']);
-        $item['uri'] = str_replace('.torrent', '', $item['uri']);
+            $item_html = getSimpleHTMLDOMCached($item['uri']);
+            if ($item_html) {
+                // Retrieve full description from page contents
+                $item_desc = str_get_html(
+                    markdownToHtml(html_entity_decode($item_html->find('#torrent-description', 0)->innertext))
+                );
 
-        $item_html = getSimpleHTMLDOMCached($item['uri']);
-        if ($item_html) {
-            // Retrieve full description from page contents
-            $item_desc = str_get_html(
-                markdownToHtml(html_entity_decode($item_html->find('#torrent-description', 0)->innertext))
-            );
-
-            // Retrieve image for thumbnail or generic logo fallback
-            $item_image = $this->getURI() . 'static/img/avatar/default.png';
-            foreach ($item_desc->find('img') as $img) {
-                if (strpos($img->src, 'prez') === false) {
-                    $item_image = $img->src;
-                    break;
+                // Retrieve image for thumbnail or generic logo fallback
+                $item_image = $this->getURI() . 'static/img/avatar/default.png';
+                foreach ($item_desc->find('img') as $img) {
+                    if (strpos($img->src, 'prez') === false) {
+                        $item_image = $img->src;
+                        break;
+                    }
                 }
+
+                $item['enclosures'] = [$item_image];
+                $item['content'] = $item_desc;
             }
 
-            $item['enclosures'] = [$item_image];
-            $item['content'] = $item_desc;
+            $this->items[] = $item;
+            if (count($this->items) >= 10) {
+                break;
+            }
         }
-
-        return $item;
     }
 
     public function getIcon()
