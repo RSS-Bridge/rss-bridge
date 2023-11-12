b4479a6d59df1344c7f0aa8891a62ce142e1af2e
knrdl <35548889+knrdl@users.noreply.github.com>
2023-11-12 19:14:59 +0100

    [KleinanzeigenBridge] linting

--- bridges/KleinanzeigenBridge.php
+++ bridges/KleinanzeigenBridge.php
@@ -52,11 +52,13 @@ class KleinanzeigenBridge extends BridgeAbstract
         ],
     ];
 
-    public function getIcon() {
+    public function getIcon()
+    {
         return 'https://www.kleinanzeigen.de/favicon.ico';
     }
 
-    public function getName() {
+    public function getName()
+    {
         switch ($this->queriedContext) {
             case 'By profile':
                 return 'Kleinanzeigen Profil';
@@ -67,24 +69,27 @@ class KleinanzeigenBridge extends BridgeAbstract
         }
     }
 
-    public function collectData() {
+    public function collectData()
+    {
         if ($this->queriedContext === 'By profile') {
             for ($i = 1; $i <= $this->getInput('pages'); $i++) {
                 $html = getSimpleHTMLDOM(self::URI . '/s-bestandsliste.html?userId=' . $this->getInput('userid') . '&pageNum=' . $i . '&sortingField=SORTING_DATE');
-                
+
                 $foundItem = false;
                 foreach ($html->find('article.aditem') as $element) {
                     $this->addItem($element);
                     $foundItem = true;
                 }
-                if (!$foundItem) break;
+                if (!$foundItem) {
+                    break;
+                }
             }
         }
 
         if ($this->queriedContext === 'By search') {
             $locationID = '';
             if ($this->getInput('location')) {
-                $json = getContents(self::URI . '/s-ort-empfehlungen.json?' . http_build_query(array('query' => $this->getInput('location'))));
+                $json = getContents(self::URI . '/s-ort-empfehlungen.json?' . http_build_query(['query' => $this->getInput('location')]));
                 $jsonFile = json_decode($json, true);
                 $locationID = str_replace('_', '', array_key_first($jsonFile));
             }
@@ -110,12 +115,15 @@ class KleinanzeigenBridge extends BridgeAbstract
                     $this->addItem($element);
                     $foundItem = true;
                 }
-                if (!$foundItem) break;
+                if (!$foundItem) {
+                    break;
+                }
             }
         }
     }
 
-    private function addItem($element) {
+    private function addItem($element)
+    {
         $item = [];
 
         $item['uid'] = $element->getAttribute('data-adid');
@@ -123,13 +131,18 @@ class KleinanzeigenBridge extends BridgeAbstract
 
         $item['title'] = $element->find('h2', 0)->plaintext;
         $item['timestamp'] = $element->find('div.aditem-main--top--right', 0)->plaintext;
-        $imgUrl = str_replace('rule=$_2.JPG', 'rule=$_57.JPG', 
-            str_replace('rule=$_35.JPG', 'rule=$_57.JPG', 
+        $imgUrl = str_replace(
+            'rule=$_2.JPG',
+            'rule=$_57.JPG',
+            str_replace(
+                'rule=$_35.JPG',
+                'rule=$_57.JPG',
                 $element->find('img', 0) ? $element->find('img', 0)->getAttribute('src') : ''
-        )); //enhance img quality
+            )
+        ); //enhance img quality
         $textContainer = $element->find('div.aditem-main', 0);
         $textContainer->find('a', 0)->href = self::URI . $textContainer->find('a', 0)->href; // add domain to url
-        $item['content'] = '<img src="' . $imgUrl . '"/>' . 
+        $item['content'] = '<img src="' . $imgUrl . '"/>' .
         $textContainer->outertext;
 
         $this->items[] = $item;
