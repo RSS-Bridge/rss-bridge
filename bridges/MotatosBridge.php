15b94c46d13ee56b466d83412374fe93d798889c
knrdl <35548889+knrdl@users.noreply.github.com>
2023-11-12 21:26:50 +0100

    [MotatosBridge] fix uid as string

--- bridges/MotatosBridge.php
+++ bridges/MotatosBridge.php
@@ -18,12 +18,12 @@ class MotatosBridge extends BridgeAbstract
     public function collectData()
     {
         // motatos uses this api to dynamically load more items on page scroll
-        $json = getContents('https://api.findify.io/v4/2a044754-6cda-4541-b159-39133b75386c/smart-collection/new-arrivals?t_client=' . time() . '&user={%22uid%22:%220%22,%22sid%22:%220%22}');
+        $json = getContents('https://api.findify.io/v4/2a044754-6cda-4541-b159-39133b75386c/smart-collection/new-arrivals?t_client=0&user={%22uid%22:%220%22,%22sid%22:%220%22}');
         $jsonFile = json_decode($json, true);
 
         foreach ($jsonFile['items'] as $entry) {
             $item = [];
-            $item['uid'] = $entry['custom_fields']['uuid'];
+            $item['uid'] = $entry['custom_fields']['uuid'][0];
             $item['uri'] = $entry['product_url'];
             $item['timestamp'] = $entry['created_at'] / 1000;
             $item['title'] = $entry['title'];
