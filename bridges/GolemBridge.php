d4ae55733b6f7684de0b878d91e53c8a5f917d41
Tone <66808319+Tone866@users.noreply.github.com>
2023-12-15 23:39:27 +0100

    Update GolemBridge.php (#3836)
    deleted the code which adds the author to the feed, because the author is already in the original feed, so it is not needed.

--- bridges/GolemBridge.php
+++ bridges/GolemBridge.php
@@ -82,11 +82,6 @@ class GolemBridge extends FeedExpander
             // URI without RSS feed reference
             $item['uri'] = $articlePage->find('head meta[name="twitter:url"]', 0)->content;
 
-            $author = $articlePage->find('article header .authors .authors__name', 0);
-            if ($author) {
-                $item['author'] = $author->plaintext;
-            }
-
             $categories = $articlePage->find('ul.tags__list li');
             foreach ($categories as $category) {
                 $trimmedcategories[] = trim(html_entity_decode($category->plaintext));
