19023689334a1e41cd96165d35f74033b3d1e61a
Arnav Jain <git@arnav.jain.se>
2023-12-15 11:29:03 +0100

    [DagensNyheterDirektBridge] Lint: Replace all tabs with space

--- bridges/DagensNyheterDirektBridge.php
+++ bridges/DagensNyheterDirektBridge.php
@@ -1,60 +1,60 @@
 <?PHP
 class DagensNyheterDirektBridge extends BridgeAbstract {
 
-	const NAME			= 'Dagens Nyheter Direkt';
-	const URI			= 'https://www.dn.se/direkt/';
-	const BASEURL		= 'https://www.dn.se';
-	const DESCRIPTION	= 'Latest news summarised by Dagens Nyheter';
-	const MAINTAINER	= 'ajain-93';
-	const LIMIT			= 20;
+    const NAME			= 'Dagens Nyheter Direkt';
+    const URI			= 'https://www.dn.se/direkt/';
+    const BASEURL		= 'https://www.dn.se';
+    const DESCRIPTION	= 'Latest news summarised by Dagens Nyheter';
+    const MAINTAINER	= 'ajain-93';
+    const LIMIT			= 20;
 
-	public function getIcon(){
-		return 'https://cdn.dn-static.se/images/favicon__c2dd3284b46ffdf4d520536e526065fa8.svg';
-	}
+    public function getIcon(){
+        return 'https://cdn.dn-static.se/images/favicon__c2dd3284b46ffdf4d520536e526065fa8.svg';
+    }
 
-	public function collectData(){
+    public function collectData(){
 
-		$NEWSURL = self::BASEURL . '/ajax/direkt/';
+        $NEWSURL = self::BASEURL . '/ajax/direkt/';
 
-		$html = getSimpleHTMLDOM($NEWSURL)
-			or returnServerError('Could not request: ' . $NEWSURL);
+        $html = getSimpleHTMLDOM($NEWSURL)
+            or returnServerError('Could not request: ' . $NEWSURL);
 
-		foreach($html->find('article') as $element) {
+        foreach($html->find('article') as $element) {
 
-			$link = $element->find('button', 0)->getAttribute('data-link');
-			$datetime = $element->getAttribute('data-publication-time');
-			$url = self::BASEURL . $link;
-			$title = $element->find('h2', 0)->plaintext;
-			$author = $element->find('div.ds-byline__titles', 0)->plaintext;
-			// Debug::log($link);
-			// Debug::log($datetime);
-			// Debug::log($title);
-			// Debug::log($url);
-			// Debug::log($author);
+            $link = $element->find('button', 0)->getAttribute('data-link');
+            $datetime = $element->getAttribute('data-publication-time');
+            $url = self::BASEURL . $link;
+            $title = $element->find('h2', 0)->plaintext;
+            $author = $element->find('div.ds-byline__titles', 0)->plaintext;
+            // Debug::log($link);
+            // Debug::log($datetime);
+            // Debug::log($title);
+            // Debug::log($url);
+            // Debug::log($author);
 
-			$article_content = $element->find('div.direkt-post__content', 0);
-			$article_html = '';
+            $article_content = $element->find('div.direkt-post__content', 0);
+            $article_html = '';
 
-			$figure = $element->find('figure', 0);
-			if ($figure) {
-				$article_html = $figure->find('img', 0) . '<p><i>' . $figure->find('figcaption', 0) . '</i></p>';
-			}
+            $figure = $element->find('figure', 0);
+            if ($figure) {
+                $article_html = $figure->find('img', 0) . '<p><i>' . $figure->find('figcaption', 0) . '</i></p>';
+            }
 
-			foreach($article_content->find('p') as $p) {
-				$article_html = $article_html . $p;
-			}
+            foreach($article_content->find('p') as $p) {
+                $article_html = $article_html . $p;
+            }
 
-			$item = array();
-			$item['uri'] = $url;
-			$item['title'] = $title;
-			$item['author'] = trim($author);
-			$item['timestamp'] = $datetime;
-			$item['content'] = trim($article_html);
-			$this->items[] = $item;
+            $item = array();
+            $item['uri'] = $url;
+            $item['title'] = $title;
+            $item['author'] = trim($author);
+            $item['timestamp'] = $datetime;
+            $item['content'] = trim($article_html);
+            $this->items[] = $item;
 
-			if (count($this->items) > self::LIMIT) {
-				break;
-			}
-		}
-	}
+            if (count($this->items) > self::LIMIT) {
+                break;
+            }
+        }
+    }
 }
