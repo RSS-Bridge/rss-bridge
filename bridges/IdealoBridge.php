16ed970219d0a38ef4fc306019fb7b3cd114b8fc
SebLaus <97241865+SebLaus@users.noreply.github.com>
2024-01-18 11:04:31 +0000

    copy paste error from local test environment

--- bridges/IdealoBridge.php
+++ bridges/IdealoBridge.php
@@ -44,7 +44,7 @@ class IdealoBridge extends BridgeAbstract
     {
         // Needs header with user-agent to function properly.
         $header = [
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0'
+            'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2.1 Safari/605.1.15'
         ];
 
         $link = $this->getInput('Link');
