d71e2bcf768b77718ad9efb080ca8a3d07a03bfb
t0stiman <18124323+t0stiman@users.noreply.github.com>
2024-02-04 17:49:43 +0100

    [CarThrottleBridge] remove unused code

--- bridges/CarThrottleBridge.php
+++ bridges/CarThrottleBridge.php
@@ -124,42 +124,4 @@ class CarThrottleBridge extends BridgeAbstract
 
         return $authorDivs[0]->innertext;
     }
-
-    //convert relative url to absolute
-    //https://stackoverflow.com/a/4444490/4671120
-    private function rel2abs($rel, $base)
-    {
-        /* return if already absolute URL */
-        if (parse_url($rel, PHP_URL_SCHEME) != '') {
-            return $rel;
-        }
-
-        /* queries and anchors */
-        if ($rel[0] == '#' || $rel[0] == '?') {
-            return $base . $rel;
-        }
-
-        /* parse base URL and convert to local variables:
-           $scheme, $host, $path */
-        extract(parse_url($base));
-
-        /* remove non-directory element from path */
-        $path = preg_replace('#/[^/]*$#', '', $path);
-
-        /* destroy path if relative url points to root */
-        if ($rel[0] == '/') {
-            $path = '';
-        }
-
-        /* dirty absolute URL */
-        $abs = "$host$path/$rel";
-
-        /* replace '//' or '/./' or '/foo/../' with '/' */
-        $re = ['#(/\.?/)#', '#/(?!\.\.)[^/]+/\.\./#'];
-        for ($n = 1; $n > 0; $abs = preg_replace($re, '/', $abs, -1, $n)) {
-        }
-
-        /* absolute URL is ready! */
-        return $scheme . '://' . $abs;
-    }
 }
