71285bfeedfb4d6ec9d3a3443f902d9174fe78d3
Phantop <phantop@tuta.io>
2024-02-15 10:59:15 -0500

    AO3Bridge: add options to fetch chapter contents and titles for list feeds
    and add downloads for each fic to enclosures

--- bridges/AO3Bridge.php
+++ bridges/AO3Bridge.php
@@ -15,6 +15,18 @@ class AO3Bridge extends BridgeAbstract
                 // Example: F/F tag, complete works only
                 'exampleValue' => 'https://archiveofourown.org/works?work_search[complete]=T&tag_id=F*s*F',
             ],
+            'range' => [
+                'name' => 'Chapter Content',
+                'title' => 'Chapter(s) to include in each work\'s feed entry',
+                'defaultValue' => 'none',
+                'type' => 'list',
+                'values' => [
+                    'None' => null,
+                    'First' => 'first',
+                    'Latest' => 'last',
+                    'Entire work' => 'all',
+                ],
+            ],
         ],
         'Bookmarks' => [
             'user' => [
@@ -61,8 +73,22 @@ class AO3Bridge extends BridgeAbstract
      */
     private function collectList($url)
     {
-        $html = getSimpleHTMLDOM($url);
+        $this->url = $url;
+        $httpClient = RssBridge::getHttpClient();
+
+        $version = 'v0.0.1';
+        $agent = ['useragent' => "rss-bridge $version (https://github.com/RSS-Bridge/rss-bridge)"];
+        $response = $httpClient->request($url, $agent);
+
+        $html = \str_get_html($response->getBody());
         $html = defaultLinkTo($html, self::URI);
+
+        // Get list title. Will include page range + count in some cases
+        $heading = ($html->find('#main > h2', 0));
+        if ($heading->find('a.tag')) {
+            $heading = $heading->find('a.tag', 0);
+        }
+        $this->title = $heading->plaintext;
 
         foreach ($html->find('.index.group > li') as $element) {
             $item = [];
@@ -83,6 +109,36 @@ class AO3Bridge extends BridgeAbstract
             $chapters = (isset($chapters) ? $chapters->plaintext : 0);
             $item['uid'] = $item['uri'] . "/$strdate/$chapters";
 
+            // Fetch workskin of desired chapter(s) in list
+            if ($this->getInput('range')) {
+                $url = $item['uri'];
+                switch ($this->getInput('range')) {
+                    case ('all'):
+                        $url .= '?view_full_work=true';
+                        break;
+                    case ('first'):
+                        break;
+                    case ('last'):
+                        // only way to get this is using the navigate page unfortunately
+                        $url .= '/navigate';
+                        $response = $httpClient->request($url, $agent);
+                        $html = \str_get_html($response->getBody());
+                        $html = defaultLinkTo($html, self::URI);
+                        $url = $html->find('ol.index.group > li > a', -1)->href;
+                        break;
+                }
+                $response = $httpClient->request($url, $agent);
+                $html = \str_get_html($response->getBody());
+                $html = defaultLinkTo($html, self::URI);
+                $item['content'] .= $html->find('#workskin', 0);
+            }
+
+            // Use predictability of download links to generate enclosures
+            $wid = explode('/', $item['uri'])[4];
+            foreach (['azw3', 'epub', 'mobi', 'pdf', 'html'] as $ext) {
+                $item['enclosures'][] = 'https://archiveofourown.org/downloads/' . $wid . '/work.' . $ext;
+            }
+
             $this->items[] = $item;
         }
     }
@@ -92,24 +148,30 @@ class AO3Bridge extends BridgeAbstract
      */
     private function collectWork($id)
     {
-        $url = self::URI . "/works/$id/navigate";
+        $url = self::URI . "/works/$id";
+        $this->url = $url;
         $httpClient = RssBridge::getHttpClient();
 
         $version = 'v0.0.1';
-        $response = $httpClient->request($url, [
-            'useragent' => "rss-bridge $version (https://github.com/RSS-Bridge/rss-bridge)",
-        ]);
+        $agent = ['useragent' => "rss-bridge $version (https://github.com/RSS-Bridge/rss-bridge)"];
 
+        $response = $httpClient->request($url . '/navigate', $agent);
         $html = \str_get_html($response->getBody());
         $html = defaultLinkTo($html, self::URI);
 
+        $response = $httpClient->request($url . '?view_full_work=true', $agent);
+        $workhtml = \str_get_html($response->getBody());
+        $workhtml = defaultLinkTo($workhtml, self::URI);
+
         $this->title = $html->find('h2 a', 0)->plaintext;
 
-        foreach ($html->find('ol.index.group > li') as $element) {
+        $nav = $html->find('ol.index.group > li');
+        for ($i = 0; $i < count($nav); $i++) {
             $item = [];
 
+            $element = $nav[$i];
             $item['title'] = $element->find('a', 0)->plaintext;
-            $item['content'] = $element;
+            $item['content'] = $workhtml->find('#chapter-' . ($i + 1), 0);
             $item['uri'] = $element->find('a', 0)->href;
 
             $strdate = $element->find('span.datetime', 0)->plaintext;
@@ -137,5 +199,14 @@ class AO3Bridge extends BridgeAbstract
     public function getIcon()
     {
         return self::URI . '/favicon.ico';
+    }
+
+    public function getURI()
+    {
+        $uri = parent::getURI();
+        if (isset($this->url)) {
+            $uri = $this->url;
+        }
+        return $uri;
     }
 }
