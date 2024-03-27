f273999743527cdbdaaec85431a223d4427f5e8b
Tone <66808319+Tone866@users.noreply.github.com>
2024-03-27 12:47:57 +0100

    Update GolemBridge.php

--- bridges/GolemBridge.php
+++ bridges/GolemBridge.php
@@ -113,7 +113,8 @@ class GolemBridge extends FeedExpander
             $link = 'https://' . str_replace('\\', '', $link[0]);
             $embedcontent->innertext .= <<<EOT
                 <iframe width="560" height="315" src=$link title="YouTube video player" frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"                                                 referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
             EOT;
         }
 
