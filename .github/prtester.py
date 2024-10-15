7755025e87c5b0536f72ec11323d78b373a5735b
Bocki <henning@bocklage.com>
2024-10-15 17:53:43 +0200

    Update prtester.py

--- .github/prtester.py
+++ .github/prtester.py
@@ -4,7 +4,6 @@ import re
 from bs4 import BeautifulSoup
 from datetime import datetime
 from typing import Iterable
-#import os.path
 import os
 import urllib
 
@@ -41,7 +40,7 @@ def main(instances: Iterable[Instance], with_upload: bool, with_reduced_upload: 
 def testBridges(instance: Instance, bridge_cards: Iterable, with_upload: bool, with_reduced_upload: bool) -> Iterable:
     instance_suffix = ''
     prid = os.getenv("PR")
-    tester_url = f'https://bockiii.github.io/prs/{prid}'
+    tester_url = f'https://rss-bridge.github.io/rss-bridge-tests/prs/{prid}'
     if instance.name:
         instance_suffix = f' ({instance.name})'
     table_rows = []
