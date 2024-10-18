12cbedf3c7d39e4b9460467ecc8f253256a1978e
Jonas Taedcke <1809673+jonastaedcke@users.noreply.github.com>
2024-10-18 06:10:27 +0200

    [AppleMusicBridge] update content string

--- bridges/AppleMusicBridge.php
+++ bridges/AppleMusicBridge.php
@@ -34,6 +34,7 @@ class AppleMusicBridge extends BridgeAbstract
                 $copyright = $item->copyright ?? '';
                 $artworkUrl500 = str_replace('/100x100', '/500x500', $item->artworkUrl100);
                 $artworkUrl2000 = str_replace('/100x100', '/2000x2000', $item->artworkUrl100);
+                $escapedCollectioName = htmlspecialchars($item->collectionName);
 
                 $this->items[] = [
                     'title' => $item->collectionName,
@@ -41,22 +42,12 @@ class AppleMusicBridge extends BridgeAbstract
                     'timestamp' => $item->releaseDate,
                     'enclosures' => $artworkUrl500,
                     'author' => $item->artistName,
-                    'content' => '<figure>'
-                        . '<img'
-                        . ' srcset="'
-                        . $item->artworkUrl60 . ' 60w'
-                        . ', ' . $item->artworkUrl100 . ' 100w'
-                        . ', ' . $artworkUrl500 . ' 500w'
-                        . ', ' . $artworkUrl2000 . ' 2000w"'
-                        . ' sizes="100%"'
-                        . ' src="' . $artworkUrl2000 . '"'
-                        . ' alt="Cover of ' . str_replace("\"", "\\\"", $item->collectionName) . '"'
-                        . ' style="display: block; margin: 0 auto;" />'
-                        . '<figcaption>'
-                        . 'from <a href="' . $artist->artistLinkUrl . '">' . $item->artistName . '</a><br />'
-                        . $copyright
-                        . '</figcaption>'
-                        . '</figure>',
+                    'content' => "<figure>
+    <img srcset=\"$item->artworkUrl60 60w, $item->artworkUrl100 100w, $artworkUrl500 500w, $artworkUrl2000 2000w\" sizes=\"100%\" src=\"$artworkUrl2000\" alt=\"Cover of $escapedCollectioName\" style=\"display: block; margin: 0 auto;\" />
+    <figcaption>
+        from <a href=\"$artist->artistLinkUrl\">$item->artistName</a><br />$copyright
+    </figcaption>
+</figure>",
                 ];
             }
         }
