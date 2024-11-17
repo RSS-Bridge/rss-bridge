87d96f1369640b75ef6bab63fddd7237702ac97a
Aleksei Besogonov <b@alex.net>
2024-11-16 16:55:00 -0800

    Add title and alt text to Oglaf feed

--- bridges/OglafBridge.php
+++ bridges/OglafBridge.php
@@ -28,7 +28,9 @@ class OglafBridge extends FeedExpander
     {
         $html = getSimpleHTMLDOMCached($item['uri']);
         $comicImage = $html->find('img[id="strip"]', 0);
-        $item['content'] = $comicImage;
+        $alt = $comicImage->getAttribute("alt");
+        $title = $comicImage->getAttribute("title");
+        $item['content'] = $comicImage . "<h3>Alt: {$alt}</h3><h3>Title: {$title}</h3>";
 
         return $item;
     }
