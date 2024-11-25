f7d1a483003cbb37f9a1b61b2650416ccf45fc66
thomas-333 <144368654+thomas-333@users.noreply.github.com>
2024-11-25 08:38:13 +0000

    Update BlueskyBridge.php to pass lint checks

--- bridges/BlueskyBridge.php
+++ bridges/BlueskyBridge.php
@@ -1,74 +1,80 @@
 <?php
 
-class BlueskyBridge extends BridgeAbstract {
+class BlueskyBridge extends BridgeAbstract
+{
     const NAME = 'Bluesky';
     const URI = 'https://bsky.app';
     const DESCRIPTION = 'Fetches posts from Bluesky';
     const MAINTAINER = 'Code modified from rsshub (TonyRL https://github.com/TonyRL) and expanded';
-	const PARAMETERS = [
-		[
-			'data_source' => [
-				'name' => 'Bluesky Data Source',
-				'type' => 'list',
-				'defaultValue' => 'Profile',
-				'values' => [
-					'Profile' => 'getAuthorFeed', 
-				],
-				'title' => 'Select the type of data source to fetch from Bluesky.'
-			],
-			'handle' => [
-				'name' => 'User Handle',
-				'type' => 'text',
-				'required' => true,
-				'exampleValue' => 'jackdodo.bsky.social',
-				'title' => 'Handle found in URL'
-			],
-			'filter' => [
-				'name' => 'Filter',
-				'type' => 'list',
-				'defaultValue' => 'posts_and_author_threads',
-				'values' => [
-					'posts_and_author_threads' => 'posts_and_author_threads',
-					'posts_with_replies' => 'posts_with_replies',
-					'posts_no_replies' => 'posts_no_replies',
-					'posts_with_media' => 'posts_with_media',
-				],
-				'title' => 'Combinations of post/repost types to include in response.'
-			]
-		]
-	];
+    const PARAMETERS = [
+        [
+            'data_source' => [
+                'name' => 'Bluesky Data Source',
+                'type' => 'list',
+                'defaultValue' => 'Profile',
+                'values' => [
+                    'Profile' => 'getAuthorFeed',
+                ],
+                'title' => 'Select the type of data source to fetch from Bluesky.'
+            ],
+            'handle' => [
+                'name' => 'User Handle',
+                'type' => 'text',
+                'required' => true,
+                'exampleValue' => 'jackdodo.bsky.social',
+                'title' => 'Handle found in URL'
+            ],
+            'filter' => [
+                'name' => 'Filter',
+                'type' => 'list',
+                'defaultValue' => 'posts_and_author_threads',
+                'values' => [
+                    'posts_and_author_threads' => 'posts_and_author_threads',
+                    'posts_with_replies' => 'posts_with_replies',
+                    'posts_no_replies' => 'posts_no_replies',
+                    'posts_with_media' => 'posts_with_media',
+                ],
+                'title' => 'Combinations of post/repost types to include in response.'
+            ]
+        ]
+    ];
 
     private $profile;
 
-    public function getName() {
+    public function getName()
+    {
         if (isset($this->profile)) {
-            return sprintf("%s (@%s) - Bluesky", $this->profile['displayName'], $this->profile['handle']);
+            return sprintf('%s (@%s) - Bluesky', $this->profile['displayName'], $this->profile['handle']);
         }
         return parent::getName();
     }
 
-    public function getURI() {
+    public function getURI()
+    {
         if (isset($this->profile)) {
             return self::URI . '/profile/' . $this->profile['handle'];
         }
         return parent::getURI();
     }
 
-    public function getIcon() {
+    public function getIcon()
+    {
         if (isset($this->profile)) {
             return $this->profile['avatar'];
         }
         return parent::getIcon();
     }
 
-    public function getDescription() {
+    public function getDescription()
+    {
         if (isset($this->profile)) {
             return $this->profile['description'];
         }
         return parent::getDescription();
     }
 
-    private function parseExternal($external, $did) {
+    private function parseExternal($external, $did)
+    {
         $description = '';
         $externalUri = $external['uri'];
         $externalTitle = htmlspecialchars($external['title'], ENT_QUOTES, 'UTF-8');
@@ -91,14 +97,16 @@ class BlueskyBridge extends BridgeAbstract {
         return $description;
     }
 
-    private function textToDescription($text) {
+    private function textToDescription($text)
+    {
         $text = nl2br(htmlspecialchars($text, ENT_QUOTES, 'UTF-8'));
         $text = preg_replace('/(https?:\/\/[^\s]+)/i', '<a href="$1">$1</a>', $text);
 
         return $text;
     }
 
-    public function collectData() {
+    public function collectData()
+    {
         $handle = $this->getInput('handle');
         $filter = $this->getInput('filter') ?: 'posts_and_author_threads';
 
@@ -136,7 +144,7 @@ class BlueskyBridge extends BridgeAbstract {
                 if ($thumbnail) {
                     $description .= "<p><video controls poster=\"$thumbnail\">";
                     $description .= "<source src=\"$playlist\" type=\"application/x-mpegURL\">";
-                    $description .= "Video source not supported</video></p>";
+                    $description .= 'Video source not supported</video></p>';
                 }
             }
 
@@ -163,7 +171,7 @@ class BlueskyBridge extends BridgeAbstract {
                 }
 
                 if ($quotedText) {
-                    $description .= "<hr /><strong>Quote from " . htmlspecialchars($quotedDisplayName) . " (@ " . htmlspecialchars($quotedAuthor) . "):</strong><br />";
+                    $description .= '<hr /><strong>Quote from ' . htmlspecialchars($quotedDisplayName) . ' (@ ' . htmlspecialchars($quotedAuthor) . '):</strong><br />';
                     $description .= $this->textToDescription($quotedText);
                     if (isset($quotedPostUri)) {
                         $description .= "<p><a href=\"$quotedPostUri\">View original quote post</a></p>";
@@ -171,47 +179,52 @@ class BlueskyBridge extends BridgeAbstract {
                 }
             }
 
-                if (isset($post['post']['embed']['record']['value']['embed']['images'])) {
-                    $quotedImages = $post['post']['embed']['record']['value']['embed']['images'];
-                    foreach ($quotedImages as $image) {
-                        $linkRef = $image['image']['ref']['$link'] ?? null;
-                        if ($linkRef) {
-                            $quotedAuthorDid = $post['post']['embed']['record']['author']['did'] ?? null;
-                            $thumbnailUrl = $this->resolveThumbnailUrl($quotedAuthorDid, $linkRef);
-                            $fullsizeUrl = $this->resolveFullsizeUrl($quotedAuthorDid, $linkRef);
-                            $description .= "<br /><br /><a href=\"$fullsizeUrl\"><img src=\"$thumbnailUrl\" alt=\"Quoted Image\"></a>";
-                        }
+            if (isset($post['post']['embed']['record']['value']['embed']['images'])) {
+                $quotedImages = $post['post']['embed']['record']['value']['embed']['images'];
+                foreach ($quotedImages as $image) {
+                    $linkRef = $image['image']['ref']['$link'] ?? null;
+                    if ($linkRef) {
+                        $quotedAuthorDid = $post['post']['embed']['record']['author']['did'] ?? null;
+                        $thumbnailUrl = $this->resolveThumbnailUrl($quotedAuthorDid, $linkRef);
+                        $fullsizeUrl = $this->resolveFullsizeUrl($quotedAuthorDid, $linkRef);
+                        $description .= "<br /><br /><a href=\"$fullsizeUrl\"><img src=\"$thumbnailUrl\" alt=\"Quoted Image\"></a>";
                     }
                 }
+            }
 
             $item['content'] = $description;
             $this->items[] = $item;
         }
     }
 
-    private function resolveHandle($handle) {
+    private function resolveHandle($handle)
+    {
         $uri = 'https://public.api.bsky.app/xrpc/com.atproto.identity.resolveHandle?handle=' . urlencode($handle);
         $response = json_decode(getContents($uri), true);
         return $response['did'];
     }
 
-    private function getProfile($did) {
+    private function getProfile($did)
+    {
         $uri = 'https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=' . urlencode($did);
         $response = json_decode(getContents($uri), true);
         return $response;
     }
 
-    private function getAuthorFeed($did, $filter) {
+    private function getAuthorFeed($did, $filter)
+    {
         $uri = 'https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed?actor=' . urlencode($did) . '&filter=' . urlencode($filter) . '&limit=30';
         $response = json_decode(getContents($uri), true);
         return $response;
     }
 
-    private function resolveThumbnailUrl($authorDid, $linkRef) {
+    private function resolveThumbnailUrl($authorDid, $linkRef)
+    {
         return 'https://cdn.bsky.app/img/feed_thumbnail/plain/' . $authorDid . '/' . $linkRef . '@jpeg';
     }
 
-    private function resolveFullsizeUrl($authorDid, $linkRef) {
+    private function resolveFullsizeUrl($authorDid, $linkRef)
+    {
         return 'https://cdn.bsky.app/img/feed_fullsize/plain/' . $authorDid . '/' . $linkRef . '@jpeg';
     }
 }
