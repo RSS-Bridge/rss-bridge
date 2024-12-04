97458db8c3ee4187e47b46132044db1958a34e45
okbaydere <okbaydere@gmail.com>
2024-12-04 20:40:08 +0300

    Updated StorytelBridge to include URL validation and cleaned up code

--- bridges/StorytelBridge.php
+++ bridges/StorytelBridge.php
@@ -1,58 +1,58 @@
-<?php 
-class StorytelBridge extends BridgeAbstract { 
-    const NAME = 'Storytel List Bridge'; 
-    const URI = 'https://www.storytel.com/tr'; 
-    const DESCRIPTION = 'Fetches books from a Storytel list, including title, author, and cover image.'; 
-    const MAINTAINER = 'Okbaydere'; 
-    const PARAMETERS = [ 
-        'List' => [ 
-            'url' => [ 
-                'name' => 'Storytel List URL', 
-                'required' => true, 
-                'exampleValue' => 'https://www.storytel.com/tr/lists/23d09e0bd8fe4d998d1832ddbfa18166', 
-            ], 
-        ], 
-    ]; 
+<?php
 
-    public function collectData() { 
-        $url = $this->getInput('url'); 
-        $html = getSimpleHTMLDOM($url); 
+class StorytelBridge extends BridgeAbstract
+{
+    const NAME = 'Storytel List Bridge';
+    const URI = 'https://www.storytel.com/tr';
+    const DESCRIPTION = 'Fetches books from a Storytel list, including title, author, and cover image.';
+    const MAINTAINER = 'Okbaydere';
+    const PARAMETERS = [
+        'List' => [
+            'url' => [
+                'name' => 'Storytel List URL',
+                'required' => true,
+                'exampleValue' => 'https://www.storytel.com/tr/lists/23d09e0bd8fe4d998d1832ddbfa18166',
+            ],
+        ],
+    ];
 
-        if (!$html) { 
-            returnServerError('Unable to fetch Storytel list'); 
-        } 
+    public function collectData()
+    {
+        $url = $this->getInput('url');
 
-        foreach ($html->find('li.sc-4615116a-1') as $element) { 
-            $item = []; 
+        if (!preg_match('/^https:\/\/www\.storytel\.com/', $url)) {
+            returnServerError('Invalid URL: Only Storytel URLs are allowed.');
+        }
 
-            // Fetch the title 
-            $titleElement = $element->find('span.sc-b1963858-0.hoTsmF', 0); 
-            $item['title'] = $titleElement ? $titleElement->plaintext : 'No title'; 
+        $html = getSimpleHTMLDOM($url);
+        if (!$html) {
+            returnServerError('Unable to fetch Storytel list');
+        }
 
-            // Fetch the author 
-            $authorElement = $element->find('span.sc-b1963858-0.ghYMwH', 0); 
-            $item['author'] = $authorElement ? $authorElement->plaintext : 'Unknown author'; 
+        foreach ($html->find('li.sc-4615116a-1') as $element) {
+            $item = [];
 
-            // Fetch the cover image 
-            $imgElement = $element->find('img.sc-da400893-5', 0); 
-            $coverUrl = $imgElement ? $imgElement->getAttribute('srcset') : ''; 
-            if ($coverUrl) { 
-                // Select the highest resolution cover image 
-                $coverUrls = explode(', ', $coverUrl); 
-                $bestCoverUrl = trim(end($coverUrls)); 
-                $item['content'] = '<img src="' . preg_replace('/\?.*/', '', $bestCoverUrl) . '"/>'; 
-            } 
+            $titleElement = $element->find('span.sc-b1963858-0.hoTsmF', 0);
+            $item['title'] = $titleElement ? $titleElement->plaintext : 'No title';
 
-            // Fetch the link (details page) 
-            $linkElement = $element->find('a', 0); 
-            $item['uri'] = $linkElement ? 'https://www.storytel.com' . $linkElement->getAttribute('href') : $url; 
+            $authorElement = $element->find('span.sc-b1963858-0.ghYMwH', 0);
+            $item['author'] = $authorElement ? $authorElement->plaintext : 'Unknown author';
 
-            // Combine content 
-            $item['content'] .= '<p>Author: ' . $item['author'] . '</p>'; 
-            $item['content'] .= '<p><a href="' . $item['uri'] . '">More details</a></p>'; 
+            $imgElement = $element->find('img.sc-da400893-5', 0);
+            $coverUrl = $imgElement ? $imgElement->getAttribute('srcset') : '';
+            if ($coverUrl) {
+                $coverUrls = explode(', ', $coverUrl);
+                $bestCoverUrl = trim(end($coverUrls));
+                $item['content'] = '<img src="' . preg_replace('/\?.*/', '', $bestCoverUrl) . '"/>';
+            }
 
-            $this->items[] = $item; 
-        } 
-    } 
-} 
-?>
+            $linkElement = $element->find('a', 0);
+            $item['uri'] = $linkElement ? 'https://www.storytel.com' . $linkElement->getAttribute('href') : $url;
+
+            $item['content'] .= '<p>Author: ' . $item['author'] . '</p>';
+            $item['content'] .= '<p><a href="' . $item['uri'] . '">More details</a></p>';
+
+            $this->items[] = $item;
+        }
+    }
+}
