359f6c41575185ab3903461d3b8360352964fb27
ORelio <ORelio@users.noreply.github.com>
2025-03-20 15:28:24 +0100

    FeedExpander: Code linting

--- lib/FeedExpander.php
+++ lib/FeedExpander.php
@@ -71,15 +71,12 @@ abstract class FeedExpander extends BridgeAbstract
         // Remove extra content at the end of the document, if any (#4485)
         // First retrieve tag name of root node, which is the first node following <?xml prolog,
         // Then find the last matching </tag> in xml string and strip anything beyond that.
-        if (preg_match('/(?:<\?xml[^>]*\?>[^<]*<)([^ "\'>]+)/i', $xmlString, $matches) === 1)
-        {
+        if (preg_match('/(?:<\?xml[^>]*\?>[^<]*<)([^ "\'>]+)/i', $xmlString, $matches) === 1) {
             $root_node_tag = $matches[1];
             $last_closing_occurrence = strripos($xmlString, '</' . $root_node_tag);
-            if ($last_closing_occurrence !== false)
-            {
+            if ($last_closing_occurrence !== false) {
                 $closing_node_end = strpos($xmlString, '>', $last_closing_occurrence);
-                if ($closing_node_end !== false)
-                {
+                if ($closing_node_end !== false) {
                     $xmlString = substr($xmlString, 0, $closing_node_end + 1);
                 }
             }
