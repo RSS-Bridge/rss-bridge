7c1e01b45a86a694ed38eb25068f8e60e825c84c
tillcash <tillcash@users.noreply.github.com>
2025-03-27 00:16:02 +0530

    [MinecraftBridge] Add Bridge (#4497)

---
+++ bridges/MinecraftBridge.php
@@ -0,0 +1,31 @@
+<?php
+
+class MinecraftBridge extends BridgeAbstract
+{
+    const NAME = 'Minecraft';
+    const URI = 'https://www.minecraft.net';
+    const DESCRIPTION = 'Catch up on the latest Minecraft articles';
+    const MAINTAINER = 'tillcash';
+
+    public function collectData()
+    {
+        $json = getContents(
+            'https://www.minecraft.net/content/minecraftnet/language-masters/en-us/_jcr_content.articles.page-1.json'
+        );
+
+        $articles = json_decode($json);
+
+        if ($articles === null) {
+            returnServerError('Failed to decode JSON content.');
+        }
+
+        foreach ($articles->article_grid as $article) {
+            $this->items[] = [
+                'title' => $article->default_tile->title,
+                'uid' => $article->article_url,
+                'uri' => self::URI . $article->article_url,
+                'content' => $article->default_tile->sub_header,
+            ];
+        }
+    }
+}
