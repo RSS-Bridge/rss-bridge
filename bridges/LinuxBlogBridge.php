5e8f5691b2812d27dca9eee0561224185fa090fb
tillcash <tillcash@users.noreply.github.com>
2025-04-23 14:26:20 +0530

    [LinuxBlogBridge] add bridge

---
+++ bridges/LinuxBlogBridge.php
@@ -0,0 +1,64 @@
+<?php
+
+class LinuxBlogBridge extends BridgeAbstract
+{
+    const NAME = 'LinuxBlog.in';
+    const URI = 'https://linuxblog.io';
+    const DESCRIPTION = 'Retrieve recent articles';
+    const MAINTAINER = 'tillcash';
+    const CACHE_TIMEOUT = 60 * 60 * 6; // 6 hours
+    const MAX_ARTICLES = 10;
+
+    public function collectData()
+    {
+        $dom = getSimpleHTMLDOM(self::URI);
+        $count = 0;
+        $articles = $dom->find('ul.display-posts-listing li.listing-item');
+
+        if (!$articles) {
+            returnServerError('Failed to retrieve articles');
+        }
+
+        foreach ($articles as $article) {
+            if ($count >= self::MAX_ARTICLES) {
+                break;
+            }
+
+            $element = $article->find('a.title', 0);
+
+            if (!$element || empty($element->plaintext) || empty($element->href)) {
+                continue;
+            }
+
+            $url = $element->href;
+            $date = $article->find('span.date', 0);
+            $timestamp = null;
+
+            if ($date && $date->plaintext) {
+                $timestamp = strtotime($date->plaintext . ' 00:00:00 GMT');
+            }
+
+            $this->items[] = [
+                'content'    => $this->constructContent($url),
+                'timestamp'  => $timestamp,
+                'title'      => trim($element->plaintext),
+                'uid'        => $url,
+                'uri'        => $url,
+            ];
+
+            $count++;
+        }
+    }
+
+    private function constructContent($url)
+    {
+        $dom = getSimpleHTMLDOMCached($url);
+        $article = $dom->find('section.entry.fix', 0);
+
+        if (!$article) {
+            return 'Content Not Found';
+        }
+
+        return $article;
+    }
+}
