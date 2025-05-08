58e30f8b4b8f5d449fe5d48ff11446511e78fe54
Dag <me@dvikan.no>
2025-05-08 23:33:18 +0200

    fix(furaffinity): date and tags, #4513 (#4543)

--- bridges/FurAffinityBridge.php
+++ bridges/FurAffinityBridge.php
@@ -936,8 +936,7 @@ class FurAffinityBridge extends BridgeAbstract
             if ($this->getInput('full') === true) {
                 $submissionHTML = $this->getFASimpleHTMLDOM($submissionURL, $cache);
                 if (!$this->isHiddenSubmission($submissionHTML)) {
-                    $stats = $submissionHTML->find('.stats-container', 0);
-                    $popupDate = $stats->find('.popup_date', 0);
+                    $popupDate = $submissionHTML->find('section .popup_date', 0);
                     if ($popupDate) {
                         $item['timestamp'] = strtotime($popupDate->title);
                     }
@@ -947,9 +946,10 @@ class FurAffinityBridge extends BridgeAbstract
                         $item['enclosures'] = [$var->href];
                     }
 
-                    foreach ($stats->find('#keywords a') as $keyword) {
+                    foreach ($submissionHTML->find('.tags-row .tags a') as $keyword) {
                         $item['categories'][] = $keyword->plaintext;
                     }
+                    $item['categories'] = array_filter($item['categories']);
 
                     $previewSrc = $submissionHTML->find('#submissionImg', 0);
                     if ($previewSrc) {
