99c0fe03e0fbea045373adf88931c9b79aee45f4
shaun <shaunewilliams@gmail.com>
2025-07-13 20:48:19 -0500

    add feedName fallbacks (thanks @Mar-Koeh)

--- bridges/YouTubePostsTabBridge.php
+++ bridges/YouTubePostsTabBridge.php
@@ -72,7 +72,11 @@ class YouTubePostsTabBridge extends BridgeAbstract
 
         $json = $this->extractJson($html->find('html', 0)->innertext);
 
-        $this->feedName = $json->header->c4TabbedHeaderRenderer->title;
+        $this->feedName = $json->header->c4TabbedHeaderRenderer->title
+            ?? $json->header->pageHeaderRenderer->pageTitle
+            ?? $json->metadata->channelMetadataRenderer->title
+            ?? $json->microformat->microformatDataRenderer->title
+            ?? '';
 
         if ($this->hasPostsTab($json) === false) {
             returnServerError('Channel does not have a posts tab');
