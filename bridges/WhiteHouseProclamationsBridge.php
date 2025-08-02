a207d963b5110e2d5f6b8452e847bc642b042721
sij-ai <67624670+sij-ai@users.noreply.github.com>
2025-08-01 18:27:04 -0700

    Create WhiteHouseProclamationsBridge.php
    Returns Proclamations from The White House

---
+++ bridges/WhiteHouseProclamationsBridge.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'WhiteHouseBridge.php';
+
+class WhiteHouseProclamationsBridge extends WhiteHouseBridge
+{
+    const MAINTAINER = 'sij-ai';
+    const NAME = 'White House Proclamations';
+    const URI = 'https://www.whitehouse.gov/presidential-actions/proclamations/';
+    const DESCRIPTION = 'Returns Proclamations from The White House.';
+    const CACHE_TIMEOUT = 7200; // 2 hours
+}
