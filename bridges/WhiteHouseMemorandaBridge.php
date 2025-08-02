9018312e0fc3a7448ce3ecd865bd23d2039fb353
sij-ai <67624670+sij-ai@users.noreply.github.com>
2025-08-01 18:26:03 -0700

    Create WhiteHouseMemorandaBridge.php
    Returns Presidential Memoranda from The White House

---
+++ bridges/WhiteHouseMemorandaBridge.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'WhiteHouseBridge.php';
+
+class WhiteHouseMemorandaBridge extends WhiteHouseBridge
+{
+    const MAINTAINER = 'sij-ai';
+    const NAME = 'White House Presidential Memoranda';
+    const URI = 'https://www.whitehouse.gov/presidential-actions/presidential-memoranda/';
+    const DESCRIPTION = 'Returns Presidential Memoranda from The White House.';
+    const CACHE_TIMEOUT = 7200; // 2 hours
+}
