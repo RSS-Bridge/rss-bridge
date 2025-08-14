9adf42115163f424fa7c28a0cd8f6b949c917a60
Matt DeMoss <demoss.matt@gmail.com>
2025-08-14 12:39:35 -0400

    apply phpcs.xml rules

--- bridges/PcGamerBridge.php
+++ bridges/PcGamerBridge.php
@@ -27,7 +27,7 @@ class PcGamerBridge extends BridgeAbstract
             // Relying on meta tags ought to be more reliable.
             $item['title'] = $articleHtml->find('meta[property=og:title]', 0)->content;
             $item['content'] = html_entity_decode($articleHtml->find('meta[name=description]', 0)->content);
-            
+
             // TODO: parsely-author is no longer available, but it is in the application/ld+json
             $item['author'] = $articleHtml->find('a[rel=author]', 0)->innertext;
 
@@ -36,25 +36,30 @@ class PcGamerBridge extends BridgeAbstract
                 $item['enclosures'][] = $imageUrl->content;
             }
 
-            /* 
+            /*
             Tags in mrf:tags are semicolon-delimited and each begins with a label and a ':'
             Example:
                 "region:US;articleType:News;channel:Gaming software;"
             Find the tag, replace ; with \n, remove the label prefixes, then explode by newline.
             */
             $item['categories'] = array_unique(
-                explode(PHP_EOL,
-                    preg_replace('/^[^:]+:/m', '', 
-                        preg_replace('/;/', PHP_EOL, 
+                explode(
+                    PHP_EOL,
+                    preg_replace(
+                        '/^[^:]+:/m',
+                        '',
+                        preg_replace(
+                            '/;/',
+                            PHP_EOL,
                             $articleHtml->find('meta[property=mrf:tags]', 0)->content
                         )
                     )
                 )
             );
-            
+
             $item['timestamp'] = strtotime($articleHtml->find('meta[name=pub_date]', 0)->content);
-            
+
             $this->items[] = $item;
-		}
+        }
     }
 }
