RSS-Bridge/rss-bridge

commit d00745c41113894e54f3ff71d73006410e3e9e20

[GolemBridge] Add and remove more stuff
Fix the inclusion of tables and add ul/ol elements.
This includes new unwanted stuff that is explicitly removed.

bridges/GolemBridge.php
@@ -139,8 +139,9 @@ class GolemBridge extends FeedExpander
 
         // delete known bad elements and unwanted gallery images
         foreach (
-            $article->find('div[id*="adtile"], #job-market, #seminars, iframe, .go-article-header__title, .go-article-header__kicker,
-                        .gbox_affiliate, div.toc, .go-button-bar, .go-alink-list, .go-teaser-block, .go-vh, .go-gallery .[data-active="false"]') as $bad
+            $article->find('div[id*="adtile"], #job-market, #seminars, iframe, .go-article-header__title, .go-article-header__kicker, .go-label--sponsored,
+                        .gbox_affiliate, div.toc, .go-button-bar, .go-alink-list, .go-teaser-block, .go-vh, .go-paywall, .go-index-link, .go-pagination__list,
+                        .go-gallery .[data-active="false"]') as $bad
         ) {
             $bad->remove();
         }
@@ -165,8 +166,8 @@ class GolemBridge extends FeedExpander
         }
 
         foreach (
-            $article->find('div.go-article-header__intro, p, h1, h2, h3, pre, .go-media img[src*="."], .go-media figcaption,
-                    div[class*="golem_tablediv"], iframe, video') as $element
+            $article->find('div.go-article-header__intro, p, h1, h2, h3, pre, ul, ol, .go-media img[src*="."], .go-media figcaption,
+                    table, iframe, video') as $element
         ) {
             if (!str_contains($prevcontent, $element)) {
                 $item .= $element;

commit 2904ebd7145e61ba72f160a512fa0f9c3cc9b149

[GolemBridge] Actually fix pagination
Commit 3f33d0e31206c4313278438dbe5454bbf0db43fc modified the logic to
accommodate for the website change.
The commit d00745c41113894e54f3ff71d73006410e3e9e20 broke
pagination again by removing the pagination elements from the article
content.
This commit finally fixes pagination again by doing all pre-processing
before calling `extractContent()`.

Tested with the following articles:
https://www.golem.de/news/supercomputer-jupiter-eingeweiht-europaeisch-ist-hochleistungsrechnen-richtig-gedacht-2509-199789.html
https://www.golem.de/news/ankers-soundcore-sleep-a30-im-praxistest-diese-anc-stoepsel-verbessern-meinen-schlaf-2509-199735.html

bridges/GolemBridge.php
@@ -94,15 +94,16 @@ class GolemBridge extends FeedExpander
                 }
             }
 
-            $item['content'] .= $this->extractContent($articlePage, $item['content']);
-
             // next page
-            $nextUri = $articlePage->find('li.go-pagination__item--next>a', 0);
+            $nextUri = $articlePage->find('li.go-pagination__item--next a', 0);
             if ($nextUri) {
                 $uri = $nextUri->href;
             } else {
                 $uri = null;
             }
+
+            // Only extract the content (and remove content) after all pre-processing is done
+            $item['content'] .= $this->extractContent($articlePage, $item['content']);
         }
 
         return $item;