d45e12e68ebe13f2c4018f63ec66dc236952b2ad
Bocki <henning@bocklage.com>
2025-10-29 00:46:48 +0000

    Should fix it

--- bridges/TeefuryBridge.php
+++ bridges/TeefuryBridge.php
@@ -19,20 +19,19 @@ class TeefuryBridge extends BridgeAbstract
             $titletext = $element->find('p', 0)->innertext;
             $title = trim(explode('<br>', $titletext)[0]);
             $today = date('m/d/Y');
-            $shirtinfo = $element->find('div[data-link*="odad-tee-mens"]', 0);
+            $shirtinfo = $element->find('div[id*="img-color-art"]', 0);
             $uri = self::URI . $shirtinfo->attr['data-link'];
             $item = [];
             $item['uri'] = $uri;
             $item['title'] = $title;
             $item['uid'] = $title;
             $item['timestamp'] = $today;
-/*            $item['content'] = $element->find('p', 0)
+            $item['content'] = $element->find('p', 0)
             . '<br><a href="'
             . $uri
             . '"><img src="'
             . $shirtinfo->find('img', 0)->attr['src']
             . '" /></a>';
-*/
             $this->items[] = $item;
         }
     }
