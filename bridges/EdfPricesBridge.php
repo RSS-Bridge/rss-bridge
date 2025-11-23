f1f9d3180094c878574a8794175c693cefae0e3d
Florent VIOLLEAU <floviolleau@laposte.net>
2025-11-23 01:01:48 +0100

    [EdfPricesBridge] Lint

--- bridges/EdfPricesBridge.php
+++ bridges/EdfPricesBridge.php
@@ -42,17 +42,17 @@ class EdfPricesBridge extends BridgeAbstract
     private function removeEmojisAndSpecialSpaces(string $text): string
     {
         // This regex covers most common emoji ranges in Unicode
-        $regex = '/[\x{1F600}-\x{1F64F}' .  // Emoticons
-                 '\x{1F300}-\x{1F5FF}' .  // Misc Symbols and Pictographs
-                 '\x{1F680}-\x{1F6FF}' .  // Transport and Map
-                 '\x{1F700}-\x{1F77F}' .  // Alchemical Symbols
-                 '\x{1F780}-\x{1F7FF}' .  // Geometric Shapes Extended
-                 '\x{1F800}-\x{1F8FF}' .  // Supplemental Arrows-C
-                 '\x{1F900}-\x{1F9FF}' .  // Supplemental Symbols and Pictographs
-                 '\x{1FA00}-\x{1FA6F}' .  // Chess Symbols, Symbols and Pictographs Extended-A
-                 '\x{1FA70}-\x{1FAFF}' .  // Symbols and Pictographs Extended-B
-                 '\x{2600}-\x{26FF}' .    // Misc symbols
-                 '\x{2700}-\x{27BF}' .    // Dingbats
+        $regex = '/[\x{1F600}-\x{1F64F}' . // Emoticons
+                 '\x{1F300}-\x{1F5FF}' . // Misc Symbols and Pictographs
+                 '\x{1F680}-\x{1F6FF}' . // Transport and Map
+                 '\x{1F700}-\x{1F77F}' . // Alchemical Symbols
+                 '\x{1F780}-\x{1F7FF}' . // Geometric Shapes Extended
+                 '\x{1F800}-\x{1F8FF}' . // Supplemental Arrows-C
+                 '\x{1F900}-\x{1F9FF}' . // Supplemental Symbols and Pictographs
+                 '\x{1FA00}-\x{1FA6F}' . // Chess Symbols, Symbols and Pictographs Extended-A
+                 '\x{1FA70}-\x{1FAFF}' . // Symbols and Pictographs Extended-B
+                 '\x{2600}-\x{26FF}' . // Misc symbols
+                 '\x{2700}-\x{27BF}' . // Dingbats
                  ']+/u';
 
         return preg_replace($regex, '', str_replace('&nbsp;', '', $text));
@@ -75,7 +75,13 @@ class EdfPricesBridge extends BridgeAbstract
                 $item = [];
 
                 $matches = [];
-                preg_match_all('/Jour (.*) :.*?Heures (.*) : (.*).*?€.*?Heures (.*) : (.*).*?€/um', $this->removeEmojisAndSpecialSpaces($elementDom->plaintext), $matches, PREG_SET_ORDER, 0);
+                preg_match_all(
+                    '/Jour (.*) :.*?Heures (.*) : (.*).*?€.*?Heures (.*) : (.*).*?€/um',
+                    $this->removeEmojisAndSpecialSpaces($elementDom->plaintext),
+                    $matches,
+                    PREG_SET_ORDER,
+                    0
+                );
 
                 // for tempo contract we have 2x3 colors
                 if ($matches && count($matches[0]) === 6) {
