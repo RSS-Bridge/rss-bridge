c30ea98be6af60f8f2486a037b5adb62024281df
LordArrin <79581469+LordArrin@users.noreply.github.com>
2025-12-27 00:40:17 +0300

    Update Rule34Bridge.php
    Write with DeepSeek, NEED TESTING

--- bridges/Rule34Bridge.php
+++ bridges/Rule34Bridge.php
@@ -1,9 +1,110 @@
 <?php
 
-class Rule34Bridge extends GelbooruBridge
+class Rule34Bridge extends BridgeAbstract
 {
     const MAINTAINER = 'mitsukarenai';
     const NAME = 'Rule34';
-    const URI = 'https://rule34.xxx/';
+    const URI = 'https://api.rule34.xxx/';
     const DESCRIPTION = 'Returns images from given page';
+
+    const PARAMETERS = [
+        'global' => [
+            'api_key' => [
+                'name' => 'API Key',
+                'type' => 'text',
+                'required' => true,
+                'title' => 'Your Rule34 API key'
+            ],
+            'user_id' => [
+                'name' => 'User ID',
+                'type' => 'number',
+                'required' => true,
+                'title' => 'Your Rule34 user ID'
+            ],
+            'p' => [
+                'name' => 'page',
+                'defaultValue' => 0,
+                'type' => 'number'
+            ],
+            't' => [
+                'name' => 'tags',
+                'exampleValue' => 'solo',
+                'title' => 'Tags to search for'
+            ],
+            'l' => [
+                'name' => 'limit',
+                'exampleValue' => 100,
+                'title' => 'How many posts to retrieve (hard limit of 1000)'
+            ]
+        ],
+        0 => []
+    ];
+
+    protected function getFullURI()
+    {
+        return $this->getURI()
+        . 'index.php?&page=dapi&s=post&q=index&json=1'
+        . '&api_key=' . urlencode($this->getInput('api_key'))
+        . '&user_id=' . urlencode($this->getInput('user_id'))
+        . '&pid=' . $this->getInput('p')
+        . '&limit=' . $this->getInput('l')
+        . '&tags=' . urlencode($this->getInput('t') ?? '');
+    }
+
+    /*
+    This function is superfluous for GelbooruBridge, but useful
+    for Bridges that inherit from it
+    */
+    protected function buildThumbnailURI($element)
+    {
+        return $this->getURI() . 'thumbnails/' . $element->directory
+        . '/thumbnail_' . $element->md5 . '.jpg';
+    }
+
+    protected function getItemFromElement($element)
+    {
+        $item = [];
+        $item['uri'] = $this->getURI() . 'index.php?page=post&s=view&id='
+        . $element->id;
+        $item['postid'] = $element->id;
+        $item['author'] = $element->owner;
+        $item['timestamp'] = date('d F Y H:i:s', $element->change);
+        $item['tags'] = $element->tags;
+        $item['title'] = $this->getName() . ' | ' . $item['postid'];
+
+        if (isset($element->preview_url)) {
+            $thumbnailUri = $element->preview_url;
+        } else {
+            $thumbnailUri = $this->buildThumbnailURI($element);
+        }
+
+        $item['content'] = '<a href="' . $item['uri'] . '"><img src="'
+        . $thumbnailUri . '" /></a><br><br><b>Dimensions:</b> '
+        . strval($element->width) . ' x ' . strval($element->height) . '<br><br><b>Tags:</b> '
+        . $item['tags'];
+        if (isset($element->source)) {
+            $item['content'] .= '<br><br><b>Source: </b><a href="' . $element->source . '">' . $element->source . '</a>';
+        }
+
+        return $item;
+    }
+
+    public function collectData()
+    {
+        $url = $this->getFullURI();
+        $content = getContents($url);
+
+        if ($content === '') {
+            return;
+        }
+
+        $posts = Json::decode($content, false);
+        if (isset($posts->post)) {
+            $posts = $posts->post;
+        }
+
+        foreach ($posts as $post) {
+            $this->items[] = $this->getItemFromElement($post);
+        }
+    }
 }
