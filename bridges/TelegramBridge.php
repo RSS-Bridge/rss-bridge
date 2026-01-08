a8c90a7e1bbab882452e5ced983b591d33482b40
Alberto Pasqualetto <39854348+albertopasqualetto@users.noreply.github.com>
2026-01-08 13:23:10 +0100

    [TelegramBridge] Get channel icon (#4843)
    * [TelegramBridge] Get channel icon
    
    * [TelegramBridge] name,icon prevent duplicate fetch
    
    Co-authored-by: Mynacol <git@mynacol.xyz>
    
    ---------
    
    Co-authored-by: Mynacol <git@mynacol.xyz>

--- bridges/TelegramBridge.php
+++ bridges/TelegramBridge.php
@@ -36,6 +36,7 @@ class TelegramBridge extends BridgeAbstract
 
     const CACHE_TIMEOUT = 60 * 60; // 1h
     private $feedName = '';
+    private $feedIcon = '';
 
     private $enclosures = [];
     private $itemTitle = '';
@@ -55,9 +56,18 @@ class TelegramBridge extends BridgeAbstract
 
             $dom = getSimpleHTMLDOM($url);
 
-            $channelTitle = $dom->find('div.tgme_channel_info_header_title span', 0)->plaintext ?? '';
-            $channelTitle = htmlspecialchars_decode($channelTitle, ENT_QUOTES);
-            $this->feedName = $channelTitle . ' (@' . $this->normalizeUsername() . ')';
+            if (!$this->feedName) {
+                $channelTitle = $dom->find('div.tgme_channel_info_header_title span', 0)->plaintext ?? '';
+                $channelTitle = htmlspecialchars_decode($channelTitle, ENT_QUOTES);
+                $this->feedName = $channelTitle . ' (@' . $this->normalizeUsername() . ')';
+            }
+
+            if (!$this->feedIcon) {
+                $photoImg = $dom->find('i.tgme_page_photo_image img', 0);
+                if ($photoImg) {
+                    $this->feedIcon = $photoImg->src;
+                }
+            }
 
             $messages = $dom->find('div.tgme_widget_message_wrap.js-widget_message_wrap');
             if (!$channelTitle && !$messages) {
@@ -173,6 +183,14 @@ class TelegramBridge extends BridgeAbstract
             return $this->feedName . ' - Telegram';
         }
         return parent::getName();
+    }
+
+    public function getIcon()
+    {
+        if ($this->feedIcon) {
+            return $this->feedIcon;
+        }
+        return parent::getIcon();
     }
 
     private function processReply($messageDiv)
