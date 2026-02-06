a6cd0b6eed45e21c5088708d63b4fd032c9d54a2
Jonatan Czarniecki <czarniecki.jonatan@gmail.com>
2026-02-07 00:31:25 +0100

    fix: fix parameter validation for arrays
    * correctly search for invalid values and return error as soon as one is found
    
    * update the error message as well

--- middlewares/SecurityMiddleware.php
+++ middlewares/SecurityMiddleware.php
@@ -10,12 +10,18 @@ class SecurityMiddleware implements Middleware
     public function __invoke(Request $request, $next): Response
     {
         foreach ($request->toArray() as $key => $value) {
+            if (is_string($value)) continue;
             //TODO: Maybe stricter checking for arrays?
             // Not required technically because the values are properly checked
             // in ParameterValidator, so basic check like this should be OK.
-            if (is_string($value) || (is_array($value) && !in_array(fn($v) => !is_string($v), $value))) continue;
+            if (is_array($value)) {
+                $flag = true;
+                foreach ($value as $v)
+                    if (!($flag = is_string($v))) break;
+                if ($flag) continue;
+            }
             return new Response(render(__DIR__ . '/../templates/error.html.php', [
-                'message' => "Query parameter \"$key\" is not a string.",
+                'message' => "Query parameter \"$key\" is not a string or array of strings.",
             ]), 400);
         }
         return $next($request);
