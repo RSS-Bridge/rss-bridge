6c989189cc8a91a7f94c1775d07a60e2a751cb47
SqrtMinusOne <thexcloud@gmail.com>
2026-02-07 16:45:23 +0300

    [NautilusBridge] Fix issues

--- bridges/NautilusBridge.php
+++ bridges/NautilusBridge.php
@@ -1,9 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 class NautilusBridge extends FeedExpander
 {
     const MAINTAINER = 'sqrtminusone';
-    const NAME = 'Nautilus Bridge';
+    const NAME = 'Nautilus';
     const URI = 'https://nautil.us/';
     const DESCRIPTION = 'Returns the latest articles from Nautilus.';
 
@@ -16,12 +18,74 @@ class NautilusBridge extends FeedExpander
                 'defaultValue' => 10,
                 'title' => 'Maximum number of returned feed items. Default 10'
             ],
+            'topic' => [
+                'name' => 'Topics and Channels',
+                'required' => false,
+                'title' => 'Select a topic',
+                'type' => 'list',
+                'defaultValue' => '',
+                'values' => [
+                    'All items' => '',
+                    'Topic: Anthropology' => 'topics/anthropology',
+                    'Topic: Arts' => 'topics/arts',
+                    'Topic: Astronomy' => 'topics/astronomy',
+                    'Topic: Communication' => 'topics/communication',
+                    'Topic: Economics' => 'topics/economics',
+                    'Topic: Environment' => 'topics/environment',
+                    'Topic: Evolution' => 'topics/evolution',
+                    'Topic: General' => 'topics/general',
+                    'Topic: Genetics' => 'topics/genetics',
+                    'Topic: Geoscience' => 'topics/geoscience',
+                    'Topic: Health' => 'topics/health',
+                    'Topic: History' => 'topics/history',
+                    'Topic: Math' => 'topics/math',
+                    'Topic: Microbiology' => 'topics/microbiology',
+                    'Topic: Neuroscience' => 'topics/neuroscience',
+                    'Topic: Paleontology' => 'topics/paleontology',
+                    'Topic: Philosophy' => 'topics/philosophy',
+                    'Topic: Physics' => 'topics/physics',
+                    'Topic: Psychology' => 'topics/psychology',
+                    'Topic: Sociology' => 'topics/sociology',
+                    'Topic: Technology' => 'topics/technology',
+                    'Topic: Zoology' => 'topics/zoology',
+                    'Channel: Art + Science' => 'channel/art-science',
+                    'Channel: Biology + Beyond' => 'channel/bilology-beyond',
+                    'Channel: Catalysts of Discovery' => 'channel/catalysts',
+                    'Channel: Cosmos' => 'channel/cosmos',
+                    'Channel: Culture' => 'channel/culture',
+                    'Channel: Currents' => 'channel/currents',
+                    'Channel: Earth' => 'channel/earth',
+                    'Channel: Life' => 'channel/life',
+                    'Channel: Mind' => 'channel/mind',
+                    'Channel: Ocean' => 'channel/ocean',
+                    'Channel: One Question' => 'channel/one-question',
+                    'Channel: Quanta Abstractions' => 'channel/abstractions',
+                    'Channel: Rewilding' => 'channel/rewilding',
+                    'Channel: Science at the Ballot Box' => 'channel/ballotbox-science',
+                    'Channel: Science Philanthropy Alliance' => 'channel/alliance',
+                    'Channel: Spart of Science' => 'channel/spark',
+                    'Channel: The Animal Issue' => 'channel/animal',
+                    'Channel: The Climates Issue' => 'channel/climates',
+                    'Channel: The Food Issue' => 'channel/food',
+                    'Channel: The Kinship Issue' => 'channel/kinship',
+                    'Channel: The Porthole' => 'channel/the-porthole',
+                    'Channel: The Reality Issue' => 'channel/reality',
+                    'Channel: The Rebel Issue' => 'channel/rebel',
+                    'Channel: Women in Science & Engineering' => 'channel/wise'
+                ]
+            ]
         ],
     ];
 
     public function collectData()
     {
-        $this->collectExpandableDatas(self::URI . 'feed', (int)$this->getInput('limit'));
+        $uri = self::URI;
+        if ($this->getInput('topic')) {
+            $uri .= $this->getInput('topic') . '/';
+        }
+        $uri .= 'feed';
+
+        $this->collectExpandableDatas($uri, (int)$this->getInput('limit'));
     }
 
     protected function parseItem(array $item)
