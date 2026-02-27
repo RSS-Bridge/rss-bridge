40f044fdab9f3ee0e4d94cf2d39b5f4bf2eac4f9
Giò Diani <mail@gionathandiani.name>
2026-02-27 18:38:37 +0100

    [RaiPlaySound] Add RaiPlay Sound Bridge

---
+++ bridges/RaiPlaySoundBridge.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+class RaiPlaySoundBridge extends BridgeAbstract
+{
+    const NAME = 'RaiPlay Sound';
+    const URI = 'https://www.raiplaysound.it';
+    const DESCRIPTION = 'Get feeds for shows in the Podcast and Audiolibri sections from RaiPlay Sound.';
+    const MAINTAINER = 'giodi';
+    const PARAMETERS = [
+    [
+      'path' => [
+        'name' => 'URL',
+        'required' => true,
+        'title' => 'Insert the URL to the page of a show.',
+        'exampleValue' => 'https://www.raiplaysound.it/programmi/ilfalso'
+      ],
+      'limit' => self::LIMIT,
+    ]
+    ];
+
+    /**
+     * Holds the title of the current show
+     *
+     * @var string
+     */
+    private $title;
+
+    public function collectData(): void
+    {
+        $url = $this->getInput('path');
+        $limit = $this->getInput('limit');
+        $html = getContents($url);
+
+        $dom = getSimpleHTMLDOM($url);
+        $h1 = $dom->find('h1');
+        $this->title = $h1[0]->plaintext . self::NAME;
+        $episodesJson = [];
+
+        foreach ($dom->find('rps-playlist-action') as $el) {
+            $episodesJson[] = json_decode($el->getAttribute('options'), true)['url'];
+        }
+
+        if ($limit !== null) {
+            $episodesJson = array_slice($episodesJson, 0, $limit);
+        }
+
+        foreach ($episodesJson as $ep) {
+            $data = json_decode(getContents(self::URI . $ep), true);
+            $item = [];
+            $item['uri'] = self::URI . $data['weblink'];
+            $item['title'] = $data['episode_title'];
+            $item['content'] = $data['description'];
+            $item['enclosures'] = [$data['audio']['url']];
+            $item['author'] = $data['podcast_info']['title'];
+            $this->items[] = $item;
+        }
+    }
+
+    /** {@inheritdoc} */
+    public function getName()
+    {
+        if (!empty($this->title)) {
+            return $this->title;
+        }
+        return parent::getName();
+    }
+}
