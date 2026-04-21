a9a1db23563a0520ec83c28f7ec4f162e02bf82d
Christian Schabesberger <chris.schabesberger@mailbox.org>
2026-04-21 09:56:03 +0200

    refactor and fix some issues with nordbayern bridge
    remove the /region/ url subpath from nordbayern bridge
    
    filter empty articles in nordbayern
    
    fix and add new filters
    
    don't track most red artickles
    
    fix images not shown in buttons
    
    fix region path
    
    adapt setInput() function to be backward compatible

--- bridges/NordbayernBridge.php
+++ bridges/NordbayernBridge.php
@@ -7,59 +7,114 @@ class NordbayernBridge extends BridgeAbstract
     const CACHE_TIMEOUT = 3600;
     const URI = 'https://www.nordbayern.de';
     const DESCRIPTION = 'Bridge for Bavarian regional news site nordbayern.de';
-    const PARAMETERS = [ [
-        'region' => [
-            'name' => 'region',
-            'type' => 'list',
-            'exampleValue' => 'Nürnberg',
-            'title' => 'Select a region',
-            'values' => [
-                'Ansbach' => 'ansbach',
-                'Bamberg' => 'bamberg',
-                'Bayreuth' => 'bayreuth',
-                'Erlangen' => 'erlangen',
-                'Forchheim' => 'forchheim',
-                'Fürth' => 'fuerth',
-                'Gunzenhausen' => 'gunzenhausen',
-                'Herzogenaurach' => 'herzogenaurach',
-                'Höchstadt' => 'hoechstadt',
-                'Neumarkt' => 'neumarkt',
-                'Neustadt/Aisch-Bad Windsheim' => 'neustadt-aisch-bad-windsheim',
-                'Nürnberg' => 'nuernberg',
-                'Nürnberger Land' => 'nuernberger-land',
-                'Regensburg' => 'regensburg',
-                'Roth' => 'roth',
-                'Schwabach' => 'schwabach',
-                'Weißenburg' => 'weissenburg'
+    const PARAMETERS = [
+        [
+            'region' => [
+                'name' => 'region',
+                'type' => 'list',
+                'exampleValue' => 'Nürnberg',
+                'title' => 'Select a region',
+                'values' => [
+                    'Ansbach' => 'ansbach',
+                    'Bamberg' => 'bamberg',
+                    'Bayreuth' => 'bayreuth',
+                    'Erlangen' => 'erlangen',
+                    'Forchheim' => 'forchheim',
+                    'Fürth' => 'fuerth',
+                    'Gunzenhausen' => 'gunzenhausen',
+                    'Herzogenaurach' => 'herzogenaurach',
+                    'Höchstadt' => 'hoechstadt',
+                    'Neumarkt' => 'neumarkt',
+                    'Neustadt/Aisch-Bad Windsheim' => 'neustadt-aisch-bad-windsheim',
+                    'Nürnberg' => 'nuernberg',
+                    'Nürnberger Land' => 'nuernberger-land',
+                    'Regensburg' => 'regensburg',
+                    'Roth' => 'roth',
+                    'Schwabach' => 'schwabach',
+                    'Weißenburg-Gunzenhausen' => 'weissenburg-gunzenhausen'
+                ]
+            ],
+            'hideGenussShopping' => [
+                'name' => 'Hide Genuss & Shopping',
+                'type' => 'checkbox',
+                'exampleValue' => 'unchecked',
+                'title' => 'Hide articles categorized as Genuss & Shopping'
+            ],
+            'hideSport' => [
+                'name' => 'Hide Sport',
+                'type' => 'checkbox',
+                'exampleValue' => 'unchecked',
+                'title' => 'Hide articles categorized as Sport'
+            ],
+            'hidePromiesTrends' => [
+                'name' => 'Hide Promies & Trends',
+                'type' => 'checkbox',
+                'exampleValue' => 'unchecked',
+                'title' => 'Hide articles categorized as Promies & Trends'
+            ],
+            'hideService' => [
+                'name' => 'Hide Service',
+                'type' => 'checkbox',
+                'exampleValue' => 'unchecked',
+                'title' => 'Hide articles categorized as Service'
+            ],
+            'hideFranken' => [
+                'name' => 'Hide Franken',
+                'type' => 'checkbox',
+                'exampleValue' => 'unchecked',
+                'title' => 'Hide articles categorized as Franken'
+            ],
+            'hideBayern' => [
+                'name' => 'Hide Bayern',
+                'type' => 'checkbox',
+                'exampleValue' => 'unchecked',
+                'title' => 'Hide articles categorized as Bayern'
+            ],
+            'hidePanorama' => [
+                'name' => 'Hide Panorama',
+                'type' => 'checkbox',
+                'exampleValue' => 'unchecked',
+                'title' => 'Hide articles categorized as Panorama'
+            ],
+            'hidePolizeiberichte' => [
+                'name' => 'Hide Polizeiberichte',
+                'type' => 'checkbox',
+                'exampleValue' => 'unchecked',
+                'title' => 'Hide articles categorized as Polizeiberichte'
+            ],
+            'hideNN' => [
+                'name' => 'Hide Nürnberger Nachrichten',
+                'type' => 'checkbox',
+                'exampleValue' => 'checked',
+                'defaultValue' => 'checked',
+                'title' => 'Hide articles hosted on www.nn.de'
             ]
-        ],
-        'policeReports' => [
-            'name' => 'Police Reports',
-            'type' => 'checkbox',
-            'exampleValue' => 'checked',
-            'title' => 'Include Police Reports',
-        ],
-        'hideNNPlus' => [
-            'name' => 'Hide NN+ articles',
-            'type' => 'checkbox',
-            'exampleValue' => 'unchecked',
-            'title' => 'Hide all paywall articles on NN'
-        ],
-        'hideDPA' => [
-        'name' => 'Hide dpa articles',
-        'type' => 'checkbox',
-        'exampleValue' => 'unchecked',
-        'title' => 'Hide external articles from dpa'
         ]
-    ]];
+    ];
+
+    public function setInput(array $input)
+    {
+        // Translate legacy parameter names so existing feed URLs keep working.
+        if (isset($input['hideNNPlus'])) {
+            $input['hideNN'] = $input['hideNNPlus'];
+            unset($input['hideNNPlus']);
+        }
+        if (isset($input['policeReports'])) {
+            if (!filter_var($input['policeReports'], FILTER_VALIDATE_BOOLEAN)) {
+                $input['hidePolizeiberichte'] = 'on';
+            }
+            unset($input['policeReports']);
+        }
+        parent::setInput($input);
+    }
 
     public function collectData()
     {
         $region = $this->getInput('region');
-        if ($region === 'rothenburg-o-d-t') {
-            $region = 'rothenburg-ob-der-tauber';
+        if ($region !== 'nurnberg' && $region !== 'fuerth' && $region !== 'erlangen') {
+            $region = 'region/' . $region;
         }
-        $url = self::URI . '/region/' . $region;
+        $url = self::URI . '/' . $region;
         $listSite = getSimpleHTMLDOM($url);
 
         $this->handleNewsblock($listSite);
@@ -105,6 +160,8 @@ class NordbayernBridge extends BridgeAbstract
                 $content .= $this->getUseFullContent($element);
             } elseif ($element->tag === 'picture') {
                 $content .= $this->getValidImage($element);
+            } elseif ($element->tag === 'button') {
+                $content .= $this->getUseFullContent($element);
             } elseif ($element->tag === 'ul') {
                 $content .= $element;
             }
@@ -151,7 +208,7 @@ class NordbayernBridge extends BridgeAbstract
 
         if ($article->find('section[class*=article__richtext]', 0) === null) {
             $content = $article->find('div[class*=modul__teaser]', 0)
-                           ->find('p', 0);
+                ->find('p', 0);
             $item['content'] .= $content;
         } else {
             $content = $article->find('article', 0);
@@ -175,34 +232,84 @@ class NordbayernBridge extends BridgeAbstract
         return $item;
     }
 
+    private function findMostReadSection($main)
+    {
+        foreach ($main->find('section') as $section) {
+            $header = $section->find('div[class=modul__header]', 0);
+            if ($header !== null && str_contains($header->plaintext, 'Meistgelesen in Nürnberg')) {
+                return $section;
+            }
+        }
+        return null;
+    }
+
+    private function isInsideSection($article, $section)
+    {
+        if ($section === null) {
+            return false;
+        }
+        $ancestor = $article->parent;
+        while ($ancestor !== null) {
+            if ($ancestor === $section) {
+                return true;
+            }
+            $ancestor = $ancestor->parent;
+        }
+        return false;
+    }
+
     private function handleNewsblock($listSite)
     {
         $main = $listSite->find('main', 0);
+        $meistgelesenSection = $this->findMostReadSection($main);
         foreach ($main->find('article') as $article) {
+            // skip articles inside the "Meistgelesen in Nürnberg" section
+            if ($this->isInsideSection($article, $meistgelesenSection)) {
+                continue;
+            }
+
+            // skip empty articles
+            if (is_null($article->find('a', 0))) {
+                continue;
+            }
+
             $url = $article->find('a', 0)->href;
             $url = urljoin(self::URI, $url);
-            // exclude nn+ articles if desired
-            if (
-                $this->getInput('hideNNPlus') &&
-                str_contains($url, 'www.nn.de')
-            ) {
+
+            // skip articles based on category segment in URL
+            if ($this->getInput('hideGenussShopping') && str_contains($url, '/genuss-shopping/')) {
+                continue;
+            }
+            if ($this->getInput('hideSport') && str_contains($url, '/sport/')) {
+                continue;
+            }
+            if ($this->getInput('hidePromiesTrends') && str_contains($url, '/promis-trends/')) {
+                continue;
+            }
+            if ($this->getInput('hideService') && str_contains($url, '/service/')) {
+                continue;
+            }
+            if ($this->getInput('hideFranken') && str_contains($url, '/franken/')) {
+                continue;
+            }
+            if ($this->getInput('hideBayern') && str_contains($url, '/bayern/')) {
+                continue;
+            }
+            if ($this->getInput('hidePanorama') && str_contains($url, '/panorama/')) {
+                continue;
+            }
+            if ($this->getInput('hidePolizeiberichte') && str_contains($url, '/polizeibericht')) {
+                continue;
+            }
+            if ($this->getInput('hideNN') && str_contains($url, 'www.nn.de')) {
                 continue;
             }
 
             $item = $this->getArticle($url);
 
-            // exclude police reports if desired
             if (
-                !$this->getInput('policeReports') &&
-                str_contains($item['content'], 'Hier geht es zu allen aktuellen Polizeimeldungen.')
-            ) {
-                continue;
-            }
-
-            // exclude dpa articles
-            if (
-                $this->getInput('hideDPA') &&
-                str_contains($item['author'], 'dpa')
+                $this->getInput('hidePolizeiberichte')
+                && str_contains($item['content'], 'Hier geht es zu allen aktuellen Polizeimeldungen.')
             ) {
                 continue;
             }
