8f0070dbde254b6c3fdfdff60b49ac9fe08e13b2
AMA147000 <am1470235689@gmail.com>
2026-05-29 02:37:09 +0300

    ran phpcs
    My issues where all about using `"` instead of `'`

--- bridges/COPRBridge.php
+++ bridges/COPRBridge.php
@@ -4,24 +4,24 @@ declare(strict_types=1);
 
 class COPRBridge extends BridgeAbstract
 {
-    const NAME = "COPR";
-    const URI = "https://copr.fedorainfracloud.org/";
-    const DESCRIPTION = "Returns the status of the latest builds for a COPR project.";
-    const MAINTAINER = "AMA147000";
+    const NAME = 'COPR';
+    const URI = 'https://copr.fedorainfracloud.org/';
+    const DESCRIPTION = 'Returns the status of the latest builds for a COPR project.';
+    const MAINTAINER = 'AMA147000';
 
     const PARAMETERS = [
         [
-            "user" => [
-                "name" => "User",
-                "type" => "text",
-                "required" => true,
-                "exampleValue" => "ama1470",
+            'user' => [
+                'name' => 'User',
+                'type' => 'text',
+                'required' => true,
+                'exampleValue' => 'ama1470',
             ],
-            "project" => [
-                "name" => "Project",
-                "type" => "text",
-                "required" => true,
-                "exampleValue" => "kwin-effects-glass",
+            'project' => [
+                'name' => 'Project',
+                'type' => 'text',
+                'required' => true,
+                'exampleValue' => 'kwin-effects-glass',
             ],
         ],
     ];
@@ -29,21 +29,21 @@ class COPRBridge extends BridgeAbstract
     public function collectData()
     {
         $url = sprintf(
-            "https://copr.fedorainfracloud.org/coprs/%s/%s/builds/",
-            $this->getInput("user"),
-            $this->getInput("project"),
+            'https://copr.fedorainfracloud.org/coprs/%s/%s/builds/',
+            $this->getInput('user'),
+            $this->getInput('project'),
         );
 
         $dom = getSimpleHTMLDOM($url);
 
-        foreach ($dom->find("tr.build-row") as $row) {
-            $columns = $row->find("td");
+        foreach ($dom->find('tr.build-row') as $row) {
+            $columns = $row->find('td');
 
             $build_id = trim($columns[0]->plaintext);
             $build_url = sprintf(
-                "https://copr.fedorainfracloud.org/coprs/%s/%s/build/%s/",
-                $this->getInput("user"),
-                $this->getInput("project"),
+                'https://copr.fedorainfracloud.org/coprs/%s/%s/build/%s/',
+                $this->getInput('user'),
+                $this->getInput('project'),
                 $build_id,
             );
             $package = trim($columns[1]->plaintext);
@@ -53,11 +53,11 @@ class COPRBridge extends BridgeAbstract
 
             // Filter out non-final statuses like 'importing' or 'starting'
             $valid_statuses = [
-                "succeeded",
-                "forked",
-                "skipped",
-                "failed",
-                "canceled",
+                'succeeded',
+                'forked',
+                'skipped',
+                'failed',
+                'canceled',
             ];
             // Side note: `needle` and `haystack` are just perfect input names lmao
             if (!in_array($status, $valid_statuses)) {
@@ -66,39 +66,39 @@ class COPRBridge extends BridgeAbstract
 
             $content_dom = getSimpleHTMLDOMCached($build_url, 86400);
             // Fix relative links
-            foreach ($content_dom->find("a") as $link) {
+            foreach ($content_dom->find('a') as $link) {
                 $href = $link->href;
 
-                if (strpos($href, "/") === 0 && strpos($href, "//") !== 0) {
-                    $link->href = "https://copr.fedorainfracloud.org" . $href;
+                if (strpos($href, '/') === 0 && strpos($href, '//') !== 0) {
+                    $link->href = 'https://copr.fedorainfracloud.org' . $href;
                 }
             }
-            $content = $content_dom->find("#content > .container > .row");
+            $content = $content_dom->find('#content > .container > .row');
             $content = implode("\n", $content);
 
             $item = [];
-            $item["uri"] = $build_url;
-            $item["title"] = "{$package} v{$version} {$status}";
-            $item["timestamp"] = $timestamp;
-            $item["author"] = $this->getInput("user");
-            $item["content"] = $content;
-            $item["categories"] = [$package, $status];
-            $item["uid"] = $build_id;
+            $item['uri'] = $build_url;
+            $item['title'] = "{$package} v{$version} {$status}";
+            $item['timestamp'] = $timestamp;
+            $item['author'] = $this->getInput('user');
+            $item['content'] = $content;
+            $item['categories'] = [$package, $status];
+            $item['uid'] = $build_id;
             $this->items[] = $item;
         }
 
         usort($this->items, function ($a, $b) {
-            return strtotime($b["timestamp"]) <=> strtotime($a["timestamp"]);
+            return strtotime($b['timestamp']) <=> strtotime($a['timestamp']);
         });
     }
 
     public function getName()
     {
-        if ($this->getInput("user") && $this->getInput("project")) {
+        if ($this->getInput('user') && $this->getInput('project')) {
             return sprintf(
-                "COPR Builds - %s/%s",
-                $this->getInput("user"),
-                $this->getInput("project"),
+                'COPR Builds - %s/%s',
+                $this->getInput('user'),
+                $this->getInput('project'),
             );
         }
         return self::NAME;
@@ -106,11 +106,11 @@ class COPRBridge extends BridgeAbstract
 
     public function getURI()
     {
-        if ($this->getInput("user") && $this->getInput("project")) {
+        if ($this->getInput('user') && $this->getInput('project')) {
             return sprintf(
-                "https://copr.fedorainfracloud.org/coprs/%s/%s/",
-                $this->getInput("user"),
-                $this->getInput("project"),
+                'https://copr.fedorainfracloud.org/coprs/%s/%s/',
+                $this->getInput('user'),
+                $this->getInput('project'),
             );
         }
         return self::URI;
@@ -118,6 +118,6 @@ class COPRBridge extends BridgeAbstract
 
     public function getIcon()
     {
-        return "https://copr.fedorainfracloud.org/static/favicon.ico";
+        return 'https://copr.fedorainfracloud.org/static/favicon.ico';
     }
 }
