efa39f7d5c4e47a4683b62e767e6f3113d89a4a5
Florent VIOLLEAU <florent.violleau@samsic.fr>
2026-07-09 00:31:48 +0200

    🐛(fix) Update EdfColorDayBridge - add strict type

--- bridges/EdfColorDayBridge.php
+++ bridges/EdfColorDayBridge.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 class EdfColorDayBridge extends BridgeAbstract
 {
     const NAME = 'EDF tempo color';
@@ -22,7 +24,8 @@ class EdfColorDayBridge extends BridgeAbstract
     const CACHE_TIMEOUT = 7200; // 2h
 
     /**
-     * @param string $json
+     * @param simple_html_dom $html
+     * @param string $contractUri
      * @return void
      */
     private function tempo(string $json): void
@@ -38,7 +41,7 @@ class EdfColorDayBridge extends BridgeAbstract
             $item = [];
 
             $text = $key . ' : ' . $this->getDisplayableColor($jsonDecoded['values'][$value]);
-            $item['uri'] = self::URI . '?season=' . $this->getTempoYear();
+            $item['uri'] = self::URI . $contractUri;
             $item['title'] = $text;
             $item['author'] = self::MAINTAINER;
             $item['content'] = $text;
